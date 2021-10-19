0f1cda0d3eb085087fa742a4bff449df62e81078
VVsssssk <88368822+VVsssssk@users.noreply.github.com>
2021-10-19 11:43:35 +0800

    [Unittest]Add mmcv unit test (#128)
    * add ncnn test exporter in test_ops.py
    
    * add ncnn test exporter in utils.py
    
    * add onnxruntime and tensorrt ops test
    
    * fix blank line
    
    * fix comment
    add nms ops test
    
    * remove nms test
    
    * add test sample
    add dockerstring
    
    * remove nms test
    
    * fix grid_sample
    add type hind
    
    * fix problem
    
    * fix dockerstring
    
    * add nms batch_nms multi_level_roi_align
    
    * add test data
    
    * fix problem
    
    * rm pkl file dependent
    
    * rm file
    
    * add docstring
    
    * remove multi_level_dependce
    
    * add mmseg module unittest
    
    * add mmseg test
    
    * add mmseg model unit test
    
    * fix blankline
    
    * rename file
    
    * add syncbn2bn unit test
    
    * add apis/export
    
    * lint
    
    * lint
    
    * ??
    
    * delete#
    
    * fix ncnn check
    
    * fix problems
    
    * fix problems
    
    * add mmcv unit test
    
    * add mmcv unit test
    
    * modify scores
    
    * fix format
    
    * fix scores to rand
    
    * remove blank line
    
    Co-authored-by: SingleZombie <singlezombie@163.com>

---
+++ tests/test_mmcv/test_mmcv_ops.py
@@ -0,0 +1,50 @@
+import tempfile
+
+import onnx
+import pytest
+import torch
+
+from mmdeploy.core import register_extra_symbolics
+from mmdeploy.utils.test import WrapFunction
+
+
+@pytest.mark.parametrize(
+    'iou_threshold, score_threshold,max_output_boxes_per_class',
+    [(0.6, 0.2, 3)])
+def test_ONNXNMSop(iou_threshold, score_threshold, max_output_boxes_per_class):
+    boxes = torch.tensor([[[291.1746, 316.2263, 343.5029, 347.7312],
+                           [288.4846, 315.0447, 343.7267, 346.5630],
+                           [288.5307, 318.1989, 341.6425, 349.7222],
+                           [918.9102, 83.7463, 933.3920, 164.9041],
+                           [895.5786, 78.2361, 907.8049, 172.0883],
+                           [292.5816, 316.5563, 340.3462, 352.9989],
+                           [609.4592, 83.5447, 631.2532, 144.0749],
+                           [917.7308, 85.5870, 933.2839, 168.4530],
+                           [895.5138, 79.3596, 908.2865, 171.0418],
+                           [291.4747, 318.6987, 347.1208, 349.5754]]])
+    scores = torch.rand(1, 5, 10)
+
+    cfg = dict()
+    register_extra_symbolics(cfg=cfg, backend='default', opset=11)
+    from mmdeploy.mmcv.ops import ONNXNMSop
+
+    def wrapped_function(torch_bboxes, torch_scores):
+        return ONNXNMSop.apply(torch_bboxes, torch_scores,
+                               max_output_boxes_per_class, iou_threshold,
+                               score_threshold)
+
+    wrapped_model = WrapFunction(wrapped_function).eval()
+    result = wrapped_model(boxes, scores)
+    assert result is not None
+    onnx_file_path = tempfile.NamedTemporaryFile().name
+    with torch.no_grad():
+        torch.onnx.export(
+            wrapped_model, (boxes, scores),
+            onnx_file_path,
+            export_params=True,
+            keep_initializers_as_inputs=True,
+            input_names=['boxes', 'scores'],
+            output_names=['result'],
+            opset_version=11)
+    model = onnx.load(onnx_file_path)
+    assert model.graph.node[3].op_type == 'NonMaxSuppression'
