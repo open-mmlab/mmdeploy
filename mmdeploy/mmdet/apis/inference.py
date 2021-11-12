1c86b3977f491e32c03a24a2b2ce0f5f78b0d893
Yifan Zhou <singlezombie@163.com>
2021-11-12 17:33:45 +0800

    [Fix]: Fix topk in post processing for ncnn (#197)
    * Fix
    
    * update
    
    * Fix ncnn postprocessing
    
    * recover code

--- mmdeploy/mmdet/apis/inference.py
+++ mmdeploy/mmdet/apis/inference.py
@@ -408,7 +408,8 @@ class PartitionSingleStageDetector(DeployBaseDetector):
         max_output_boxes_per_class = post_params.max_output_boxes_per_class
         iou_threshold = cfg.nms.get('iou_threshold', post_params.iou_threshold)
         score_threshold = cfg.get('score_thr', post_params.score_threshold)
-        pre_top_k = post_params.pre_top_k
+        pre_top_k = -1 if post_params.pre_top_k >= bboxes.shape[1] \
+            else post_params.pre_top_k
         keep_top_k = cfg.get('max_per_img', post_params.keep_top_k)
         ret = multiclass_nms(
             bboxes,
