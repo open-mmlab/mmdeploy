d79413fa3252c586833d4e1c093ca8eb87db879b
lvhan028 <lvhan_028@163.com>
2021-12-24 18:43:26 +0800

    update c-api's comments by following doxygen's guide (#336)
    * update c-api's comments by following doxygen's guide
    
    * fix according to review comments

--- csrc/apis/c/detector.h
+++ csrc/apis/c/detector.h
@@ -1,5 +1,10 @@
 // Copyright (c) OpenMMLab. All rights reserved.
 
+/**
+ * @file detector.h
+ * @brief Interface to MMDetection task
+ */
+
 #ifndef MMDEPLOY_DETECTOR_H
 #define MMDEPLOY_DETECTOR_H
 
@@ -18,16 +23,24 @@ typedef struct mm_detect_t {
   mm_instance_mask_t* mask;
 } mm_detect_t;
 
+/**
+ * @brief Create detector's handle
+ * @param[in] model an instance of mmdetection sdk model created by
+ * \ref mmdeploy_model_create_by_path or \ref mmdeploy_model_create in \ref model.h
+ * @param[in] device_name name of device, such as "cpu", "cuda", etc.
+ * @param[in] device_id id of device.
+ * @param[out] handle instance of a detector
+ * @return status of creating detector's handle
+ */
 MM_SDK_API int mmdeploy_detector_create(mm_model_t model, const char* device_name, int device_id,
                                         mm_handle_t* handle);
 
 /**
  * @brief Create detector's handle
- * @param model_path detector's config which is supposed to be json. Refer to
- * `@PROJECT_ROOT_DIR/config/detector/retinanet_config.json`
- * @param device_name name of device, such as "cpu", "cuda" and etc.
- * @param device_id id of device.
- * @param handle instance of a detector
+ * @param[in] model_path path of mmdetection sdk model exported by mmdeploy model converter
+ * @param[in] device_name name of device, such as "cpu", "cuda", etc.
+ * @param[in] device_id id of device.
+ * @param[out] handle instance of a detector
  * @return status of creating detector's handle
  */
 MM_SDK_API int mmdeploy_detector_create_by_path(const char* model_path, const char* device_name,
@@ -35,33 +48,30 @@ MM_SDK_API int mmdeploy_detector_create_by_path(const char* model_path, const ch
 
 /**
  * @brief Apply detector to batch images and get their inference results
- * @param handle detector's handle made by `mmdeploy_detector_create_by_path`
- * @param mats batch images
- * @param mat_count number of images in a batch
- * @param results a consecutive buffer to save detection results of each image.
- * User has to access `result_count` to get the beginning and ending position of
- * an image's detection results. It is created inside and should be destroyed by
- * calling `mmdeploy_detector_release_result`
- * @param result_count a consecutive buffer to save the number of detection
- * results of each image. Its length is equal to `mat_count`. It is created
- * inside and should be destroyed by calling `mmdeploy_detector_release_result`
+ * @param[in] handle detector's handle created by \ref mmdeploy_detector_create_by_path
+ * @param[in] mats a batch of images
+ * @param[in] mat_count number of images in the batch
+ * @param[out] results a linear buffer to save detection results of each image. It must be released
+ * by \ref mmdeploy_detector_release_result
+ * @param result_count a linear buffer with length being \p mat_count to save the number of
+ * detection results of each image. And it must be released by \ref
+ * mmdeploy_detector_release_result
  * @return status of inference
  */
 MM_SDK_API int mmdeploy_detector_apply(mm_handle_t handle, const mm_mat_t* mats, int mat_count,
                                        mm_detect_t** results, int** result_count);
 
-/** @brief release the inference result buffer
- * @param results a consecutive buffer to save images' detection results
- * @param result_count a consecutive buffer to save the size of detection
- * results of each image in a batch
- * @param count length of `result_count`
+/** @brief Release the inference result buffer created by \ref mmdeploy_detector_apply
+ * @param[in] results detection results buffer
+ * @param[in] result_count  \p results size buffer
+ * @param[in] count length of \p result_count
  */
 MM_SDK_API void mmdeploy_detector_release_result(mm_detect_t* results, const int* result_count,
                                                  int count);
 
 /**
- * @brief destroy detector's handle
- * @param handle detector's handle made by `mmdeploy_detector_create_by_path`
+ * @brief Destroy detector's handle
+ * @param[in] handle detector's handle created by \ref mmdeploy_detector_create_by_path
  */
 MM_SDK_API void mmdeploy_detector_destroy(mm_handle_t handle);
 
--- csrc/apis/c/text_detector.h
+++ csrc/apis/c/text_detector.h
@@ -1,5 +1,10 @@
 // Copyright (c) OpenMMLab. All rights reserved.
 
+/**
+ * @file text_detector.h
+ * @brief Interface to MMOCR text detection task
+ */
+
 #ifndef MMDEPLOY_TEXT_DETECTOR_H
 #define MMDEPLOY_TEXT_DETECTOR_H
 
@@ -10,16 +15,26 @@ typedef struct mm_text_detect_t {
   float score;
 } mm_text_detect_t;
 
+/**
+ * @brief Create text-detector's handle
+ * @param[in] model an instance of mmocr text detection model created by
+ * \ref mmdeploy_model_create_by_path or \ref mmdeploy_model_create in \ref model.h
+ * @param[in] device_name name of device, such as "cpu", "cuda", etc.
+ * @param[in] device_id id of device.
+ * @param[out] handle instance of a text-detector, which must be destroyed
+ * by \ref mmdeploy_text_detector_destroy
+ * @return status of creating text-detector's handle
+ */
 MM_SDK_API int mmdeploy_text_detector_create(mm_model_t model, const char* device_name,
                                              int device_id, mm_handle_t* handle);
 
 /**
  * @brief Create text-detector's handle
- * @param config text-detector's config which is supposed to be json. Refer to
- * `@PROJECT_ROOT_DIR/config/text-detector/dbnet_config.json`
- * @param device_name name of device, such as "cpu", "cuda" and etc.
- * @param device_id id of device.
- * @param handle instance of a text-detector
+ * @param[in] model_path path to text detection model
+ * @param[in] device_name name of device, such as "cpu", "cuda", etc.
+ * @param[in] device_id id of device
+ * @param[out] handle instance of a text-detector, which must be destroyed
+ * by \ref mmdeploy_text_detector_destroy
  * @return status of creating text-detector's handle
  */
 MM_SDK_API int mmdeploy_text_detector_create_by_path(const char* model_path,
@@ -28,35 +43,30 @@ MM_SDK_API int mmdeploy_text_detector_create_by_path(const char* model_path,
 
 /**
  * @brief Apply text-detector to batch images and get their inference results
- * @param handle text-detector's handle made by `mmdeploy_text_detector_create_by_path`
- * @param mats batch images
- * @param mat_count number of images in a batch
- * @param results a consecutive buffer to save text detection results of each
- * image. Its length equals to `mat_count` if this function returns success. The buffer should be
- * destroyed by calling `mmdeploy_text_detector_release_result`.
- * @param result_count a consecutive buffer to save the number of detection
- * results of each image. Its length equals to `mat_count` if this function returns success. It
- * should be destroyed by calling `mmdeploy_detector_release_result`
+ * @param[in] handle text-detector's handle created by \ref mmdeploy_text_detector_create_by_path
+ * @param[in] mats a batch of images
+ * @param[in] mat_count number of images in the batch
+ * @param[out] results a linear buffer to save text detection results of each
+ * image. It must be released by calling \ref mmdeploy_text_detector_release_result
+ * @param[out] result_count a linear buffer of length \p mat_count to save the number of detection
+ * results of each image. It must be released by \ref mmdeploy_detector_release_result
  * @return status of inference
  */
 MM_SDK_API int mmdeploy_text_detector_apply(mm_handle_t handle, const mm_mat_t* mats, int mat_count,
                                             mm_text_detect_t** results, int** result_count);
 
-/** @brief release the inference result buffer
- * @param results a consecutive buffer to save images' text detection results,
- * which is created by `mmdeploy_text_detector_apply`
- * @param result_count a consecutive buffer to save the size of text detection
- * results of each image in a batch. `result_count[i]` represents the text bbox number in the `i`-th
- * input image, i.e. `mats[i]` in `mmdeploy_text_detector_apply`
- * @param count the length of buffer `results`, which is the same as `mat_count` in
- * `mmdeploy_text_detector_apply`
+/** @brief Release the inference result buffer returned by \ref mmdeploy_text_detector_apply
+ * @param[in] results text detection result buffer
+ * @param[in] result_count  \p results size buffer
+ * @param[in] count the length of buffer \p result_count
  */
 MM_SDK_API void mmdeploy_text_detector_release_result(mm_text_detect_t* results,
                                                       const int* result_count, int count);
 
 /**
- * @brief destroy text-detector's handle
- * @param handle text-detector's handle made by `mmdeploy_text_detector_create_by_path`
+ * @brief Destroy text-detector's handle
+ * @param[in] handle text-detector's handle created by \ref mmdeploy_text_detector_create_by_path or
+ * \ref mmdeploy_text_detector_create
  */
 MM_SDK_API void mmdeploy_text_detector_destroy(mm_handle_t handle);
 
