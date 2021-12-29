cc85230269bb4251415b1b461adb69e7b3917f62
grimoire <yaoqian@sensetime.com>
2021-12-29 11:49:29 +0800

    add typehint and docstring

--- mmdeploy/utils/utils.py
+++ mmdeploy/utils/utils.py
@@ -1,9 +1,26 @@
 import logging
 import sys
 import traceback
+from typing import Callable, Optional
+
+import torch.multiprocessing as mp
 
 
-def target_wrapper(target, log_level, ret_value, *args, **kwargs):
+def target_wrapper(target: Callable,
+                   log_level: int,
+                   ret_value: Optional[mp.Value] = None,
+                   *args,
+                   **kwargs):
+    """The wrapper used to start a new subprocess.
+
+    Args:
+        target (Callable): The target function to be wrapped.
+        log_level (int): Log level for logging.
+        ret_value (mp.Value): The success flag of target.
+
+    Return:
+        Any: The return of target.
+    """
     logger = logging.getLogger()
     logging.basicConfig(
         format='%(asctime)s,%(name)s %(levelname)-8s'
