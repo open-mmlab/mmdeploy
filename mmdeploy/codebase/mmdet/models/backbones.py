eda511cab276a28ead31b70ee6942e4d9ead5a07
hanrui1sensetime <hanrui1@sensetime.com>
2021-12-29 20:11:35 +0800

    add yolox ncnn

---
+++ mmdeploy/codebase/mmdet/models/backbones.py
@@ -0,0 +1,53 @@
+import torch
+
+from mmdeploy.core import FUNCTION_REWRITER
+
+
+@FUNCTION_REWRITER.register_rewriter(
+    func_name='mmdet.models.backbones.csp_darknet.Focus.forward',
+    backend='ncnn')
+def focus__forward__ncnn(ctx,
+                         self,
+                         x):
+    """Rewrite forward function of Focus class for ncnn.
+
+    Focus width and height information into channel space. NCNN does not
+    support slice operator which step greater than 1, so we use another
+    way to implement.
+
+    Args:
+        x (Tensor): The input tensor with shape (N, C, H, W).
+
+    Returns:
+        x (Tensor): The calculated tensor with shape (N, 4*C, H//2, W//2).
+    """
+
+    batch_size, c, h, w = x.shape
+    assert h % 2 == 0 and w % 2 == 0, f'focus for yolox needs even feature\
+        height and width, got {(h, w)}.'
+    x_col = x.reshape(batch_size, c, -1, 2)
+    left = x_col[:, :, :, 0:1]
+    right = x_col[:, :, :, 1:2]
+    left = left.reshape(batch_size, c, h, -1)
+    left = left.permute(0, 1, 3, 2).reshape(batch_size, c, -1, 2)
+    top_left = left[:, :, :, 0:1].reshape(batch_size, c, w//2, h//2).\
+        permute(0, 1, 3, 2)
+    bot_left = left[:, :, :, 1:2].reshape(batch_size, c, w//2, h//2).\
+        permute(0, 1, 3, 2)
+    right = right.reshape(batch_size, c, h, -1)
+    right = right.permute(0, 1, 3, 2).reshape(batch_size, c, -1, 2)
+    top_right = right[:, :, :, 0:1].reshape(batch_size, c, w//2, h//2).\
+        permute(0, 1, 3, 2)
+    bot_right = right[:, :, :, 1:2].reshape(batch_size, c, w//2, h//2).\
+        permute(0, 1, 3, 2)
+
+    x = torch.cat(
+        (
+            top_left,
+            bot_left,
+            top_right,
+            bot_right,
+            ),
+        dim=1,
+        )
+    return self.conv(x)
--- mmdeploy/codebase/mmdet/models/dense_heads/__init__.py
+++ mmdeploy/codebase/mmdet/models/dense_heads/__init__.py
@@ -5,12 +5,12 @@ from .fovea_head import fovea_head__get_bboxes
 from .rpn_head import rpn_head__get_bboxes, rpn_head__get_bboxes__ncnn
 from .ssd_head import ssd_head__get_bboxes__ncnn
 from .yolo_head import yolov3_head__get_bboxes, yolov3_head__get_bboxes__ncnn
-from .yolox_head import yolox_head__get_bboxes
+from .yolox_head import yolox_head__get_bboxes, yolox_head__get_bboxes__ncnn
 
 __all__ = [
     'rpn_head__get_bboxes', 'rpn_head__get_bboxes__ncnn',
     'yolov3_head__get_bboxes', 'yolov3_head__get_bboxes__ncnn',
     'yolox_head__get_bboxes', 'base_dense_head__get_bbox',
     'fovea_head__get_bboxes', 'base_dense_head__get_bboxes__ncnn',
-    'ssd_head__get_bboxes__ncnn'
+    'ssd_head__get_bboxes__ncnn', 'yolox_head__get_bboxes__ncnn'
 ]
