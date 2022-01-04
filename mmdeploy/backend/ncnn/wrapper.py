0a6cace5b8ffdd6c85be801d9a9379067d7629bb
hanrui1sensetime <hanrui1@sensetime.com>
2022-01-04 17:18:57 +0800

    fix None bugs for ncnn

--- mmdeploy/backend/ncnn/wrapper.py
+++ mmdeploy/backend/ncnn/wrapper.py
@@ -112,11 +112,15 @@ class NCNNWrapper(BaseWrapper):
                     mat = None
                     logging.warning(
                         f'The "{name}" output of ncnn model is empty.')
+                    continue
                 outputs[name][batch_id] = torch.from_numpy(np.array(mat))
 
         # stack outputs together
         for name, output_tensor in outputs.items():
-            outputs[name] = torch.stack(output_tensor)
+            if None in output_tensor:
+                outputs[name] = None
+            else:
+                outputs[name] = torch.stack(output_tensor)
 
         return outputs
 
