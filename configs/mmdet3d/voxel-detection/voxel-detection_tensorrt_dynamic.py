84c6d2a2745272324b75990b99ac1cd336529272
VVsssssk <shenkun@pjlab.org.cn>
2022-01-24 19:19:53 +0800

    fix config

--- configs/mmdet3d/voxel-detection/voxel-detection_tensorrt_dynamic.py
+++ configs/mmdet3d/voxel-detection/voxel-detection_tensorrt_dynamic.py
@@ -5,14 +5,14 @@ backend_config = dict(
         dict(
             input_shapes=dict(
                 voxels=dict(
-                    min_shape=[3000, 32, 4],
+                    min_shape=[4000, 32, 4],
                     opt_shape=[4000, 32, 4],
-                    max_shape=[5000, 32, 4]),
+                    max_shape=[4000, 32, 4]),
                 num_points=dict(
-                    min_shape=[3000], opt_shape=[4000], max_shape=[5000]),
+                    min_shape=[4000], opt_shape=[4000], max_shape=[4000]),
                 coors=dict(
-                    min_shape=[3000, 4],
+                    min_shape=[4000, 4],
                     opt_shape=[4000, 4],
-                    max_shape=[5000, 4]),
+                    max_shape=[4000, 4]),
             ))
     ])
