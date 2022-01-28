a2b69fc9f6a04c1027526685b1f6bb3dc55c1a59
grimoire <yaoqian@sensetime.com>
2022-01-28 19:43:23 +0800

    add python api

--- mmdeploy/backend/torchscript/init_plugins.py
+++ mmdeploy/backend/torchscript/init_plugins.py
@@ -1,5 +1,6 @@
 import glob
 import os.path as osp
+import platform
 
 
 def get_ops_path() -> str:
@@ -12,6 +13,22 @@ def get_ops_path() -> str:
         osp.join(
             osp.dirname(__file__),
             '../../../build/lib/libmmdeploy_torchscript_ops.so'))
+
+    paths = glob.glob(wildcard)
+    lib_path = paths[0] if len(paths) > 0 else ''
+    return lib_path
+
+
+def get_optimizer_path() -> str:
+    """Get ts_optimizer path.
+
+    Returns:
+        str: A path of ts_optimizer tool.
+    """
+    wildcard = osp.abspath(
+        osp.join(osp.dirname(__file__), '../../../build/bin/ts_optimizer'))
+    if platform.system() == 'Windows':
+        wildcard += '.exe'
 
     paths = glob.glob(wildcard)
     lib_path = paths[0] if len(paths) > 0 else ''
