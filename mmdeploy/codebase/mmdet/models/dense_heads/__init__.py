af591460550cad888ac148013c38608e15b216b0
Richard-mei <mei__h@163.com>
2022-02-08 19:05:19 +0800

    add gfl_head.py

--- mmdeploy/codebase/mmdet/models/dense_heads/__init__.py
+++ mmdeploy/codebase/mmdet/models/dense_heads/__init__.py
@@ -6,11 +6,14 @@ from .rpn_head import rpn_head__get_bboxes, rpn_head__get_bboxes__ncnn
 from .ssd_head import ssd_head__get_bboxes__ncnn
 from .yolo_head import yolov3_head__get_bboxes, yolov3_head__get_bboxes__ncnn
 from .yolox_head import yolox_head__get_bboxes, yolox_head__get_bboxes__ncnn
+from .gfl_head import gfl_head__get_bbox, gfl_head__forward_single
+
 
 __all__ = [
     'rpn_head__get_bboxes', 'rpn_head__get_bboxes__ncnn',
     'yolov3_head__get_bboxes', 'yolov3_head__get_bboxes__ncnn',
     'yolox_head__get_bboxes', 'base_dense_head__get_bbox',
     'fovea_head__get_bboxes', 'base_dense_head__get_bboxes__ncnn',
-    'ssd_head__get_bboxes__ncnn', 'yolox_head__get_bboxes__ncnn'
+    'ssd_head__get_bboxes__ncnn', 'yolox_head__get_bboxes__ncnn',
+    'gfl_head__get_bbox', 'gfl_head__forward_single'
 ]
