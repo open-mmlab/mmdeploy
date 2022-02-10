a2b3ab3d7fbb19c8cb6ae361b398ba1090206163
AllentDan <41138331+AllentDan@users.noreply.github.com>
2022-02-10 11:43:27 +0800

    Fix ci for mmocr (#144)
    * fix mmocr unittests
    
    * remove useless
    
    * lock mmdet maximum version to 2.20
    
    * pip install -U numpy

--- tests/test_codebase/test_mmocr/test_mmocr_models.py
+++ tests/test_codebase/test_mmocr/test_mmocr_models.py
@@ -74,7 +74,7 @@ def get_encode_decode_recognizer_model():
     cfg = dict(
         preprocessor=None,
         backbone=dict(type='VeryDeepVgg', leaky_relu=False, input_channels=1),
-        encoder=dict(type='TFEncoder'),
+        encoder=None,
         decoder=dict(type='CRNNDecoder', in_channels=512, rnn_flag=True),
         loss=dict(type='CTCLoss'),
         label_convertor=dict(
@@ -394,12 +394,15 @@ def test_forward_of_fpnc(backend: Backend):
                 model_inputs=[
                     dict(
                         input_shapes=dict(
-                            input=dict(
+                            inputs=dict(
                                 min_shape=[1, 3, 64, 64],
                                 opt_shape=[1, 3, 64, 64],
                                 max_shape=[1, 3, 64, 64])))
                 ]),
-            onnx_config=dict(input_shape=[64, 64], output_names=['output']),
+            onnx_config=dict(
+                input_shape=None,
+                input_names=['inputs'],
+                output_names=['output']),
             codebase_config=dict(type='mmocr', task='TextDetection')))
 
     input = torch.rand(1, 3, 64, 64).cuda()
