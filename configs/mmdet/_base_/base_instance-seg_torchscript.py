d6c9b8143b961dd2a290a89f1ea20170ccbe4a09
AllentDan <AllentDan@yeah.net>
2022-02-17 16:50:39 +0800

    det dynamic

--- configs/mmdet/_base_/base_instance-seg_torchscript.py
+++ configs/mmdet/_base_/base_instance-seg_torchscript.py
@@ -1,5 +1,26 @@
 _base_ = ['./base_torchscript.py']
 
 ir_config = dict(
-    output_names=['dets', 'labels', 'masks'], input_shape=[1344, 768])
+    output_names=['dets', 'labels', 'masks'],
+    dynamic_axes={
+        'input': {
+            0: 'batch',
+            2: 'height',
+            3: 'width'
+        },
+        'dets': {
+            0: 'batch',
+            1: 'num_dets',
+        },
+        'labels': {
+            0: 'batch',
+            1: 'num_dets',
+        },
+        'masks': {
+            0: 'batch',
+            1: 'num_dets',
+            2: 'height',
+            3: 'width'
+        },
+    })
 codebase_config = dict(post_processing=dict(export_postprocess_mask=False))
