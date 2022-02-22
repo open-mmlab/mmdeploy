c9c44a55908123023eaf26ef07025d7413d3e5ba
maningsheng <mnsheng@yeah.net>
2022-02-22 20:06:34 +0800

    support emanet and icnet

--- mmdeploy/codebase/mmseg/models/decode_heads/__init__.py
+++ mmdeploy/codebase/mmseg/models/decode_heads/__init__.py
@@ -1,5 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .aspp_head import aspp_head__forward
+from .ema_head import ema_module__forward
 from .psp_head import ppm__forward
 
-__all__ = ['aspp_head__forward', 'ppm__forward']
+__all__ = ['aspp_head__forward', 'ppm__forward', 'ema_module__forward']
