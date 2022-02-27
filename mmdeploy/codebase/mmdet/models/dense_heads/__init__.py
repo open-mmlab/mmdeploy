b582d757448ebe610e91c0daf4276258d1f11314
Richard-mei <mei__h@163.com>
2022-02-27 12:15:57 +0800

    Update __init__.py
    remove '**_forward_single'

--- mmdeploy/codebase/mmdet/models/dense_heads/__init__.py
+++ mmdeploy/codebase/mmdet/models/dense_heads/__init__.py
@@ -2,7 +2,7 @@
 from .base_dense_head import (base_dense_head__get_bbox,
                               base_dense_head__get_bboxes__ncnn)
 from .fovea_head import fovea_head__get_bboxes
-from .gfl_head import gfl_head__forward_single, gfl_head__get_bbox
+from .gfl_head import gfl_head__get_bbox
 from .rpn_head import rpn_head__get_bboxes, rpn_head__get_bboxes__ncnn
 from .ssd_head import ssd_head__get_bboxes__ncnn
 from .yolo_head import yolov3_head__get_bboxes, yolov3_head__get_bboxes__ncnn
@@ -14,5 +14,5 @@ __all__ = [
     'yolox_head__get_bboxes', 'base_dense_head__get_bbox',
     'fovea_head__get_bboxes', 'base_dense_head__get_bboxes__ncnn',
     'ssd_head__get_bboxes__ncnn', 'yolox_head__get_bboxes__ncnn',
-    'gfl_head__get_bbox', 'gfl_head__forward_single'
+    'gfl_head__get_bbox'
 ]
