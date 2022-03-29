dda6711cd0bfa2a33483c3860c7caaa994f33d66
maningsheng <mnsheng@yeah.net>
2022-03-29 11:03:32 +0800

    fix mmseg config

---
+++ configs/mmseg/segmentation_openvino_static-1024x2048.py
@@ -0,0 +1,4 @@
+_base_ = ['./segmentation_static.py', '../_base_/backends/openvino.py']
+onnx_config = dict(input_shape=[2048, 1024])
+backend_config = dict(
+    model_inputs=[dict(opt_shapes=dict(input=[1, 3, 1024, 2048]))])
