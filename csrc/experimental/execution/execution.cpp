843d65660a342c9e7f8cdd0b160a80ee33cda60e
zhangli <lzhang329@gmail.com>
2022-04-04 23:34:44 +0800

    simplify C API impl

--- csrc/experimental/execution/execution.cpp
+++ csrc/experimental/execution/execution.cpp
@@ -1,6 +1,5 @@
-//
-// Created by li on 2022/3/11.
-//
+// Copyright (c) OpenMMLab. All rights reserved.
+
 #include "execution.h"
 
 #include "core/value.h"
@@ -13,56 +12,69 @@ using namespace mmdeploy;
 
 using _Value = std::tuple<Value>;
 
-mmdeploy_scheduler_t mmdeploy_inline_scheduler() {
-  static auto v = new _TypeErasedScheduler(InlineScheduler{});
-  return reinterpret_cast<mmdeploy_scheduler_t>(v);
+namespace {
+
+inline _TypeErasedScheduler* Cast(mmdeploy_scheduler_t s) {
+  return reinterpret_cast<_TypeErasedScheduler*>(s);
 }
 
-mmdeploy_sender_t mmdeploy_executor_just(mmdeploy_value_t value) {
-  auto j = Just(*reinterpret_cast<Value*>(value));
-  auto s = new _TypeErasedSender<_Value>(std::move(j));
+inline mmdeploy_scheduler_t Cast(_TypeErasedScheduler* s) {
+  return reinterpret_cast<mmdeploy_scheduler_t>(s);
+}
+
+inline _TypeErasedSender<_Value>* Cast(mmdeploy_sender_t s) {
+  return reinterpret_cast<_TypeErasedSender<_Value>*>(s);
+}
+
+inline mmdeploy_sender_t Cast(_TypeErasedSender<_Value>* s) {
   return reinterpret_cast<mmdeploy_sender_t>(s);
 }
 
+inline mmdeploy_value_t Cast(Value* s) { return reinterpret_cast<mmdeploy_value_t>(s); }
+
+inline Value* Cast(mmdeploy_value_t s) { return reinterpret_cast<Value*>(s); }
+
+}  // namespace
+
+mmdeploy_scheduler_t mmdeploy_inline_scheduler() {
+  static auto v = new _TypeErasedScheduler(InlineScheduler{});
+  return Cast(v);
+}
+
+mmdeploy_sender_t mmdeploy_executor_just(mmdeploy_value_t value) {
+  auto j = Just(*Cast(value));
+  return Cast(new _TypeErasedSender<_Value>(std::move(j)));
+}
+
 mmdeploy_sender_t mmdeploy_executor_schedule(mmdeploy_scheduler_t scheduler) {
-  auto sched = reinterpret_cast<_TypeErasedScheduler*>(scheduler);
-  auto wrapped = Then(Schedule(*sched), [] { return Value(); });
-  return reinterpret_cast<mmdeploy_sender_t>(new _TypeErasedSender<_Value>(std::move(wrapped)));
+  auto wrapped = Then(Schedule(*Cast(scheduler)), [] { return Value(); });
+  return Cast(new _TypeErasedSender<_Value>(std::move(wrapped)));
 }
 
 mmdeploy_sender_t mmdeploy_executor_transfer(mmdeploy_sender_t input,
                                              mmdeploy_scheduler_t scheduler) {
-  auto input_sender = reinterpret_cast<_TypeErasedSender<_Value>*>(input);
-  auto sched = reinterpret_cast<_TypeErasedScheduler*>(scheduler);
-  auto output_sender = ScheduleFrom(*sched, std::move(*input_sender));
-  auto output = new _TypeErasedSender<_Value>(std::move(output_sender));
-  return reinterpret_cast<mmdeploy_sender_t>(output);
+  auto output_sender = ScheduleFrom(*Cast(scheduler), std::move(*Cast(input)));
+  return Cast(new _TypeErasedSender<_Value>(std::move(output_sender)));
 }
 
 mmdeploy_sender_t mmdeploy_executor_then(mmdeploy_sender_t input, mmdeploy_invocable_t fn,
                                          void* context) {
-  auto sender1 = reinterpret_cast<_TypeErasedSender<_Value>*>(input);
-  auto sender2 = Then(std::move(*sender1), [fn, context](Value u) {
-    auto v = reinterpret_cast<Value*>(fn(reinterpret_cast<mmdeploy_value_t>(&u), context));
+  auto sender2 = Then(std::move(*Cast(input)), [fn, context](Value u) {
+    auto v = Cast(fn(Cast(&u), context));
     Value w = std::move(*v);
     delete v;
     return w;
   });
-  auto output = new _TypeErasedSender<_Value>(std::move(sender2));
-  return reinterpret_cast<mmdeploy_sender_t>(output);
+  return Cast(new _TypeErasedSender<_Value>(std::move(sender2)));
 }
 
 mmdeploy_sender_t mmdeploy_executor_split(mmdeploy_sender_t input) {
-  auto input_sender = reinterpret_cast<_TypeErasedSender<_Value>*>(input);
-  auto split = Split(std::move(*input_sender));
-  auto output_sender = new _TypeErasedSender<_Value>(std::move(split));
-  return reinterpret_cast<mmdeploy_sender_t>(output_sender);
+  auto split = Split(std::move(*Cast(input)));
+  return Cast(new _TypeErasedSender<_Value>(std::move(split)));
 }
 
 mmdeploy_value_t mmdeploy_executor_sync_wait(mmdeploy_sender_t input) {
-  auto _input = reinterpret_cast<_TypeErasedSender<_Value>*>(input);
-  auto output = new Value(std::get<0>(SyncWait(std::move(*_input))));
-  return reinterpret_cast<mmdeploy_value_t>(output);
+  return Cast(new Value(std::get<0>(SyncWait(std::move(*Cast(input))))));
 }
 
 #endif
--- tests/test_csrc/core/test_execution.cpp
+++ tests/test_csrc/core/test_execution.cpp
@@ -1,3 +1,5 @@
+// Copyright (c) OpenMMLab. All rights reserved.
+
 #include <chrono>
 #include <numeric>
 
