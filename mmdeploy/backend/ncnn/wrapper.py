c15e2059d483aeb77fad56e416218deed0d61b6f
hanrui1sensetime <hanrui1@sensetime.com>
2022-04-07 22:13:59 +0800

    support ncnn vulkan

--- mmdeploy/backend/ncnn/wrapper.py
+++ mmdeploy/backend/ncnn/wrapper.py
@@ -41,6 +41,7 @@ class NCNNWrapper(BaseWrapper):
                  **kwargs):
 
         net = ncnn.Net()
+        net.opt.use_vulkan_compute = True
         if importlib.util.find_spec('mmdeploy.backend.ncnn.ncnn_ext'):
             from mmdeploy.backend.ncnn import ncnn_ext
             ncnn_ext.register_mmdeploy_custom_layers(net)
