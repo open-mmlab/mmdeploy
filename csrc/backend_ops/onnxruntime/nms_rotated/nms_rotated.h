f82931fc4085a33d1ae30b4df8bc691de2d1e353
erenzhou <592267829@qq.com>
2022-04-13 21:15:45 +0800

    fix lint

--- csrc/backend_ops/onnxruntime/nms_rotated/nms_rotated.h
+++ csrc/backend_ops/onnxruntime/nms_rotated/nms_rotated.h
@@ -41,9 +41,7 @@ struct NMSRotatedOp : Ort::CustomOpBase<NMSRotatedOp, NMSRotatedKernel> {
   }
 
   // force cpu
-  const char* GetExecutionProviderType() const {
-    return "CPUExecutionProvider";
-  }
+  const char* GetExecutionProviderType() const { return "CPUExecutionProvider"; }
 };
 }  // namespace mmdeploy
 
