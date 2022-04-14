e62fa01aeda7201d8e758c5d782127cb59f11bea
erenzhou <592267829@qq.com>
2022-04-14 15:49:19 +0800

    add mmrotate part

---
+++ mmdeploy/codebase/mmrotate/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .single_stage_rotated_detector import \
+    single_stage_rotated_detector__simple_test
+
+__all__ = ['single_stage_rotated_detector__simple_test']
