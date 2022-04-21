c8464805afb3a47887e303f788d37a1b03c10745
hanrui1sensetime <hanrui1@sensetime.com>
2022-04-21 06:48:38 -0700

    unified chunk

--- mmdeploy/codebase/mmpose/models/backbones/__init__.py
+++ mmdeploy/codebase/mmpose/models/backbones/__init__.py
@@ -1,11 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
-from .litehrnet import (conditional_channel_weighting_forward__ncnn,
-                        cross_resolution_weighting__forward,
-                        shuffle_unit__forward__ncnn, stem__forward__ncnn)
+from .litehrnet import (cross_resolution_weighting__forward,
+                        cross_resolution_weighting__forward__ncnn)
 
 __all__ = [
-    'conditional_channel_weighting_forward__ncnn',
-    'cross_resolution_weighting__forward', 'shuffle_unit__forward__ncnn',
-    'stem__forward__ncnn'
+    'cross_resolution_weighting__forward__ncnn',
+    'cross_resolution_weighting__forward',
 ]
