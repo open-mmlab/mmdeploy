86be3f10e181147948f2d5c2d820ab7067806202
hanrui1sensetime <hanrui1@sensetime.com>
2022-04-21 00:51:02 -0700

    fix yapf

--- mmdeploy/codebase/mmpose/models/backbones/__init__.py
+++ mmdeploy/codebase/mmpose/models/backbones/__init__.py
@@ -4,7 +4,8 @@ from .litehrnet import (conditional_channel_weighting_forward__ncnn,
                         cross_resolution_weighting__forward,
                         shuffle_unit__forward__ncnn, stem__forward__ncnn)
 
-__all__ = ['conditional_channel_weighting_forward__ncnn',
-           'cross_resolution_weighting__forward',
-           'shuffle_unit__forward__ncnn',
-           'stem__forward__ncnn']
+__all__ = [
+    'conditional_channel_weighting_forward__ncnn',
+    'cross_resolution_weighting__forward', 'shuffle_unit__forward__ncnn',
+    'stem__forward__ncnn'
+]
