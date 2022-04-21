c8ca2bcb2e2ff92aaffaef70c0a148b8d8a51117
erenzhou <592267829@qq.com>
2022-04-21 23:39:51 +0800

    fix

--- mmdeploy/codebase/mmrotate/core/post_processing/bbox_nms.py
+++ mmdeploy/codebase/mmrotate/core/post_processing/bbox_nms.py
@@ -5,60 +5,112 @@ from torch import Tensor
 from mmdeploy.mmcv.ops import ONNXNMSRotatedOp
 
 
-def multiclass_nms_rotated(multi_bboxes: Tensor,
-                           multi_scores: Tensor,
+def select_nms_index(scores: torch.Tensor,
+                     boxes: torch.Tensor,
+                     nms_index: torch.Tensor,
+                     batch_size: int,
+                     keep_top_k: int = -1):
+    """Transform NMS output.
+
+    Args:
+        scores (Tensor): The detection scores of shape
+            [N, num_classes, num_boxes].
+        boxes (Tensor): The bounding boxes of shape [N, num_boxes, 6].
+        nms_index (Tensor): NMS output of bounding boxes indexing.
+        batch_size (int): Batch size of the input image.
+        keep_top_k (int): Number of top K boxes to keep after nms.
+            Defaults to -1.
+
+    Returns:
+        tuple[Tensor, Tensor]: (dets, labels), `dets` of shape [N, num_det, 6]
+            and `labels` of shape [N, num_det].
+    """
+    batch_inds, cls_inds = nms_index[:, 0], nms_index[:, 1]
+    box_inds = nms_index[:, 2]
+
+    # index by nms output
+    scores = scores[batch_inds, cls_inds, box_inds].unsqueeze(1)
+    boxes = boxes[batch_inds, box_inds, ...]
+    dets = torch.cat([boxes, scores], dim=1)
+
+    # batch all
+    batched_dets = dets.unsqueeze(0).repeat(batch_size, 1, 1)
+    batch_template = torch.arange(
+        0, batch_size, dtype=batch_inds.dtype, device=batch_inds.device)
+    batched_dets = batched_dets.where(
+        (batch_inds == batch_template.unsqueeze(1)).unsqueeze(-1),
+        batched_dets.new_zeros(1))
+
+    batched_labels = cls_inds.unsqueeze(0).repeat(batch_size, 1)
+    batched_labels = batched_labels.where(
+        (batch_inds == batch_template.unsqueeze(1)),
+        batched_labels.new_ones(1) * -1)
+
+    N = batched_dets.shape[0]
+
+    # expand tensor to eliminate [0, ...] tensor
+    batched_dets = torch.cat((batched_dets, batched_dets.new_zeros((N, 1, 6))),
+                             1)
+    batched_labels = torch.cat((batched_labels, batched_labels.new_zeros(
+        (N, 1))), 1)
+
+    # sort
+    is_use_topk = keep_top_k > 0 and \
+        (torch.onnx.is_in_onnx_export() or keep_top_k < batched_dets.shape[1])
+    if is_use_topk:
+        _, topk_inds = batched_dets[:, :, -1].topk(keep_top_k, dim=1)
+    else:
+        _, topk_inds = batched_dets[:, :, -1].sort(dim=1, descending=True)
+    topk_batch_inds = torch.arange(
+        batch_size, dtype=topk_inds.dtype,
+        device=topk_inds.device).view(-1, 1).expand_as(topk_inds)
+    batched_dets = batched_dets[topk_batch_inds, topk_inds, ...]
+    batched_labels = batched_labels[topk_batch_inds, topk_inds, ...]
+
+    # slice and recover the tensor
+    return batched_dets, batched_labels
+
+
+def multiclass_nms_rotated(boxes: Tensor,
+                           scores: Tensor,
                            iou_threshold: float = 0.1,
                            score_threshold: float = 0.05,
+                           pre_top_k: int = -1,
                            keep_top_k: int = -1):
     """NMS for multi-class bboxes.
 
     Args:
-        multi_bboxes (torch.Tensor): shape (n, 5)
-        multi_scores (torch.Tensor): shape (n, #class), where the last column
-            contains scores of the background class, but this will be ignored.
+        boxes (Tensor): The bounding boxes of shape [N, num_boxes, 5].
+        scores (Tensor): The detection scores of shape
+            [N, num_boxes, num_classes].
+        iou_threshold (float): IOU threshold of nms. Defaults to 0.5.
         score_threshold (float): bbox threshold, bboxes with scores lower than
             it will not be considered.
-        keep_top_k (int, optional): if there are more than keep_top_k bboxes
-            after NMS, only top keep_top_k will be kept. Default to -1.
+        pre_top_k (int): Number of top K boxes to keep before nms.
+            Defaults to -1.
+        keep_top_k (int): Number of top K boxes to keep after nms.
+            Defaults to -1.
 
     Returns:
-        tuple (dets, labels, indices (optional)): tensors of shape (k, 5), \
-        (k), and (k). Dets are boxes with scores. Labels are 0-based.
+        tuple[Tensor, Tensor]: (dets, labels), `dets` of shape [N, num_det, 6]
+            and `labels` of shape [N, num_det].
     """
-    num_classes = multi_scores.size(1) - 1
-    # exclude background category
+    # iou_threshold = torch.tensor([iou_threshold], dtype=torch.float32)
+    # score_threshold = torch.tensor([score_threshold], dtype=torch.float32)
+    batch_size = scores.shape[0]
 
-    bboxes = multi_bboxes[:, None].expand(multi_scores.size(0), num_classes, 5)
-    scores = multi_scores[:, :-1]
+    if pre_top_k > 0:
+        max_scores, _ = scores.max(-1)
+        _, topk_inds = max_scores.topk(pre_top_k)
+        batch_inds = torch.arange(batch_size).view(
+            -1, 1).expand_as(topk_inds).long()
+        boxes = boxes[batch_inds, topk_inds, :]
+        scores = scores[batch_inds, topk_inds, :]
 
-    labels = torch.arange(num_classes, dtype=torch.long)
-    labels = labels.view(1, -1).expand_as(scores)
-    bboxes = bboxes.reshape(-1, 5)
-    scores = scores.reshape(-1)
-    labels = labels.reshape(-1)
+    scores = scores.permute(0, 2, 1)
+    selected_indices = ONNXNMSRotatedOp.apply(boxes, scores, iou_threshold)
 
-    # remove low scoring boxes
-    valid_mask = scores > score_threshold
-
-    inds = valid_mask.nonzero(as_tuple=False).squeeze(1)
-    bboxes, scores, labels = bboxes[inds], scores[inds], labels[inds]
-
-    if bboxes.numel() == 0:
-        dets = torch.cat([bboxes, scores[:, None]], -1)
-        return dets, labels
-
-    max_coordinate = bboxes.max()
-    offsets = labels.to(bboxes) * (max_coordinate + 1)
-    bboxes_for_nms = bboxes.clone()
-    bboxes_for_nms[:, :2] = bboxes_for_nms[:, :2] + offsets[:, None]
-    keep = ONNXNMSRotatedOp.apply(bboxes_for_nms, scores, iou_threshold)
-
-    if keep_top_k > 0:
-        keep = keep[:keep_top_k]
-
-    bboxes = bboxes[keep]
-    scores = scores[keep]
-    labels = labels[keep]
-    dets = torch.cat([bboxes, scores[:, None]], 1)
+    dets, labels = select_nms_index(
+        scores, boxes, selected_indices, batch_size, keep_top_k=keep_top_k)
 
     return dets, labels
