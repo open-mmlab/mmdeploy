1ffa0bab9d78abd0d67208b24d1e68661479558b
hanrui1sensetime <hanrui1@sensetime.com>
2022-04-22 05:01:51 -0700

    fix lint

--- mmdeploy/pytorch/ops/__init__.py
+++ mmdeploy/pytorch/ops/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .adaptive_avg_pool import (adaptive_avg_pool1d__default,
                                 adaptive_avg_pool2d__default,
-                                adaptive_avg_pool3d__default,
-                                adaptive_avg_pool2d__ncnn)
+                                adaptive_avg_pool2d__ncnn,
+                                adaptive_avg_pool3d__default)
 from .grid_sampler import grid_sampler__default
 from .hardsigmoid import hardsigmoid__default
 from .instance_norm import instance_norm__tensorrt
--- tests/test_pytorch/test_pytorch_ops.py
+++ tests/test_pytorch/test_pytorch_ops.py
@@ -1,5 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from sys import api_version
 import tempfile
 
 import onnx
