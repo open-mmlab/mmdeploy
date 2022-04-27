cecd1ecb477ffd7a7c002604e852c1da3678fc4a
lzhangzz <lzhang329@gmail.com>
2022-04-27 15:15:57 +0800

    [Fix] fix feature test for `std::source_location` (#416)
    * fix feature test for `std::source_location`
    
    * suppress msvc warnings
    
    * fix consistency

--- csrc/core/utils/source_location.h
+++ csrc/core/utils/source_location.h
@@ -3,17 +3,31 @@
 #ifndef MMDEPLOY_SRC_UTILS_SOURCE_LOCATION_H_
 #define MMDEPLOY_SRC_UTILS_SOURCE_LOCATION_H_
 
-#if __has_include(<source_location>) && !_MSC_VER
-#include <source_location>
-namespace mmdeploy {
-using SourceLocation = std::source_location;
-}
-#elif __has_include(<experimental/source_location>)
-#include <experimental/source_location>
-namespace mmdeploy {
-using SourceLocation = std::experimental::source_location;
-}
-#else
+// clang-format off
+#if __has_include(<source_location>) && (!_MSC_VER || __cplusplus >= 202002L)
+  #include <source_location>
+  #if __cpp_lib_source_location >= 201907L
+    #define MMDEPLOY_HAS_SOURCE_LOCATION 1
+    namespace mmdeploy {
+    using SourceLocation = std::source_location;
+    }
+  #endif
+#endif
+
+#ifndef MMDEPLOY_HAS_SOURCE_LOCATION
+  #if __has_include(<experimental/source_location>)
+    #include <experimental/source_location>
+    #if __cpp_lib_experimental_source_location >= 201505L
+      #define MMDEPLOY_HAS_SOURCE_LOCATION 1
+      namespace mmdeploy {
+      using SourceLocation = std::experimental::source_location;
+      }
+    #endif
+  #endif
+#endif
+// clang-format on
+
+#ifndef MMDEPLOY_HAS_SOURCE_LOCATION
 #include <cstdint>
 namespace mmdeploy {
 class SourceLocation {
