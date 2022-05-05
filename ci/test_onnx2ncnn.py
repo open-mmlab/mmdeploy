469fd3430eb25f0c45e04b33dd543d941837432f
tpoisonooo <khj.application@aliyun.com>
2022-05-05 22:55:07 +0800

    ci(onnx2ncnn): add ncnn backend

--- ci/test_onnx2ncnn.py
+++ ci/test_onnx2ncnn.py
@@ -11,16 +11,30 @@ CONFIGS = [
     (
         'mmclassification/configs/vision_transformer/vit-base-p32_ft-64xb64_in1k-384.py',  # noqa: E501
         'https://download.openmmlab.com/mmclassification/v0/vit/finetune/vit-base-p32_in21k-pre-3rdparty_ft-64xb64_in1k-384_20210928-9cea8599.pth',  # noqa: E501
-        'vit.onnx'),
+        'vit.onnx',
+        'https://pjlab-my.sharepoint.cn/personal/konghuanjun_pjlab_org_cn/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2Fkonghuanjun%5Fpjlab%5Forg%5Fcn%2FDocuments%2Fmmdeploy%2Donnx2ncnn%2Dci%2Fvit%2Eonnx'  # noqa: E501
+    ),  # noqa: E501
     (
         'mmclassification/configs/resnet/resnet50_8xb32_in1k.py',
         'https://download.openmmlab.com/mmclassification/v0/resnet/resnet50_8xb32_in1k_20210831-ea4938fc.pth',  # noqa: E501
-        'resnet.onnx'),
+        'resnet50.onnx',
+        'https://pjlab-my.sharepoint.cn/personal/konghuanjun_pjlab_org_cn/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2Fkonghuanjun%5Fpjlab%5Forg%5Fcn%2FDocuments%2Fmmdeploy%2Donnx2ncnn%2Dci%2Fresnet50%2Eonnx'  # noqa: E501
+    ),  # noqa: E501
+    (
+        'mmclassification/configs/resnet/resnet18_8xb32_in1k.py',
+        'https://download.openmmlab.com/mmclassification/v0/resnet/resnet18_8xb32_in1k_20210831-fbbb1da6.pth',  # noqa: E501
+        'resnet18.onnx',
+        'https://pjlab-my.sharepoint.cn/personal/konghuanjun_pjlab_org_cn/_layouts/15/download.aspx?UniqueId=07f4e5ae%2D84d0%2D43d1%2D9412%2D74300a0b9bb8'  # noqa: E501
+    ),  # noqa: E501
     (
         'mmclassification/configs/mobilenet_v2/mobilenet-v2_8xb32_in1k.py',
         'https://download.openmmlab.com/mmclassification/v0/mobilenet_v2/mobilenet_v2_batch256_imagenet_20200708-3b2dc3af.pth',  # noqa: E501
-        'mobilenet.onnx')
+        'mobilenet-v2.onnx',
+        'https://pjlab-my.sharepoint.cn/personal/konghuanjun_pjlab_org_cn/_layouts/15/download.aspx?SourceUrl=%2Fpersonal%2Fkonghuanjun%5Fpjlab%5Forg%5Fcn%2FDocuments%2Fmmdeploy%2Donnx2ncnn%2Dci%2Fmobilenet%2Dv2%2Eonnx'  # noqa: E501
+    )  # noqa: E501
 ]
+
+TEST_LIST = [2, 3]
 
 
 def parse_args():
@@ -60,11 +74,13 @@ def generate_onnx(args):
 
 
 def run(args):
-    for conf in CONFIGS:
-        convert_cmd = [
-            './onnx2ncnn',
-            os.path.join(args.out, conf[2]), 'onnx.param', 'onnx.bin'
-        ]
+    for idx in TEST_LIST:
+        download_url = CONFIGS[idx][3]
+        filename = CONFIGS[idx][2]
+        download_cmd = ['wget', download_url, '-o', filename]
+        subprocess.call(download_cmd)
+
+        convert_cmd = ['./onnx2ncnn', filename, 'onnx.param', 'onnx.bin']
         subprocess.call(convert_cmd)
 
 
