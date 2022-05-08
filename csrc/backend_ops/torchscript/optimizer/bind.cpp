88d6a215311bee95439e5308fdf206cf59cf27d5
grimoire <streetyao@live.com>
2022-05-08 18:32:15 +0800

    add flatten cls head

--- csrc/backend_ops/torchscript/optimizer/bind.cpp
+++ csrc/backend_ops/torchscript/optimizer/bind.cpp
@@ -4,8 +4,9 @@
 #include <string>
 
 #include "optimizer.h"
-#include "passes/merge_shape_concate.h"
-#include "passes/onnx_peephole.h"
+#include "passes/onnx/flatten_cls_head.h"
+#include "passes/onnx/merge_shape_concate.h"
+#include "passes/onnx/onnx_peephole.h"
 
 namespace mmdeploy {
 namespace torch_jit {
@@ -28,8 +29,10 @@ PYBIND11_MODULE(ts_optimizer, m) {
   m.def("optimize_for_backend", optimize_for_backend, py::arg("module"),
         py::arg("ir") = std::string("torchscript"),
         py::arg("backend") = std::string("torchscript"));
-  m.def("_jit_pass_merge_shape_concate", MergeShapeConcate, py::arg("graph"));
-  m.def("_jit_pass_onnx_peephole", ONNXPeephole, py::arg("graph"));
+  py::module_ onnx_module = m.def_submodule("onnx");
+  onnx_module.def("_jit_pass_merge_shape_concate", MergeShapeConcate, py::arg("graph"));
+  onnx_module.def("_jit_pass_onnx_peephole", ONNXPeephole, py::arg("graph"));
+  onnx_module.def("_jit_pass_flatten_cls_head", FlattenClsHead, py::arg("graph"));
 }
 
 }  // namespace torch_jit
--- mmdeploy/apis/onnx/passes/optimize_onnx.py
+++ mmdeploy/apis/onnx/passes/optimize_onnx.py
@@ -7,8 +7,9 @@ def optimize_onnx(graph, params_dict, torch_out):
     logger.info('Execute onnx optimize passes.')
     try:
         from mmdeploy.backend.torchscript import ts_optimizer
-        ts_optimizer._jit_pass_merge_shape_concate(graph)
-        ts_optimizer._jit_pass_onnx_peephole(graph)
+        ts_optimizer.onnx._jit_pass_merge_shape_concate(graph)
+        ts_optimizer.onnx._jit_pass_onnx_peephole(graph)
+        ts_optimizer.onnx._jit_pass_flatten_cls_head(graph)
     except Exception:
         pass
 
