e59484d0144eace9654557bfe50dbc584c4d09e5
HinGwenWoong <peterhuang0323@qq.com>
2022-05-08 17:50:48 +0800

    Add script

---
+++ tools/package_tools/test_prebuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Copyright (c) OpenMMLab. All rights reserved.
+# Script for test pre-build wheel
+
+build_config=$1
+
+# build wheel
+
+# using another env to pip install it
+
+# test convert
+
+# build sdk wheel
+
+# test sdk demo
+
+exit 0
