58c01d64bd85b31a7010e3ea76028a1864c89f9d
ChenXin <chenxin2@sensetime.com>
2022-05-16 11:13:05 +0800

    fix type & remove unused meta info

--- mmdeploy/codebase/mmrotate/deploy/rotated_detection_model.py
+++ mmdeploy/codebase/mmrotate/deploy/rotated_detection_model.py
@@ -163,13 +163,12 @@ class End2EndModel(BaseBackendModel):
 
 @__BACKEND_MODEL.register_module('sdk')
 class SDKEnd2EndModel(End2EndModel):
-    """SDK inference class, converts SDK output to mmcls format."""
+    """SDK inference class, converts SDK output to mmrotate format."""
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def forward(self, img: List[torch.Tensor],
-                img_metas: Sequence[Sequence[dict]], *args, **kwargs) -> list:
+    def forward(self, img: List[torch.Tensor], *args, **kwargs) -> list:
         """Run forward inference.
 
         Args:
