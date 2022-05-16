f2a3a2ee09be5674196f08d78d7767af14d2dcfe
tpoisonooo <khj.application@aliyun.com>
2022-05-16 14:53:34 +0800

    feat(tools/deploy.py): support quant

--- mmdeploy/apis/ncnn/__init__.py
+++ mmdeploy/apis/ncnn/__init__.py
@@ -6,4 +6,8 @@ __all__ = ['is_available', 'is_plugin_available']
 if is_available():
     from mmdeploy.backend.ncnn.onnx2ncnn import (get_output_model_file,
                                                  onnx2ncnn)
-    __all__ += ['onnx2ncnn', 'get_output_model_file']
+    from mmdeploy.backend.ncnn.quant import get_quant_model_file, ncnn2int8
+    __all__ += [
+        'onnx2ncnn', 'get_output_model_file', 'ncnn2int8',
+        'get_quant_model_file'
+    ]
