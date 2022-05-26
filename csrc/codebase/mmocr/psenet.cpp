a295595a8e1a404b0470610b854ae426b9067d28
zhangli <lzhang329@gmail.com>
2022-05-26 19:25:15 +0800

    add cpu impls for PSENet

--- csrc/codebase/mmocr/psenet.cpp
+++ csrc/codebase/mmocr/psenet.cpp
@@ -1,5 +1,7 @@
 // Copyright (c) OpenMMLab. All rights reserved.
 
+#include "codebase/mmocr/psenet.h"
+
 #include <algorithm>
 #include <opencv2/opencv.hpp>
 
@@ -9,7 +11,8 @@
 #include "core/serialization.h"
 #include "core/utils/device_utils.h"
 
-namespace mmdeploy::mmocr {
+namespace mmdeploy {
+namespace mmocr {
 
 void contour_expand(const cv::Mat_<uint8_t>& kernel_masks, const cv::Mat_<int32_t>& kernel_label,
                     const cv::Mat_<float>& score, int min_kernel_area, int kernel_num,
@@ -18,14 +21,19 @@ void contour_expand(const cv::Mat_<uint8_t>& kernel_masks, const cv::Mat_<int32_
 
 class PSEHead : public MMOCR {
  public:
-  explicit PSEHead(const Value& config) : MMOCR(config) {}
+  explicit PSEHead(const Value& config) : MMOCR(config) {
+    auto platform = Platform(device_.platform_id()).GetPlatformName();
+    auto creator = Registry<PseHeadImpl>::Get().GetCreator("cpu");
+    if (!creator) {
+      MMDEPLOY_ERROR("PANHead: implementation for platform \"{}\" not found", platform);
+      throw_exception(eEntryNotFound);
+    }
+    impl_ = creator->Create(nullptr);
+    impl_->Init(stream_);
+  }
 
   Result<Value> operator()(const Value& _data, const Value& _pred) noexcept {
-    Device cpu_device{"cpu"};
-    OUTCOME_TRY(auto _preds,
-                MakeAvailableOnDevice(_pred["output"].get<Tensor>(), cpu_device, stream_));
-    OUTCOME_TRY(stream_.Wait());
-
+    auto _preds = _pred["output"].get<Tensor>();
     if (_preds.shape().size() != 4 || _preds.shape(0) != 1 ||
         _preds.data_type() != DataType::kFLOAT) {
       MMDEPLOY_ERROR("unsupported `output` tensor, shape: {}, dtype: {}", _preds.shape(),
@@ -36,30 +44,19 @@ class PSEHead : public MMOCR {
     // drop batch dimension
     _preds.Squeeze();
 
-    auto channels = static_cast<int>(_preds.shape(0));
-    auto height = static_cast<int>(_preds.shape(1));
-    auto width = static_cast<int>(_preds.shape(2));
+    cv::Mat_<uint8_t> masks;
+    cv::Mat_<int> kernel_labels;
+    cv::Mat_<float> score;
+    int region_num = 0;
 
-    cv::Mat_<float> preds(_preds.shape(0), height * width, _preds.data<float>());
-    sigmoid(preds);
-
-    cv::Mat_<float> score = preds.row(0).reshape(1, height);
-
-    cv::Mat_<uint8_t> masks = preds > min_kernel_confidence_;
-
-    for (int i = 1; i < channels; ++i) {
-      masks.row(i) &= masks.row(0);
-    }
-
-    cv::Mat_<int32_t> kernel_labels;
-    auto region_num = cv::connectedComponents(masks.row(channels - 1).reshape(1, height),
-                                              kernel_labels, 4, CV_32S);
+    OUTCOME_TRY(
+        impl_->Process(_preds, min_kernel_confidence_, score, masks, kernel_labels, region_num));
 
     std::vector<int> text_areas;
     std::vector<float> text_scores;
     std::vector<std::vector<int>> text_points;
-    contour_expand(masks.rowRange(1, channels), kernel_labels, score, min_kernel_area_, region_num,
-                   text_areas, text_scores, text_points);
+    contour_expand(masks.rowRange(1, masks.rows), kernel_labels, score, min_kernel_area_,
+                   region_num, text_areas, text_scores, text_points);
 
     auto scale_w = _data["img_metas"]["scale_factor"][0].get<float>();
     auto scale_h = _data["img_metas"]["scale_factor"][1].get<float>();
@@ -96,11 +93,6 @@ class PSEHead : public MMOCR {
     return to_value(output);
   }
 
-  static void sigmoid(cv::Mat_<float>& score) {
-    cv::exp(-score, score);
-    score = 1 / (1 + score);
-  }
-
   static bool filter_instance(float area, float confidence, float min_area, float min_confidence) {
     return area < min_area || confidence < min_confidence;
   }
@@ -111,8 +103,14 @@ class PSEHead : public MMOCR {
   float min_text_area_{16};
   bool rescale_{true};
   float downsample_ratio_{.25f};
+
+  std::unique_ptr<PseHeadImpl> impl_;
 };
 
 REGISTER_CODEBASE_COMPONENT(MMOCR, PSEHead);
 
-}  // namespace mmdeploy::mmocr
+}  // namespace mmocr
+
+MMDEPLOY_DEFINE_REGISTRY(mmocr::PseHeadImpl);
+
+}  // namespace mmdeploy
