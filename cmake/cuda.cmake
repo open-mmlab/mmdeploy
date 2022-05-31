bbf3b6d48c5dfeaa8af93cae4c054e774bc000ba
zhangli <lzhang329@gmail.com>
2022-05-31 22:25:12 +0800

    simplify cuda deps

--- cmake/cuda.cmake
+++ cmake/cuda.cmake
@@ -6,14 +6,14 @@ if (${CMAKE_VERSION} VERSION_GREATER_EQUAL "3.18.0")
     cmake_policy(SET CMP0104 OLD)
 endif ()
 
-if (MSVC)
+if (MSVC OR (NOT DEFINED CMAKE_CUDA_RUNTIME_LIBRARY))
     # use shared, on windows, python api can't build with static lib.
     set(CMAKE_CUDA_RUNTIME_LIBRARY Shared)
+    set(CUDA_USE_STATIC_CUDA_RUNTIME OFF)
 endif ()
 
 # nvcc compiler settings
 find_package(CUDA REQUIRED)
-#message(STATUS "CUDA VERSION: ${CUDA_VERSION_STRING}")
 
 if (MSVC)
     set(CMAKE_CUDA_COMPILER ${CUDA_TOOLKIT_ROOT_DIR}/bin/nvcc.exe)
