db9a01d9e39779bb13a837bfeeacb3c1a676350d
zhangli <lzhang329@gmail.com>
2022-06-01 13:05:40 +0800

    workaround for cmake<3.17

---
+++ cmake/post-install.cmake
@@ -0,0 +1,10 @@
+
+
+set(_TARGETS_PATH ${CMAKE_INSTALL_PREFIX}/lib/cmake/MMDeploy/MMDeployTargets.cmake)
+
+file(READ ${_TARGETS_PATH} _MMDEPLOY_TARGETS)
+
+string(REGEX REPLACE "::@<0x[a-z0-9]+>" ""
+        _MMDEPLOY_TARGETS_FIXED "${_MMDEPLOY_TARGETS}")
+
+file(WRITE ${_TARGETS_PATH} "${_MMDEPLOY_TARGETS_FIXED}")
