aeb3fff3c6abddea758152244f4696aa1457bcf9
hanrui1sensetime <hanrui1@sensetime.com>
2022-06-05 17:58:39 +0800

    fix conflict classifier.cpp

--- csrc/apis/java/classifier.cpp
+++ csrc/apis/java/classifier.cpp
@@ -2,22 +2,18 @@
 
 #include "classifier.h"
 
-#include "common.h"
-
-//#include <platform.h>
-//#include <benchmark.h>
+#include <android/log.h>
 #include <jni.h>
+#include <stdio.h>
 #include <unistd.h>
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include "common.h"
 
-namespace MMDeployJava {
+namespace MMDeploy {
 
 extern "C" {
-JNIEXPORT jobject JNICALL mmdeployClassifierCreateByPath(JNIEnv* env, jobject thiz,
-                                                         jstring modelPath, jstring deviceName,
-                                                         jint deviceID, jobject handlePointer) {
+JNIEXPORT jobject JNICALL CreateByPath(JNIEnv* env, jobject thiz, jstring modelPath,
+                                       jstring deviceName, jint deviceID, jobject handlePointer) {
   // handlePointer saves the value of mm_handle_t, because mm_handle_t is already a handle pointer.
   int status{};
   const char* model_path = env->GetStringUTFChars(modelPath, 0);
@@ -30,17 +26,17 @@ JNIEXPORT jobject JNICALL mmdeployClassifierCreateByPath(JNIEnv* env, jobject th
   int device_id = (int)deviceID;
   status = mmdeploy_classifier_create_by_path(model_path, device_name, device_id, &classifier);
   if (status != MM_SUCCESS) {
-    fprintf(stderr, "failed to create classifier, code: %d\n", (int)status);
+    __android_log_print(ANDROID_LOG_ERROR, "jni", "failed to create classifier, code: %d\n",
+                        (int)status);
     return env->NewObject(clazz, initMethod, env->NewStringUTF("mm_handle_t"), (long)0);
   }
   jobject result =
       env->NewObject(clazz, initMethod, env->NewStringUTF("mm_handle_t"), (long)classifier);
   return result;
 }
-JNIEXPORT jboolean JNICALL mmdeployClassifierApply(JNIEnv* env, jobject thiz, jobject handlePointer,
-                                                   jobject matsPointer, jint matCount,
-                                                   jobject resultsPointer,
-                                                   jobject resultCountPointer) {
+JNIEXPORT jboolean JNICALL Apply(JNIEnv* env, jobject thiz, jobject handlePointer,
+                                 jobject matsPointer, jint matCount, jobject resultsPointer,
+                                 jobject resultCountPointer) {
   int status{};
   jclass handle_clazz = env->GetObjectClass(handlePointer);
   jfieldID id_handle_address = env->GetFieldID(handle_clazz, "address", "J");
@@ -69,9 +65,8 @@ JNIEXPORT jboolean JNICALL mmdeployClassifierApply(JNIEnv* env, jobject thiz, jo
   env->SetLongField(resultCountPointer, id_result_count_address, (jlong)count_apply);
   return JNI_TRUE;
 }
-JNIEXPORT void JNICALL mmdeployClassifierReleaseResult(JNIEnv* env, jobject thiz,
-                                                       jobject resultsPointer,
-                                                       jobject resultCountPointer, jint count) {
+JNIEXPORT void JNICALL ReleaseResult(JNIEnv* env, jobject thiz, jobject resultsPointer,
+                                     jobject resultCountPointer, jint count) {
   jclass results_clazz = env->GetObjectClass(resultsPointer);
   jfieldID id_results_address = env->GetFieldID(results_clazz, "address", "J");
   jlong presults = env->GetLongField(resultsPointer, id_results_address);
@@ -83,7 +78,7 @@ JNIEXPORT void JNICALL mmdeployClassifierReleaseResult(JNIEnv* env, jobject thiz
   mmdeploy_classifier_release_result(res, rescount, (int)count);
 }
 
-JNIEXPORT void JNICALL mmdeployClassifierDestroy(JNIEnv* env, jobject thiz, jobject handlePointer) {
+JNIEXPORT void JNICALL Destroy(JNIEnv* env, jobject thiz, jobject handlePointer) {
   jclass handle_clazz = env->GetObjectClass(handlePointer);
   jfieldID id_handle_address = env->GetFieldID(handle_clazz, "address", "J");
   long phandle = (long)env->GetLongField(handlePointer, id_handle_address);
@@ -92,28 +87,27 @@ JNIEXPORT void JNICALL mmdeployClassifierDestroy(JNIEnv* env, jobject thiz, jobj
 }
 
 static JNINativeMethod method[] = {
-    {"mmdeployClassifierCreateByPath",
-     "(Ljava/lang/String;Ljava/lang/String;ILcom/openmmlab/mmdeployxclassifier/"
-     "PointerWrapper;)Lcom/openmmlab/mmdeployxclassifier/PointerWrapper;",
-     (bool*)mmdeployClassifierCreateByPath},
-    {"mmdeployClassifierApply",
-     "(Lcom/openmmlab/mmdeployxclassifier/PointerWrapper;Lcom/openmmlab/mmdeployxclassifier/"
-     "PointerWrapper;ILcom/openmmlab/mmdeployxclassifier/PointerWrapper;Lcom/openmmlab/"
-     "mmdeployxclassifier/PointerWrapper;)Z",
-     (bool*)mmdeployClassifierApply},
-    {"mmdeployClassifierReleaseResult",
-     "(Lcom/openmmlab/mmdeployxclassifier/PointerWrapper;Lcom/openmmlab/mmdeployxclassifier/"
+    {"CreateByPath",
+     "(Ljava/lang/String;Ljava/lang/String;ILcn/org/openmmlab/mmdeploy/"
+     "PointerWrapper;)Lcn/org/openmmlab/mmdeploy/PointerWrapper;",
+     (bool*)CreateByPath},
+    {"Apply",
+     "(Lcn/org/openmmlab/mmdeploy/PointerWrapper;Lcn/org/openmmlab/mmdeploy/"
+     "PointerWrapper;ILcn/org/openmmlab/mmdeploy/PointerWrapper;Lcn/org/openmmlab/"
+     "mmdeploy/PointerWrapper;)Z",
+     (bool*)Apply},
+    {"ReleaseResult",
+     "(Lcn/org/openmmlab/mmdeploy/PointerWrapper;Lcn/org/openmmlab/mmdeploy/"
      "PointerWrapper;I)V",
-     (void*)mmdeployClassifierReleaseResult},
-    {"mmdeployClassifierDestroy", "(Lcom/openmmlab/mmdeployxclassifier/PointerWrapper;)V",
-     (void*)mmdeployClassifierDestroy}};
+     (void*)ReleaseResult},
+    {"Destroy", "(Lcn/org/openmmlab/mmdeploy/PointerWrapper;)V", (void*)Destroy}};
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   JNIEnv* env = NULL;
   jint result = -1;
   if (vm->GetEnv((void**)&env, JNI_VERSION_1_6) != JNI_OK) {
     return result;
   }
-  jclass jClassName = env->FindClass("com/openmmlab/mmdeployxclassifier/MMDeployClassifier");
+  jclass jClassName = env->FindClass("cn/org/openmmlab/mmdeploy/Classifier");
   jint ret = env->RegisterNatives(jClassName, method, sizeof(method) / sizeof(JNINativeMethod));
   if (ret != JNI_OK) {
     return -1;
@@ -121,4 +115,4 @@ JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   return JNI_VERSION_1_6;
 }
 }
-}  // namespace MMDeployJava
+}  // namespace MMDeploy
