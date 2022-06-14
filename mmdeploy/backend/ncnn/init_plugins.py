de6a58f46580a31f3f85da55f39071e442b3407b
tpoisonooo <khj.application@aliyun.com>
2022-06-14 16:45:21 +0800

    fix(CI): revert get onnx2ncnn path

--- mmdeploy/backend/ncnn/init_plugins.py
+++ mmdeploy/backend/ncnn/init_plugins.py
@@ -24,10 +24,7 @@ def get_onnx2ncnn_path() -> str:
         str: A path of onnx2ncnn tool.
     """
     candidates = ['./onnx2ncnn', './onnx2ncnn.exe']
-    onnx2ncnn = get_file_path(os.path.dirname(__file__), candidates)
-    if not os.path.exists(onnx2ncnn):
-        onnx2ncnn = shutil.which('onnx2ncnn')
-    return onnx2ncnn
+    return get_file_path(os.path.dirname(__file__), candidates)
 
 
 def get_ncnn2int8_path() -> str:
@@ -36,8 +33,8 @@ def get_ncnn2int8_path() -> str:
     Returns:
         str: A path of ncnn2int8 tool.
     """
-    ncnn2int8 = shutil.which('ncnn2int8')
-    if ncnn2int8 is None or not os.path.exists(ncnn2int8):
+    ncnn2int8_path = shutil.which('ncnn2int8')
+    if ncnn2int8_path is None:
         raise Exception(
             'Cannot find ncnn2int8, try `export PATH=/path/to/ncnn2int8`')
-    return ncnn2int8
+    return ncnn2int8_path
