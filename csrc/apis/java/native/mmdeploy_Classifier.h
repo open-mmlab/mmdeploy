875b167124ddab754c0262f18144d9018feace8f
hanrui1sensetime <hanrui1@sensetime.com>
2022-06-15 16:22:19 +0800

    remove mmrotate ncnn java api and demo

--- csrc/apis/java/native/mmdeploy_Classifier.h
+++ csrc/apis/java/native/mmdeploy_Classifier.h
@@ -12,22 +12,24 @@ extern "C" {
  * Method:    create
  * Signature: (Ljava/lang/String;Ljava/lang/String;I)J
  */
-JNIEXPORT jlong JNICALL Java_mmdeploy_Classifier_create(JNIEnv *, jobject, jstring, jstring, jint);
+JNIEXPORT jlong JNICALL Java_mmdeploy_Classifier_create
+  (JNIEnv *, jobject, jstring, jstring, jint);
 
 /*
  * Class:     mmdeploy_Classifier
  * Method:    destroy
  * Signature: (J)V
  */
-JNIEXPORT void JNICALL Java_mmdeploy_Classifier_destroy(JNIEnv *, jobject, jlong);
+JNIEXPORT void JNICALL Java_mmdeploy_Classifier_destroy
+  (JNIEnv *, jobject, jlong);
 
 /*
  * Class:     mmdeploy_Classifier
  * Method:    apply
  * Signature: (J[Lmmdeploy/Mat;[I)[Lmmdeploy/Classifier/Result;
  */
-JNIEXPORT jobjectArray JNICALL Java_mmdeploy_Classifier_apply(JNIEnv *, jobject, jlong,
-                                                              jobjectArray, jintArray);
+JNIEXPORT jobjectArray JNICALL Java_mmdeploy_Classifier_apply
+  (JNIEnv *, jobject, jlong, jobjectArray, jintArray);
 
 #ifdef __cplusplus
 }
