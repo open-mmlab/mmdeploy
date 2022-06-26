11f2a38d5ae1afc9adf94ee795e487c902baab9a
Daigo Hirooka <daigo.hirooka@gmail.com>
2022-06-26 18:26:05 +0900

    add panoptic detection model

--- mmdeploy/codebase/mmdet/models/detectors/__init__.py
+++ mmdeploy/codebase/mmdet/models/detectors/__init__.py
@@ -4,9 +4,12 @@ from .rpn import rpn__simple_test
 from .single_stage import single_stage_detector__simple_test
 from .two_stage import (two_stage_detector__extract_feat,
                         two_stage_detector__simple_test)
+from .panoptic_two_stage_segmentor import (
+    two_stage_panoptic_segmentor__simple_test)
 
 __all__ = [
     'base_detector__forward', 'rpn__simple_test',
     'single_stage_detector__simple_test', 'two_stage_detector__extract_feat',
-    'two_stage_detector__simple_test'
+    'two_stage_detector__simple_test',
+    'two_stage_panoptic_segmentor__simple_test'
 ]
