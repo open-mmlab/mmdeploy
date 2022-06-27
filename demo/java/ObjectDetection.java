38f76dd3b2e8ab5329b14ffef7ab38ab155e79f6
hanrui1sensetime <hanrui1@sensetime.com>
2022-06-27 11:59:46 +0800

    add visualize

--- demo/java/ObjectDetection.java
+++ demo/java/ObjectDetection.java
@@ -5,22 +5,15 @@ import mmdeploy.Mat;
 import mmdeploy.Utils;
 
 import javax.imageio.ImageIO;
+
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.awt.Graphics;
 import java.io.File;
 import java.io.IOException;
 
 public class ObjectDetection {
-
-    private static Mat loadImage(String path) {
-        try {
-            return Utils.loadImage(path);
-        }
-        catch (Exception e) {
-            System.out.println("exception: " + e.getMessage());
-        }
-        return null;
-    }
 
     public static void main(String[] args) {
         // Parse arguments
@@ -37,11 +30,13 @@ public class ObjectDetection {
         try {
             detector = new Detector(modelPath, deviceName, 0);
             // load image
-            Mat img = loadImage(imagePath);
+            BufferedImage srcImg = ImageIO.read(new File(imagePath));
+            Mat img = Utils.bufferedImg2Mat(srcImg);
 
             // apply detector
             Detector.Result[] result = detector.apply(img);
             // print results
+            Graphics ghandle = srcImg.createGraphics();
             for (int i = 0; i < result.length; i++) {
                 Detector.Result value = result[i];
                 System.out.printf("box %d, left=%.2f, top=%.2f, right=%.2f, bottom=%.2f, label=%d, score=%.4f\n",
@@ -57,7 +52,11 @@ public class ObjectDetection {
                 if (value.mask != null) {
                     System.out.printf("mask %d, height=%d, width=%d\n", i, value.mask.shape[0], value.mask.shape[1]);
                 }
+                ghandle.setColor(new Color(0, 255, 0));
+                ghandle.drawRect((int)value.bbox.left, (int)value.bbox.top, (int)value.bbox.right - (int)value.bbox.left + 1, (int)value.bbox.bottom - (int)value.bbox.top + 1);
             }
+            ghandle.dispose();
+            ImageIO.write(srcImg, "png", new File("output_detection.png"));
         } catch (Exception e) {
             System.out.println("exception: " + e.getMessage());
         } finally {
