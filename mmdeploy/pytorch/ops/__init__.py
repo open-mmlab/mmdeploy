ad30799b4291735be7d589603a934f336eaa4322
AllentDan <AllentDan@yeah.net>
2022-06-28 17:53:17 +0800

    add roll export

--- mmdeploy/pytorch/ops/__init__.py
+++ mmdeploy/pytorch/ops/__init__.py
@@ -11,6 +11,7 @@ from .layer_norm import layer_norm__ncnn
 from .linear import linear__ncnn
 from .lstm import generic_rnn__ncnn
 from .pad import _prepare_onnx_paddings__tensorrt
+from .roll import roll_default
 from .squeeze import squeeze__default
 
 __all__ = [
@@ -18,5 +19,6 @@ __all__ = [
     'adaptive_avg_pool3d__default', 'grid_sampler__default',
     'hardsigmoid__default', 'instance_norm__tensorrt', 'generic_rnn__ncnn',
     'squeeze__default', 'adaptive_avg_pool2d__ncnn', 'gelu__ncnn',
-    'layer_norm__ncnn', 'linear__ncnn', '_prepare_onnx_paddings__tensorrt'
+    'layer_norm__ncnn', 'linear__ncnn', '_prepare_onnx_paddings__tensorrt',
+    'roll_default'
 ]
