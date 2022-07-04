b8796cec07d96047883d73f79a69368247909af6
hanrui1sensetime <hanrui1@sensetime.com>
2022-07-04 17:15:28 +0800

    fix ut of distance2bbox

--- tests/test_codebase/test_mmdet/test_mmdet_core.py
+++ tests/test_codebase/test_mmdet/test_mmdet_core.py
@@ -149,12 +149,41 @@ def test_tblr2bbox(backend_type: Backend):
         assert rewrite_outputs is not None
 
 
-def test_distance2bbox():
-    from mmdeploy.codebase.mmdet.core import distance2bbox
+@pytest.mark.parametrize('backend_type', [Backend.ONNXRUNTIME])
+def test_distance2bbox(backend_type: Backend):
+    check_backend(backend_type)
+    deploy_cfg = mmcv.Config(
+        dict(
+            onnx_config=dict(output_names=None, input_shape=None),
+            backend_config=dict(type=backend_type.value, model_inputs=None),
+            codebase_config=dict(type='mmdet', task='ObjectDetection')))
+
+    # wrap function to enable rewrite
+    def distance2bbox(*args, **kwargs):
+        import mmdet.core.bbox.transforms
+        return mmdet.core.bbox.transforms.distance2bbox(*args, **kwargs)
+
     points = torch.rand(3, 2)
     distance = torch.rand(3, 4)
-    bbox = distance2bbox(points, distance)
-    assert bbox.shape == torch.Size([3, 4])
+    original_outputs = distance2bbox(points, distance)
+
+    # wrap function to nn.Module, enable torch.onnx.export
+    wrapped_func = WrapFunction(distance2bbox)
+    rewrite_outputs, is_backend_output = get_rewrite_outputs(
+        wrapped_func,
+        model_inputs={
+            'points': points,
+            'distance': distance
+        },
+        deploy_cfg=deploy_cfg)
+
+    if is_backend_output:
+        model_output = original_outputs.squeeze().cpu().numpy()
+        rewrite_output = rewrite_outputs[0].squeeze()
+        assert np.allclose(
+            model_output, rewrite_output, rtol=1e-03, atol=1e-05)
+    else:
+        assert rewrite_outputs is not None
 
 
 @backend_checker(Backend.ONNXRUNTIME)
