fd15b3a7b2cff7b84b8ea0ee2e57044de8c8a2fa
nijkah <nijkah@gmail.com>
2022-07-06 19:45:54 +0900

    [Feature] Support RoITransRoIHead

--- mmdeploy/codebase/mmrotate/models/__init__.py
+++ mmdeploy/codebase/mmrotate/models/__init__.py
@@ -3,6 +3,7 @@ from .oriented_standard_roi_head import (
     oriented_standard_roi_head__simple_test,
     oriented_standard_roi_head__simple_test_bboxes)
 from .roi_extractors import rotated_single_roi_extractor__forward__tensorrt
+from .roi_trans_roi_head import roi_trans_roi_head__simple_test
 from .rotated_anchor_head import rotated_anchor_head__get_bbox
 from .rotated_bbox_head import rotated_bbox_head__get_bboxes
 from .rotated_rpn_head import rotated_rpn_head__get_bboxes
@@ -14,6 +15,6 @@ __all__ = [
     'rotated_anchor_head__get_bbox', 'rotated_rpn_head__get_bboxes',
     'oriented_standard_roi_head__simple_test',
     'oriented_standard_roi_head__simple_test_bboxes',
-    'rotated_bbox_head__get_bboxes',
+    'roi_trans_roi_head__simple_test', 'rotated_bbox_head__get_bboxes',
     'rotated_single_roi_extractor__forward__tensorrt'
 ]
