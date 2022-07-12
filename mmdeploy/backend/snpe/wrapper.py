1f20dfb49b4749ce5a71721815b98abf0b5ffdfd
tpoisonooo <khj.application@aliyun.com>
2022-07-12 12:33:52 +0800

    improvement(backend/snpe): add grpc interceptor for resend request

--- mmdeploy/backend/snpe/wrapper.py
+++ mmdeploy/backend/snpe/wrapper.py
@@ -6,18 +6,87 @@ from random import randint
 from typing import Dict, Optional, Sequence, Tuple
 
 import grpc
-# import mmdeploy.backend.snpe.inference_pb2
-# import mmdeploy.backend.snpe.inference_pb2_grpc
 import inference_pb2
 import inference_pb2_grpc
 import numpy as np
 import torch
+import abc
 
 from mmdeploy.backend.snpe.onnx2dlc import get_env_key
 from mmdeploy.utils import Backend, get_root_logger
 from mmdeploy.utils.timer import TimeCounter
 from ..base import BACKEND_WRAPPER, BaseWrapper
 
+# add interceptor to sleep and retry request
+# https://github.com/grpc/grpc/issues/19514
+class SleepingPolicy(abc.ABC):
+    @abc.abstractmethod
+    def sleep(self, try_i: int):
+        """
+        How long to sleep in milliseconds.
+        :param try_i: the number of retry (starting from zero)
+        """
+        assert try_i >= 0
+
+class ExponentialBackoff(SleepingPolicy):
+    def __init__(self, *, init_backoff_ms: int, max_backoff_ms: int, multiplier: int):
+        self.init_backoff = randint(0, init_backoff_ms)
+        self.max_backoff = max_backoff_ms
+        self.multiplier = multiplier
+
+    def sleep(self, try_i: int):
+        sleep_range = min(
+            self.init_backoff * self.multiplier ** try_i, self.max_backoff
+        )
+        sleep_ms = randint(0, sleep_range)
+        logger = get_root_logger()
+        logger.debug(f"Sleeping for {sleep_ms}")
+        time.sleep(sleep_ms / 1000)
+
+class RetryOnRpcErrorClientInterceptor(
+    grpc.UnaryUnaryClientInterceptor, grpc.StreamUnaryClientInterceptor
+):
+    def __init__(
+        self,
+        *,
+        max_attempts: int,
+        sleeping_policy: SleepingPolicy,
+        status_for_retry: Optional[Tuple[grpc.StatusCode]] = None,
+    ):
+        self.max_attempts = max_attempts
+        self.sleeping_policy = sleeping_policy
+        self.status_for_retry = status_for_retry
+
+    def _intercept_call(self, continuation, client_call_details, request_or_iterator):
+
+        for try_i in range(self.max_attempts):
+            response = continuation(client_call_details, request_or_iterator)
+
+            if isinstance(response, grpc.RpcError):
+
+                # Return if it was last attempt
+                if try_i == (self.max_attempts - 1):
+                    return response
+
+                # If status code is not in retryable status codes
+                if (
+                    self.status_for_retry
+                    and response.code() not in self.status_for_retry
+                ):
+                    return response
+
+                self.sleeping_policy.sleep(try_i)
+            else:
+                return response
+
+    def intercept_unary_unary(self, continuation, client_call_details, request):
+        return self._intercept_call(continuation, client_call_details, request)
+
+    def intercept_stream_unary(
+        self, continuation, client_call_details, request_iterator
+    ):
+        return self._intercept_call(continuation, client_call_details, request_iterator)
+    
 
 # add interceptor to sleep and retry request
 # https://github.com/grpc/grpc/issues/19514
@@ -122,6 +191,8 @@ class SNPEWrapper(BaseWrapper):
                  **kwargs):
 
         logger = get_root_logger()
+        
+        interceptors = (RetryOnRpcErrorClientInterceptor(max_attempts=4,sleeping_policy=ExponentialBackoff(init_backoff_ms=100, max_backoff_ms=1600, multiplier=2),status_for_retry=(grpc.StatusCode.UNAVAILABLE,),),)
 
         interceptors = (RetryOnRpcErrorClientInterceptor(
             max_attempts=4,
@@ -138,12 +209,18 @@ class SNPEWrapper(BaseWrapper):
         if uri is None:
             logger.error('URI not set')
 
+        # self.channel = grpc.insecure_channel(
+        #     uri,
+        #     options=(('grpc.GRPC_ARG_KEEPALIVE_TIME_MS',
+        #               2000), ('grpc.max_send_message_length', MAX_SIZE),
+        #              ('grpc.keepalive_permit_without_calls', 1)))
+        
         weights = bytes()
         filesize = os.stat(dlc_file).st_size
 
         logger.info(f'reading local model file {dlc_file}')
-        # with open(dlc_file, 'rb') as f:
-        #     weights = f.read(filesize)
+        with open(dlc_file, 'rb') as f:
+            weights = f.read(filesize)
 
         self.stub = inference_pb2_grpc.InferenceStub(
             grpc.intercept_channel(grpc.insecure_channel(uri), *interceptors))
