ebba5ff6c92b5fa6cee0a31eda1f4e0d148dad05
AllentDan <41138331+AllentDan@users.noreply.github.com>
2022-07-20 14:18:48 +0800

    [Fix] fix yolox point_generator (#758)
    * fix yolox point_generator
    
    * add a UT
    
    * resolve comments
    
    * fix comment lines

--- mmdeploy/codebase/mmdet/core/__init__.py
+++ mmdeploy/codebase/mmdet/core/__init__.py
@@ -2,4 +2,5 @@
 from .anchor import *  # noqa: F401,F403
 from .bbox import *  # noqa: F401,F403
 from .ops import *  # noqa: F401,F403
+from .point_generator import *  # noqa: F401,F403
 from .post_processing import *  # noqa: F401,F403
