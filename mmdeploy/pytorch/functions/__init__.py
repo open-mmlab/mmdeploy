9be1373de05fb37170717ffb9c96428f71c79791
AllentDan <AllentDan@yeah.net>
2022-07-22 11:58:17 +0800

    triu -> triu_default

--- mmdeploy/pytorch/functions/__init__.py
+++ mmdeploy/pytorch/functions/__init__.py
@@ -10,12 +10,12 @@ from .normalize import normalize__ncnn
 from .repeat import tensor__repeat__tensorrt
 from .size import tensor__size__ncnn
 from .topk import topk__dynamic, topk__tensorrt
-from .triu import triu
+from .triu import triu__default
 
 __all__ = [
     'tensor__getattribute__ncnn', 'group_norm__ncnn', 'interpolate__ncnn',
     'interpolate__tensorrt', 'linear__ncnn', 'tensor__repeat__tensorrt',
     'tensor__size__ncnn', 'topk__dynamic', 'topk__tensorrt', 'chunk__ncnn',
-    'triu', 'atan2__default', 'normalize__ncnn', 'expand__ncnn',
+    'triu__default', 'atan2__default', 'normalize__ncnn', 'expand__ncnn',
     'chunk__torchscript'
 ]
