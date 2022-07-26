ab7b6d3b5bd29a26bbd92ec128c0c9c281c09f91
hanrui1sensetime <hanrui1@sensetime.com>
2022-07-26 15:42:23 +0800

    fix for mmdet ut

--- mmdeploy/codebase/__init__.py
+++ mmdeploy/codebase/__init__.py
@@ -34,6 +34,8 @@ def import_codebase(codebase: Codebase):
         importlib.import_module(f'{lib}.models')
         importlib.import_module(f'{lib}.core')
         importlib.import_module(f'{lib}.datasets')
+        # for ut metrics registeration
+        importlib.import_module(f'{lib}.metrics')
 
 
 __all__ = ['MMCodebase', 'BaseTask', 'get_codebase_class']
