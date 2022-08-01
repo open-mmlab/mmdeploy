24b68bc26f490596160e4e2987b90938f6a73619
hanrui1sensetime <hanrui1@sensetime.com>
2022-08-01 19:35:09 +0800

    remove unused pad

--- mmdeploy/pytorch/ops/__init__.py
+++ mmdeploy/pytorch/ops/__init__.py
@@ -10,7 +10,6 @@ from .instance_norm import instance_norm__tensorrt
 from .layer_norm import layer_norm__ncnn
 from .linear import linear__ncnn
 from .lstm import generic_rnn__ncnn
-from .pad import _prepare_onnx_paddings__tensorrt
 from .roll import roll_default
 from .squeeze import squeeze__default
 
@@ -19,6 +18,5 @@ __all__ = [
     'adaptive_avg_pool3d__default', 'grid_sampler__default',
     'hardsigmoid__default', 'instance_norm__tensorrt', 'generic_rnn__ncnn',
     'squeeze__default', 'adaptive_avg_pool2d__ncnn', 'gelu__ncnn',
-    'layer_norm__ncnn', 'linear__ncnn', 'roll_default',
-    '_prepare_onnx_paddings__tensorrt'
+    'layer_norm__ncnn', 'linear__ncnn', 'roll_default'
 ]
