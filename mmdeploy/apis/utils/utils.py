3c0d77d649fdd2d2d6138a794352fd54aa55c82d
AllentDan <AllentDan@yeah.net>
2022-08-10 14:48:33 +0800

    add device backend check

--- mmdeploy/apis/utils/utils.py
+++ mmdeploy/apis/utils/utils.py
@@ -2,7 +2,26 @@
 import mmcv
 
 from mmdeploy.codebase import BaseTask, get_codebase_class, import_codebase
-from mmdeploy.utils import get_codebase, get_task_type
+from mmdeploy.utils import (get_backend_config, get_codebase, get_task_type,
+                            parse_device_id)
+
+
+def check_backend_device(deploy_cfg: mmcv.Config, device: str):
+    """Check if device is appropriate for the backend.
+
+    Args:
+        deploy_cfg (str | mmcv.Config): Deployment config file.
+        device (str): A string specifying device type.
+    """
+    backend = get_backend_config(deploy_cfg)['type']
+    device_id = parse_device_id(device)
+    mismatch = dict(
+        tensorrt=lambda id: id == -1,
+        openvino=lambda id: id > -1,
+        rknn=lambda id: id > -1,
+        ncnn=lambda id: id > -1)
+    if mismatch[backend](device_id):
+        raise ValueError(f'{device} is invalid for the backend {backend}')
 
 
 def build_task_processor(model_cfg: mmcv.Config, deploy_cfg: mmcv.Config,
@@ -17,6 +36,7 @@ def build_task_processor(model_cfg: mmcv.Config, deploy_cfg: mmcv.Config,
     Returns:
         BaseTask: A task processor.
     """
+    check_backend_device(deploy_cfg=deploy_cfg, device=device)
     codebase_type = get_codebase(deploy_cfg)
     import_codebase(codebase_type)
     codebase = get_codebase_class(codebase_type)
