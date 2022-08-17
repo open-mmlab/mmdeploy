d7e449cf6d3f580652ce5495803ca5e3dbc53ce6
RunningLeon <mnsheng@yeah.net>
2022-08-17 14:28:06 +0800

    fix mmcls

--- mmdeploy/codebase/mmcls/deploy/classification.py
+++ mmdeploy/codebase/mmcls/deploy/classification.py
@@ -169,7 +169,7 @@ class Classification(BaseTask):
         model_cfg = self.model_cfg
         assert 'test_pipeline' in model_cfg, \
             f'test_pipeline not found in {model_cfg}.'
-        from mmcls.datasets.pipelines import Compose
+        from mmengine.dataset import Compose
         pipeline = deepcopy(model_cfg.test_pipeline)
 
         if isinstance(imgs, str):
