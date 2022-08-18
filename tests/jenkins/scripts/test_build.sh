4a42b856bbab455f1289c956c5a473bc6a3dccf7
kumailf <kumailf77@gmail.com>
2022-08-18 12:47:48 +0800

    fix

--- tests/jenkins/scripts/test_build.sh
+++ tests/jenkins/scripts/test_build.sh
@@ -5,4 +5,4 @@ export docker_image=mmdeploy-ci-ubuntu-18.04
 docker build tests/jenkins/docker/${docker_image}/ -t ${docker_image}
 export container_id=$(docker run -itd ${docker_image} /bin/bash) 
 docker exec ${container_id} git clone https://github.com/kumailf/mmdeploy.git
-docker exec ${container_id} bin/bash -c /root/workspace/mmdeploy/tests/jenkins/scripts/docker_exec_for_build.sh
+docker exec ${container_id} bash -c "/root/workspace/mmdeploy/tests/jenkins/scripts/docker_exec_for_build.sh"
