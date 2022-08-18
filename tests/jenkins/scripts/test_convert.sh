9f9933acd132634a3280848b442362e6ccae3fef
kumailf <kumailf77@gmail.com>
2022-08-18 14:13:21 +0800

    fix

--- tests/jenkins/scripts/test_convert.sh
+++ tests/jenkins/scripts/test_convert.sh
@@ -8,8 +8,6 @@ export codebase_list=($2)
 ## build
 
 docker build tests/jenkins/docker/${docker_image}/ -t ${docker_image}
-export container_id=$(docker run -itd ${docker_image} /bin/bash) 
-
 
 # docker run cmd for convert
 for codebase in ${codebase_list}
@@ -17,13 +15,12 @@ do
     log_dir=/data2/regression_log/$(date +%Y%m%d%H%M)/${codebase}
     mkdir -p ${log_dir}
     container_id=$(
-        docker run -it \
+        docker run -itd \
             -v /data2/checkpoints/${codebase}:/root/workspace/mmdeploy_checkpoints \
             -v ${log_dir}:/root/workspace/mmdeploy_regression_working_dir \
             -v /data2/benchmark:/root/workspace/mmdeploy_benchmark \
             --name ${codebase}-${docker_image} \
-            ${docker_image}
+            ${docker_image} "/bin/bash -c git clone https://github.com/kumailf/mmdeploy.git"
     )
-    docker exec ${container_id} git clone https://github.com/kumailf/mmdeploy.git
-    docker exec ${container_id} bash -c "/root/workspace/mmdeploy/tests/jenkins/scripts/docker_exec_for_build.sh ${codebase}"
+    docker exec -d ${container_id} bash -c "/root/workspace/mmdeploy/tests/jenkins/scripts/docker_exec_for_build.sh ${codebase}"
 done
