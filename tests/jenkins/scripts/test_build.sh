b9d56300199571d58eabf55879ef7f3b6965ed98
kumailf <kumailf77@gmail.com>
2022-08-18 14:21:55 +0800

    fix

--- tests/jenkins/scripts/test_build.sh
+++ tests/jenkins/scripts/test_build.sh
@@ -8,5 +8,6 @@ export docker_image=$1
 ## build
 
 docker build tests/jenkins/docker/${docker_image}/ -t ${docker_image}
-container_id=$(docker run -itd ${docker_image} "/bin/bash -c git clone --recursive https://github.com/open-mmlab/mmdeploy.git") 
-docker exec -d ${container_id} bash -c "/root/workspace/mmdeploy/tests/jenkins/scripts/docker_exec_for_build.sh"
+container_id=$(docker run -itd ${docker_image} /bin/bash) 
+docker exec ${container_id} git clone --recrusive https://github.com/kumailf/mmdeploy.git
+docker exec ${container_id} bash -c "/root/workspace/mmdeploy/tests/jenkins/scripts/docker_exec_for_build.sh"
