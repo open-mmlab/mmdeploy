efd02eaaf2ea5a2496ad29f5c2a78e2a92713142
RunningLeon <mnsheng@yeah.net>
2022-08-23 10:29:11 +0800

    update

--- .github/scripts/test_mmcls_full_pipeline.sh
+++ .github/scripts/test_mmcls_full_pipeline.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-mmcls_version=$1
-
-echo  "$mmcls_version"
-
-#git clone  --depth 1 --single-branch --branch $mmcls_version https://github.com/open-mmlab/mmclassification.git ../mmclassification
-cd ../mmclassification
-pip install -e .
-cd -
+#resnet18 + cifar10
 
 checkpoint=https://download.openmmlab.com/mmclassification/v0/resnet/resnet18_b16x8_cifar10_20210528-bd6371c8.pth
 model_cfg=../mmclassification/configs/resnet/resnet18_8xb16_cifar10.py
@@ -19,7 +12,7 @@ work_dir=work_dir
 mkdir -p $work_dir
 device=cpu
 
-python tools/deploy.py \
+python3 tools/deploy.py \
   $ort_cfg \
   $model_cfg \
   $checkpoint \
@@ -28,15 +21,15 @@ python tools/deploy.py \
   --work-dir $work_dir \
   --dump-info
 
+# prepare dataset
 mkdir -p data/cifar10
 url=https://raw.githubusercontent.com/RunningLeon/mmdeploy-testdata/master/data/cifar10/cifar-10-python.tar.gz
-#wget -o data/cifar10/cifar-10-python.tar.gz $url
+wget -o data/cifar10/cifar-10-python.tar.gz $url
 tar -xzvf data/cifar10/cifar-10-python.tar.gz -C data/cifar10/
-
 # change to avoid md5 check
 sed -i "s/get_dist_info()/1,0/g" ../mmclassification/mmcls/datasets/cifar.py
 
-python tools/test.py \
+python3 tools/test.py \
   $ort_cfg \
   $model_cfg \
   --model $work_dir/end2end.onnx \
