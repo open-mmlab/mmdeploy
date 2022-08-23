aebbd4a729460073d20c51236ac3a586bf82e661
irexyc <xinchen.tju@gmail.com>
2022-08-23 16:07:18 +0800

    update toolchain

--- cmake/toolchains/riscv64-linux-gnu.cmake
+++ cmake/toolchains/riscv64-linux-gnu.cmake
@@ -18,5 +18,9 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
-set(CMAKE_C_FLAGS "-march=rv64gc -mabi=lp64d -mtune=c906")
-set(CMAKE_CXX_FLAGS "-march=rv64gc -mabi=lp64d -mtune=c906")
+set(CMAKE_C_FLAGS "-march=rv64gc")
+set(CMAKE_CXX_FLAGS "-march=rv64gc")
+
+# cache flags
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "c++ flags")
