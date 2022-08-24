4862d78faf0ef9b506a1ad058f210d6b1be8a17d
kumailf <kumailf77@gmail.com>
2022-08-24 12:04:16 +0800

    fix

--- tests/jenkins/scripts/docker_exec_convert_gpu.sh
+++ tests/jenkins/scripts/docker_exec_convert_gpu.sh
@@ -58,6 +58,8 @@ do
     conda run --name torch${TORCH_VERSION} "
         python ./tools/regression_test.py \
             --codebase ${codebase} \
-            --work-dir "../mmdeploy_regression_working_dir/${codebase}/torch${TORCH_VERSION}"
+            --work-dir "../mmdeploy_regression_working_dir/${codebase}/torch${TORCH_VERSION} \
+            --performance
+            "
     "
 done
