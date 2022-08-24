4535a847dc73e23146e8064caa295f36328db7a9
kumailf <kumailf77@gmail.com>
2022-08-24 18:29:13 +0800

    fix

--- tests/jenkins/scripts/test_prebuild.sh
+++ tests/jenkins/scripts/test_prebuild.sh
@@ -3,7 +3,8 @@
 ## parameters
 export docker_image=$1
 
-log_dir=/data2/regression_log/prebuild/$(date +%Y%m%d)/$(date +%Y%m%d%H%M).log
+log_dir=/data2/regression_log/prebuild/$(date +%Y%m%d)/$(date +%Y%m%d%H%M)
+mkdir -p ${log_dir}
 
 container_id=$(
     docker run \
@@ -11,4 +12,4 @@ container_id=$(
         -v /data2/shared/prebuilt-mmdeploy:/root/workspace/prebuild-mmdeploy \
         ${docker_image} /bin/bash
     ) 
-nohup docker exec ${container_id} bash -c "git clone --recursive https://github.com/kumailf/mmdeploy.git && /root/workspace/mmdeploy/tests/jenkins/scripts/docker_exec_prebuild.sh" > ${log_dir} 2>&1 &
+nohup docker exec ${container_id} bash -c "git clone --recursive https://github.com/kumailf/mmdeploy.git && /root/workspace/mmdeploy/tests/jenkins/scripts/docker_exec_prebuild.sh" > ${log_dir}/prebuild.log 2>&1 &
