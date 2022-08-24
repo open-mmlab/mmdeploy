b558069532a6a4a3d9c0717a8c8cf17180019a2b
kumailf <kumailf77@gmail.com>
2022-08-24 18:23:55 +0800

    fix

--- tests/jenkins/scripts/test_prebuild.sh
+++ tests/jenkins/scripts/test_prebuild.sh
@@ -9,4 +9,4 @@ container_id=$(
         -v /data2/shared/prebuilt-mmdeploy:/root/workspace/prebuild-mmdeploy \
         ${docker_image} /bin/bash
     ) 
-nohup docker exec ${container_id} bash -c "git clone --recursive https://github.com/kumailf/mmdeploy.git && /root/workspace/mmdeploy/tests/jenkins/scripts/docker_exec_prebuild.sh" > prebuild.log 2>&1 &
+nohup docker exec ${container_id} bash -c "git clone --recursive https://github.com/kumailf/mmdeploy.git && /root/workspace/mmdeploy/tests/jenkins/scripts/docker_exec_prebuild.sh" > /data2/regression_log/prebuild/$(date +%Y%m%d)/$(date +%Y%m%d%H%M) 2>&1 &
