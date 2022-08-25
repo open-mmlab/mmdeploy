f5f091eb6dcca4a2d4e760f09ecc84ac9642d7d9
hanrui1sensetime <hanrui1@sensetime.com>
2022-08-25 20:32:53 +0800

    adapt new mmdet 2.0 0825

--- mmdeploy/codebase/mmdet/models/__init__.py
+++ mmdeploy/codebase/mmdet/models/__init__.py
@@ -2,6 +2,8 @@
 from .backbones import *  # noqa: F401, F403
 from .dense_heads import *  # noqa: F401,F403
 from .detectors import *  # noqa: F401,F403
+from .layers import *  # noqa: F401,F403
 from .necks import *  # noqa: F401,F403
 from .roi_heads import *  # noqa: F401,F403
+from .task_modules import *  # noqa: F401,F403
 from .transformer import *  # noqa: F401,F403
