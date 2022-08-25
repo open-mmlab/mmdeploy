715329a36d368643b8bd77254b83dd99a429d2ce
kumailf <kumailf77@gmail.com>
2022-08-25 18:21:27 +0800

    fix

--- tests/jenkins/scripts/docker_exec_prebuild.sh
+++ tests/jenkins/scripts/docker_exec_prebuild.sh
@@ -11,13 +11,13 @@ cd /root/workspace/mmdeploy
 
 /opt/conda/envs/torch1.10.0/bin/mim install mmdet==v2.20.0
 cd /root/workspace
-git clone -depth 1 -branch v2.20.0 https://github.com/open-mmlab/mmdetection.git
+git clone --depth 1 -branch v2.20.0 https://github.com/open-mmlab/mmdetection.git
 
 cd /root/workspace/mmdeploy
 
 conda run --name torch1.10.0 "
     python ./tools/package_tools/mmdeploy_builder.py tools/package_tools/configs/linux_x64.yaml .
-"
+" > /root/work/log/build.log 2>&1 &
 /opt/conda/envs/torch1.10.0/bin/pip install mmdeploy-0.7.0-linux-x86_64-onnxruntime1.8.1/sdk/python/mmdeploy_python-0.7.0-cp38-none-linux_x86_64.whl
 /opt/conda/envs/torch1.10.0/bin/pip install mmdeploy-0.7.0-linux-x86_64-onnxruntime1.8.1/dist/mmdeploy-0.7.0-py3-none-linux_x86_64.whl
 /opt/conda/envs/torch1.10.0/bin/pip install mmdeploy-0.7.0-linux-x86_64-cuda11.3-tensorrt8.2.1.8/dist/mmdeploy-0.7.0-py3-none-linux_x86_64.whl
@@ -31,4 +31,4 @@ mv mmdeploy-*-tensorrt* ./prebuild-mmdeploy
 
 conda run --name torch1.10.0 "
     python tools/regression_test.py --codebase mmdet --models ssd --backends onnxruntime tensorrt -p --device cuda
-"
+" > /root/workspace/test_prebuild.log 2>&1 &
--- tests/jenkins/scripts/test_prebuild.sh
+++ tests/jenkins/scripts/test_prebuild.sh
@@ -14,8 +14,9 @@ container_id=$(
         -v /data2/checkpoints/${codebase}:/root/workspace/mmdeploy_checkpoints \
         -v /data2/benchmark:/root/workspace/mmdeploy_benchmark \
         -v /data2/shared/prebuilt-mmdeploy:/root/workspace/prebuild-mmdeploy \
+        -v ${log_dir}:/root/workspace/log \
         --name ${container_name} \
         ${docker_image} /bin/bash
     ) 
 echo "container_id=${container_id}"
-nohup docker exec ${container_id} bash -c "git clone --recursive https://github.com/kumailf/mmdeploy.git && /root/workspace/mmdeploy/tests/jenkins/scripts/docker_exec_prebuild.sh" > ${log_dir}/prebuild.log 2>&1 &
+nohup docker exec ${container_id} bash -c "git clone --recursive https://github.com/kumailf/mmdeploy.git && /root/workspace/mmdeploy/tests/jenkins/scripts/docker_exec_prebuild.sh" > ${log_dir}/prebuild.log 2>&1 & 
