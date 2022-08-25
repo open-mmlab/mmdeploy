90e03d16dfdd8e0ef3f74fe556a486f45d0cb401
kumailf <kumailf77@gmail.com>
2022-08-25 18:57:14 +0800

    fix

--- tests/jenkins/scripts/docker_exec_prebuild.sh
+++ tests/jenkins/scripts/docker_exec_prebuild.sh
@@ -17,7 +17,7 @@ cd /root/workspace/mmdeploy
 
 conda run --name torch1.10.0 "
     python ./tools/package_tools/mmdeploy_builder.py tools/package_tools/configs/linux_x64.yaml .
-" > /root/work/log/build.log 2>&1 &
+" > /root/workspace/log/build.log 2>&1 &
 /opt/conda/envs/torch1.10.0/bin/pip install mmdeploy-0.7.0-linux-x86_64-onnxruntime1.8.1/sdk/python/mmdeploy_python-0.7.0-cp38-none-linux_x86_64.whl
 /opt/conda/envs/torch1.10.0/bin/pip install mmdeploy-0.7.0-linux-x86_64-onnxruntime1.8.1/dist/mmdeploy-0.7.0-py3-none-linux_x86_64.whl
 /opt/conda/envs/torch1.10.0/bin/pip install mmdeploy-0.7.0-linux-x86_64-cuda11.3-tensorrt8.2.1.8/dist/mmdeploy-0.7.0-py3-none-linux_x86_64.whl
@@ -31,4 +31,4 @@ mv mmdeploy-*-tensorrt* ./prebuild-mmdeploy
 
 conda run --name torch1.10.0 "
     python tools/regression_test.py --codebase mmdet --models ssd --backends onnxruntime tensorrt -p --device cuda
-" > /root/workspace/test_prebuild.log 2>&1 &
+" > /root/workspace/log/test_prebuild.log 2>&1 &
