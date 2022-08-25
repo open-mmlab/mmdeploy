0370d4916ce6bd92e509d60bcd361ac48d12dd6a
kumailf <kumailf77@gmail.com>
2022-08-25 21:01:32 +0800

    fix

--- tests/jenkins/scripts/docker_exec_prebuild.sh
+++ tests/jenkins/scripts/docker_exec_prebuild.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
 
-source ~/.bashrc
+## init conda
+__conda_setup="$('/opt/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/conda/etc/profile.d/conda.sh" ]; then
+        . "/opt/conda/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/conda/bin:$PATH"
+    fi
+fi
+unset __conda_setup && conda activate torch1.10.0 && conda env list
+
+
 #### to be removed
 apt install -y g++-7 gcc-7
 export pplcv_DIR=/root/workspace/ppl.cv/cuda-build/install/lib/cmake/ppl
