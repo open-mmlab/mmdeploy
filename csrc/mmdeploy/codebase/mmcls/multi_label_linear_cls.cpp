ed0431b405cf04797eaaef567bbfcc9f1a803258
gaoying <gaoying@xiaobaishiji.com>
2022-08-29 21:05:00 +0800

    delete idx

--- csrc/mmdeploy/codebase/mmcls/multi_label_linear_cls.cpp
+++ csrc/mmdeploy/codebase/mmcls/multi_label_linear_cls.cpp
@@ -47,11 +47,8 @@ class MultiLabelLinearClsHead : public MMClassification {
   Value GetLabels(const Tensor& scores, int class_num) const {
     auto scores_data = scores.data<float>();
     ClassifyOutput output;
-    output.labels.reserve(num_classes_);
-    std::vector<int> idx(class_num);
-    iota(begin(idx), end(idx), 0);
     for (int i = 0; i < num_classes_; ++i) {
-      auto label = ClassifyOutput::Label{idx[i], scores_data[idx[i]]};
+      auto label = ClassifyOutput::Label{i, scores_data[i]};
       MMDEPLOY_DEBUG("label_id: {}, score: {}", label.label_id, label.score);
       output.labels.push_back(label);
     }
