ac0533e1ad30ff99ed6ca58ce2672e0878880ecc
kumailf <kumailf77@gmail.com>
2022-08-30 17:34:22 +0800

    fix

--- tests/jenkins/scripts/docker_exec_convert_gpu.sh
+++ tests/jenkins/scripts/docker_exec_convert_gpu.sh
@@ -95,6 +95,6 @@ do
         --codebase ${codebase} \
         --work-dir ${log_dir} \
         --backends tensorrt \
-        --models ResNet ssd masterrcnn fcn srcnn hrnet dbnet crnn pointpillas \
+        --models ResNet ssd masterrcnn fcn srcnn hrnet dbnet crnn PointPillars \
         --performance 2>&1 | tee ${log_path}
 done
