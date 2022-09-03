1a9870ca174f416526649e01250d80d2d80b10fb
kumailf <kumailf77@gmail.com>
2022-09-03 15:56:03 +0800

    fix

--- tests/jenkins/scripts/test_build.sh
+++ tests/jenkins/scripts/test_build.sh
@@ -10,7 +10,7 @@ time_snap=$(date +%Y%m%d%H%M)
 log_dir=/data2/regression_log/build_log/${date_snap}/${time_snap}
 mkdir -p ${log_dir}
 container_name=build-${time_snap}
-container_id=$(docker run -itd --gpus all ${docker_image} /bin/bash)
+container_id=$(docker run -itd --gpus all -v ~/mmdeploy/tests/jenkins/scripts:/root/workspace/mmdeploy_script ${docker_image} /bin/bash)
 echo "container_id=${container_id} --name ${container_name}"
 
 nohup docker exec ${container_id} bash -c "git clone --depth 1 --branch master --recursive https://github.com/open-mmlab/mmdeploy.git &&\
