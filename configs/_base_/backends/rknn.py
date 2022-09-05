96471da213264482a89219517b98b2e169970c56
AllentDan <AllentDan@yeah.net>
2022-09-05 14:30:06 +0800

    remote output_tensor_type

--- configs/_base_/backends/rknn.py
+++ configs/_base_/backends/rknn.py
@@ -3,7 +3,6 @@ backend_config = dict(
     common_config=dict(
         mean_values=None,
         std_values=None,
-        output_tensor_type=None,
         target_platform='rk3588',
         optimization_level=3),
     quantization_config=dict(do_quantization=False, dataset=None))
