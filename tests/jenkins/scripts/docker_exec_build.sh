195f85667a281d477f1974906c5006e9b04e377f
kumailf <kumailf77@gmail.com>
2022-09-07 16:37:31 +0800

    fix

--- tests/jenkins/scripts/docker_exec_build.sh
+++ tests/jenkins/scripts/docker_exec_build.sh
@@ -29,52 +29,60 @@ pip install -r requirements/build.txt
 ## 校验动态库构建
 mkdir -p ${MMDEPLOY_DIR}/build_share_libs && cd ${MMDEPLOY_DIR}/build_share_libs
 
-#TODO add openvino
-cmake .. -DMMDEPLOY_BUILD_SDK=ON -DMMDEPLOY_BUILD_EXAMPLES=ON \
-         -DMMDEPLOY_BUILD_SDK_MONOLITHIC=ON -DMMDEPLOY_BUILD_TEST=ON \
-         -DMMDEPLOY_BUILD_SDK_PYTHON_API=ON -DMMDEPLOY_BUILD_SDK_JAVA_API=ON \
-         -DMMDEPLOY_BUILD_EXAMPLES=ON -DMMDEPLOY_ZIP_MODEL=ON \
-         -DMMDEPLOY_TARGET_BACKENDS="ort;trt;ncnn" \
-         -DMMDEPLOY_SHARED_LIBS=ON \
-         -DTENSORRT_DIR=${TENSORRT_DIR} \
-         -DCUDNN_DIR=${CUDNN_DIR} \
-         -DONNXRUNTIME_DIR=${ONNXRUNTIME_DIR} \
-         -Dncnn_DIR=${ncnn_DIR} \
-         -DTorch_DIR=${Torch_DIR} \
-         -Dpplcv_DIR=${pplcv_DIR} \
-         -DMMDEPLOY_TARGET_DEVICES="cuda;cpu"
-
+cmake .. -DMMDEPLOY_BUILD_SDK=ON \
+        -DMMDEPLOY_BUILD_EXAMPLES=ON \
+        -DMMDEPLOY_BUILD_SDK_MONOLITHIC=ON -DMMDEPLOY_BUILD_TEST=ON \
+        -DMMDEPLOY_BUILD_SDK_PYTHON_API=ON -DMMDEPLOY_BUILD_SDK_JAVA_API=ON \
+        -DMMDEPLOY_BUILD_EXAMPLES=ON -DMMDEPLOY_ZIP_MODEL=ON \
+        -DMMDEPLOY_TARGET_BACKENDS="trt;ort;ncnn" \
+        -DMMDEPLOY_SHARED_LIBS=ON \
+        -DTENSORRT_DIR=${TENSORRT_DIR} \
+        -DCUDNN_DIR=${CUDNN_DIR} \
+        -DONNXRUNTIME_DIR=${ONNXRUNTIME_DIR} \
+        -Dncnn_DIR=${ncnn_DIR} \
+        -DTorch_DIR=${Torch_DIR} \
+        -Dpplcv_DIR=${pplcv_DIR} \
+        -DMMDEPLOY_TARGET_DEVICES="cuda;cpu"
 make -j $(nproc) && make install && cd ${MMDEPLOY_DIR}
 
 pip install -v .
-# TODO 插入校验语句，确认pip install 成功
+if [ $? -ne 0 ]; then
+    echo "build mmdeploy with shared lib fail!"
+    exit
+fi
 
 ## 校验静态库构建
 mkdir -p ${MMDEPLOY_DIR}/build && cd ${MMDEPLOY_DIR}/build
-cmake .. -DMMDEPLOY_BUILD_SDK=ON -DMMDEPLOY_BUILD_EXAMPLES=ON \
-         -DMMDEPLOY_BUILD_SDK_MONOLITHIC=ON -DMMDEPLOY_BUILD_TEST=ON \
-         -DMMDEPLOY_BUILD_SDK_PYTHON_API=ON -DMMDEPLOY_BUILD_SDK_JAVA_API=ON \
-         -DMMDEPLOY_BUILD_EXAMPLES=ON -DMMDEPLOY_ZIP_MODEL=ON \
-         -DMMDEPLOY_TARGET_BACKENDS="ort;trt;ncnn" \
-         -DMMDEPLOY_SHARED_LIBS=OFF \
-         -DONNXRUNTIME_DIR=${ONNXRUNTIME_DIR} \
-         -Dncnn_DIR=${ncnn_DIR} \
-         -DTENSORRT_DIR=${TENSORRT_DIR} \
-         -DTorch_DIR=${Torch_DIR} \
-         -Dpplcv_DIR=${pplcv_DIR} \
-         -DMMDEPLOY_TARGET_DEVICES="cuda;cpu"
-
+cmake .. -DMMDEPLOY_BUILD_SDK=ON \
+        -DMMDEPLOY_BUILD_EXAMPLES=ON \
+        -DMMDEPLOY_BUILD_SDK_MONOLITHIC=ON -DMMDEPLOY_BUILD_TEST=ON \
+        -DMMDEPLOY_BUILD_SDK_PYTHON_API=ON -DMMDEPLOY_BUILD_SDK_JAVA_API=ON \
+        -DMMDEPLOY_BUILD_EXAMPLES=ON -DMMDEPLOY_ZIP_MODEL=ON \
+        -DMMDEPLOY_TARGET_BACKENDS="trt;ort;ncnn" \
+        -DMMDEPLOY_SHARED_LIBS=OFF \
+        -DTENSORRT_DIR=${TENSORRT_DIR} \
+        -DCUDNN_DIR=${CUDNN_DIR} \
+        -DONNXRUNTIME_DIR=${ONNXRUNTIME_DIR} \
+        -Dncnn_DIR=${ncnn_DIR} \
+        -DTorch_DIR=${Torch_DIR} \
+        -Dpplcv_DIR=${pplcv_DIR} \
+        -DMMDEPLOY_TARGET_DEVICES="cuda;cpu"
 make -j $(nproc) && make install && cd ${MMDEPLOY_DIR}
 
 pip install -v .
-# todo 插入校验语句，确认pip install 成功
+if [ $? -ne 0 ]; then
+    echo "build mmdeploy with ubshared lib in torch1.8.0 fail!"
+    exit
+fi
 
 ## 校验不同torch版本下安装
 for TORCH_VERSION in 1.9.0 1.10.0 1.11.0 1.12.0
 do
     conda activate torch${TORCH_VERSION}
     pip install -v .
-    python tools/check_env.py
-    # todo 插入校验语句，确认pip install 成功
+    if [ $? -ne 0 ]; then
+        echo "build mmdeploy with ubshared lib in torch${TORCH_VERSION} fail!"
+        exit
+    fi
 done
 echo "time-$(date +%Y%m%d%H%M)"
