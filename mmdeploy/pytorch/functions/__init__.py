7facb7c53d1555a9cdadac723b7bd2e34ec1440e
AllentDan <AllentDan@yeah.net>
2022-09-08 18:16:01 +0800

    add mod rewriter for TRT

--- mmdeploy/pytorch/functions/__init__.py
+++ mmdeploy/pytorch/functions/__init__.py
@@ -12,6 +12,7 @@ from .group_norm import group_norm__ncnn
 from .interpolate import interpolate__ncnn, interpolate__tensorrt
 from .linear import linear__ncnn
 from .masked_fill import masked_fill__onnxruntime
+from .mod import mod__tensorrt
 from .normalize import normalize__ncnn
 from .repeat import tensor__repeat__tensorrt
 from .size import tensor__size__ncnn
@@ -28,5 +29,6 @@ __all__ = [
     'chunk__torchscript', 'masked_fill__onnxruntime',
     'tensor__setitem__default', 'tensor__getitem__ascend',
     'adaptive_avg_pool2d__default', 'adaptive_avg_pool2d__ncnn',
-    'multi_head_attention_forward', 'flatten__coreml', 'clip__coreml'
+    'multi_head_attention_forward', 'flatten__coreml', 'clip__coreml',
+    'mod__tensorrt'
 ]
