0561d8eaa1c868ebced886a673d4ba8066cc4e88
sunjiahao <578431509@qq.com>
2022-09-14 20:32:19 +0800

    add monodet task

--- mmdeploy/codebase/mmdet3d/deploy/__init__.py
+++ mmdeploy/codebase/mmdet3d/deploy/__init__.py
@@ -2,5 +2,10 @@
 from .mmdetection3d import MMDetection3d
 from .voxel_detection import VoxelDetection
 from .voxel_detection_model import VoxelDetectionModel
+from .monocular_detection import MonocularDetection
+from .monocular_detection_model import MonocularDetectionModel
 
-__all__ = ['MMDetection3d', 'VoxelDetection', 'VoxelDetectionModel']
+__all__ = [
+    'MMDetection3d', 'VoxelDetection', 'VoxelDetectionModel',
+    'MonocularDetection', 'MonocularDetectionModel'
+]
