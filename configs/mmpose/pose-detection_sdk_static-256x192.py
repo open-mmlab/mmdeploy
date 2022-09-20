2f9103ae74dc4148834e1375907f1f305bcb9003
RunningLeon <mnsheng@yeah.net>
2022-09-20 18:00:35 +0800

    add mmpose sdk

--- configs/mmpose/pose-detection_sdk_static-256x192.py
+++ configs/mmpose/pose-detection_sdk_static-256x192.py
@@ -3,14 +3,9 @@ _base_ = ['./pose-detection_static.py', '../_base_/backends/sdk.py']
 codebase_config = dict(model_type='sdk')
 
 backend_config = dict(pipeline=[
-    dict(type='LoadImageFromFile', channel_order='bgr'),
-    dict(
-        type='PackPoseInputs',
-        keys=['img'],
-        meta_keys=[
-            'id', 'img_id', 'img_path', 'ori_shape', 'img_shape', 'input_size',
-            'flip_indices', 'category'
-        ])
+    dict(type='LoadImageFromFile'),
+    dict(type='GetBBoxCenterScale'),
+    dict(type='PackPoseInputs')
 ])
 
 ext_info = dict(image_size=[192, 256], padding=1.25)
