2258a82a04e0a324a99123f8955c1304af9c87a4
RunningLeon <mnsheng@yeah.net>
2022-09-21 15:52:53 +0800

    reuse mmdet

--- mmdeploy/codebase/mmdet/deploy/__init__.py
+++ mmdeploy/codebase/mmdet/deploy/__init__.py
@@ -5,5 +5,5 @@ from .utils import (clip_bboxes, get_post_processing_params, pad_with_value,
 
 __all__ = [
     'get_post_processing_params', 'clip_bboxes', 'pad_with_value',
-    'pad_with_value_if_necessary', 'MMDetection', 'ObjectDetection'
+    'pad_with_value_if_necessary', 'ObjectDetection'
 ]
