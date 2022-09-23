f502f06e0078c8377a93e4efc122b8f939e23289
hanrui1sensetime <hanrui1@sensetime.com>
2022-09-23 21:22:17 +0800

    add base codebase

--- mmdeploy/codebase/base/mmcodebase.py
+++ mmdeploy/codebase/base/mmcodebase.py
@@ -49,6 +49,10 @@ class MMCodebase(metaclass=ABCMeta):
                 deploy_cfg=deploy_cfg,
                 device=device))
 
+    @classmethod
+    def import_module(cls):
+        pass
+
 
 # Note that the build function returns the class instead of its instance.
 
