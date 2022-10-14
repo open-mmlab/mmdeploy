28d25e847122820de661f83075b83e0c967a82fe
kumailf <kumailf77@gmail.com>
2022-10-14 15:56:40 +0800

    add req.json file

--- tests/jenkins/scripts/test_convert.sh
+++ tests/jenkins/scripts/test_convert.sh
@@ -44,7 +44,7 @@ for codebase in ${codebase_list[@]}; do
         )
         echo "container_id=${container_id}"
         nohup docker exec ${container_id} bash -c "git clone --depth 1 --branch ${mmdeploy_branch} --recursive ${repo_url} &&\
-        /root/workspace/mmdeploy_script/docker_exec_convert_gpu.sh ${codebase} "${exec_performance}" ${TENSORRT_VERSION}" "${REQUIREMENT}" >${log_dir}/${codebase}.log 2>&1 &
+        /root/workspace/mmdeploy_script/docker_exec_convert_gpu.sh ${codebase} "${exec_performance}" ${TENSORRT_VERSION} ${REQUIREMENT}" >${log_dir}/${codebase}.log 2>&1 &
         wait
         docker stop $container_id
         echo "${codebase} convert finish!"
