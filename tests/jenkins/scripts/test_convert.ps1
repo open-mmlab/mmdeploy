54c7bddce9bfac1d40122c09ac4295819c239494
jmkill1 <542827340@qq.com>
2022-10-14 10:56:14 +0800

    fic codebase path

--- tests/jenkins/scripts/test_convert.ps1
+++ tests/jenkins/scripts/test_convert.ps1
@@ -1,5 +1,5 @@
 $env:DEPS_DIR="D:\huangzijie\deps"
-$env:WORKSPACE="D:\huangzijie"
+$env:WORKSPACE="D:\huangzijie\workspace"
 $env:OPENCV_DIR=(Join-PATH $env:DEPS_DIR opencv\4.6.0\build\x64\vc15)
 $env:TENSORRT_DIR=(Join-PATH $env:DEPS_DIR TensorRT-8.2.3.0)
 $env:ONNXRUNTIME_DIR=(Join-PATH $env:DEPS_DIR onnxruntime-win-x64-1.8.1)
@@ -132,20 +132,20 @@ $script_block = {
     param(
         [string] $codebase,
         [string] $exec_performance,
-        [string] $codebase_path,
+        [string] $codebase_fullname,
         [string] $log_dir,
         [string] $scriptDir
     )
-    Write-Host "pwsh $scriptDir\win_convert_exec.ps1 $codebase $exec_performance $codebase_path *> $log_dir\$codebase.log"
-    invoke-expression -command "pwsh $scriptDir\win_convert_exec.ps1 '$codebase' '$exec_performance' '$codebase_path' *> $log_dir\$codebase.log"
+    Write-Host "$scriptDir\win_convert_exec.ps1 $codebase $exec_performance $codebase_fullname *> $log_dir\$codebase.log"
+    invoke-expression -command "$scriptDir\win_convert_exec.ps1 $codebase $exec_performance $codebase_fullname *> $log_dir\$codebase.log"
 }
 
 $threads = @()
 
 $handles = foreach ($codebase in $codebase_list -split ' ')
 {
-    $codebase_path = $codebase_fullname_opt.([string]$codebase)
-    $powershell = [powershell]::Create().AddScript($script_block).AddArgument($codebase).AddArgument($exec_performance).AddArgument($codebase_path).AddArgument($log_dir).AddArgument($scriptDir)
+    $codebase_fullname = $codebase_fullname_opt.([string]$codebase)
+    $powershell = [powershell]::Create().AddScript($script_block).AddArgument($codebase).AddArgument($exec_performance).AddArgument($codebase_fullname).AddArgument($log_dir).AddArgument($scriptDir)
 	  $powershell.RunspacePool = $Pool
 	  $powershell.BeginInvoke()
     $threads += $powershell
--- tests/jenkins/scripts/win_convert_exec.ps1
+++ tests/jenkins/scripts/win_convert_exec.ps1
@@ -1,20 +1,21 @@
 param(
     $codebase,
     $exec_performance,
-    $codebase_path
+    $codebase_fullname
 )
-
-Write-Host "exec_path:  $pwd"
+$scriptDir = Split-Path -parent $MyInvocation.MyCommand.Path
+Import-Module $scriptDir\utils.psm1
+Write-Host "exec_path: $pwd"
 Write-Host "mim install $codebase"
+Write-Host "codebase_fullname = $codebase_fullname"
+$codebase_path = (Join-Path $env:WORKSPACE $codebase_fullname)
 Write-Host "codebase_path = $codebase_path"
-$codebase_path = (Join-Path $env:WORKSPACE $codebase_path)
-Write-Host "codebase_path = $codebase_path"
+InitMim $codebase $codebase_fullname
 mim uninstall mmcv
 if ($codebase -eq "mmdet3d")
 {
     # mim install $codebase
     mim install mmcv-full==1.5.2
-
 }
 else
 {
