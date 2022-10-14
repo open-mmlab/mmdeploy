e2d228ac1959e2f257244de5dda03bd7d0cd4f4d
jmkill1 <542827340@qq.com>
2022-10-14 11:23:05 +0800

    fix bug

--- tests/jenkins/scripts/win_convert_exec.ps1
+++ tests/jenkins/scripts/win_convert_exec.ps1
@@ -10,21 +10,24 @@ Write-Host "mim install $codebase"
 Write-Host "codebase_fullname = $codebase_fullname"
 $codebase_path = (Join-Path $env:WORKSPACE $codebase_fullname)
 Write-Host "codebase_path = $codebase_path"
+$log_dir = (Join-Path $env:WORKSPACE "mmdeploy_regression_working_dir\$codebase\$env:CUDA_VERSION")
+Write-Host "log_dir = $log_dir"
 InitMim $codebase $codebase_fullname
-mim uninstall mmcv
+pip uninstall mmcv-full -y
+mim uninstall $codebase -y
 if ($codebase -eq "mmdet3d")
 {
-    # mim install $codebase
     mim install mmcv-full==1.5.2
 }
 else
 {
-    # mim install $codebase
     mim install mmcv-full==1.6.0
 }
+mim install $codebase
 pip install -v $codebase_path
 python $env:MMDEPLOY_DIR/tools/regression_test.py `
     --codebase $codebase `
     --device cuda:0 `
     --backends tensorrt onnxruntime `
+    --work_dir $log_dir
     $exec_performance
