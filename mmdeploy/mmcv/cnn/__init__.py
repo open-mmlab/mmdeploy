4b392058663230405af0e4e067200d3d0dad41d1
SsTtOoNnEe <1602965550@qq.com>
2022-10-17 16:20:30 +0800

    Rewrite Conv2dAdaptiveOps for conversion of EfficientNet (static shape) (#1045)
    * Rewrite Conv2dAdaptiveOps for conversion of EfficientNet (static shape)
    
    * Refactor codes and Add unit test
    
    * Simplify codes
    
    * update supported model configs in yaml
    
    * update mmcls.yml
    
    * resolve lint error
    
    Co-authored-by: SenseTime Research Singapore <SENSETIME\research.sgres@sg0016000001u.domain.sensetime.com>
    (cherry picked from commit dd7550a08d65d1afff08b47c463c729820fb0b9c)

--- mmdeploy/mmcv/cnn/__init__.py
+++ mmdeploy/mmcv/cnn/__init__.py
@@ -1,5 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .conv2d_adaptive_padding import (
+    AdaptivePadOp, conv2d_adaptive_padding__forward__tensorrt)
 from .transformer import (MultiHeadAttentionop,
                           multiheadattention__forward__ncnn)
 
-__all__ = ['multiheadattention__forward__ncnn', 'MultiHeadAttentionop']
+__all__ = [
+    'multiheadattention__forward__ncnn', 'MultiHeadAttentionop',
+    'conv2d_adaptive_padding__forward__tensorrt', 'AdaptivePadOp'
+]
