27ac0ab002e9975bae654a791ef9209704ce7fde
LiuYi-Up <73060646+LiuYi-Up@users.noreply.github.com>
2022-10-19 10:24:23 +0800

    [Docs] add support for vipnas (#1164)
    * add support for vipnas
    
    * add some unit tests
    
    Signed-off-by: LiuYi-Up <1150854440@qq.com>
    
    * Update quantization.md
    
    * resolve some conflicts in docs
    
    Signed-off-by: LiuYi-Up <1150854440@qq.com>
    
    * fix the mdformat
    
    Signed-off-by: LiuYi-Up <1150854440@qq.com>
    
    * fix the layer_norm.py & test_mmcv_cnn.py
    
    Signed-off-by: LiuYi-Up <1150854440@qq.com>
    
    Signed-off-by: LiuYi-Up <1150854440@qq.com>

--- mmdeploy/mmcv/cnn/__init__.py
+++ mmdeploy/mmcv/cnn/__init__.py
@@ -1,10 +1,15 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .context_block import context_block_spatial_pool
 from .conv2d_adaptive_padding import (
     AdaptivePadOp, conv2d_adaptive_padding__forward__tensorrt)
+from .hsigmoid import hsigmoid__forward__ncnn
+from .hswish import hswish__forward__ncnn
 from .transformer import (MultiHeadAttentionop,
                           multiheadattention__forward__ncnn)
 
 __all__ = [
     'multiheadattention__forward__ncnn', 'MultiHeadAttentionop',
-    'conv2d_adaptive_padding__forward__tensorrt', 'AdaptivePadOp'
+    'hswish__forward__ncnn', 'hsigmoid__forward__ncnn',
+    'context_block_spatial_pool', 'conv2d_adaptive_padding__forward__tensorrt',
+    'AdaptivePadOp'
 ]
