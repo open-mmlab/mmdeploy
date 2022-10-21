7e9b741bdfe40e554bd004367360f3a422b940e4
lvhan028 <lvhan_028@163.com>
2022-10-21 19:27:41 +0800

    0.1.1

--- csrc/mmdeploy/codebase/mmdet/ssd_head.cpp
+++ csrc/mmdeploy/codebase/mmdet/ssd_head.cpp
@@ -8,80 +8,6 @@
 
 namespace mmdeploy::mmdet {
 
-void SSDHead::FilterScoresAndTopk(Tensor& scores, float score_thr, int topk,
-                                  std::vector<float>& probs, std::vector<int>& label_ids,
-                                  std::vector<int>& anchor_idxs) {
-  auto kDets = scores.shape(1);
-  auto kClasses = scores.shape(2);
-  auto score_ptr = scores.data<float>();
-
-  for (auto i = 0; i < kDets; ++i, score_ptr += kClasses) {
-    auto iter = std::max_element(score_ptr, score_ptr + kClasses);
-    auto max_score = *iter;
-    if (*iter < score_thr) {
-      continue;
-    }
-    probs.push_back(*iter);
-    label_ids.push_back(iter - score_ptr);
-    anchor_idxs.push_back(i);
-  }
-}
-
-float SSDHead::IOU(float xmin0, float ymin0, float xmax0, float ymax0, float xmin1, float ymin1,
-                   float xmax1, float ymax1) {
-  auto w = std::max(0.f, std::min(xmax0, xmax1) - std::max(xmin0, xmin1));
-  auto h = std::max(0.f, std::min(ymax0, ymax1) - std::max(ymin0, ymin1));
-  auto area = w * h;
-  auto sum = (xmax0 - xmin0) * (ymax0 - ymin0) + (xmax1 - xmin1) * (ymax1 - ymin1);
-  auto iou = area / (sum - area);
-  return iou <= 0.f ? 0.f : iou;
-}
-
-void SSDHead::NMS(Tensor& dets, float iou_threshold, std::vector<int>& keep_idxs) {
-  auto det_ptr = dets.data<float>();
-  for (auto i = 0; i < keep_idxs.size(); ++i) {
-    auto n = keep_idxs[i];
-    for (auto j = i + 1; j < keep_idxs.size(); ++j) {
-      auto m = keep_idxs[j];
-
-      // `delta_xywh_bbox_coder` decode return tl_x, tl_y, br_x, br_y
-      float xmin0 = det_ptr[n * 4 + 0];
-      float ymin0 = det_ptr[n * 4 + 1];
-      float xmax0 = det_ptr[n * 4 + 2];
-      float ymax0 = det_ptr[n * 4 + 3];
-
-      float xmin1 = det_ptr[m * 4 + 0];
-      float ymin1 = det_ptr[m * 4 + 1];
-      float xmax1 = det_ptr[m * 4 + 2];
-      float ymax1 = det_ptr[m * 4 + 3];
-
-      float iou = IOU(xmin0, ymin0, xmax0, ymax0, xmin1, ymin1, xmax1, ymax1);
-
-      if (iou > iou_threshold) {
-        keep_idxs[j] = -1;
-      }
-    }
-  }
-}
-
-void SSDHead::Sort(std::vector<float>& probs, std::vector<int>& label_ids,
-                   std::vector<int>& anchor_idxs) {
-  std::vector<int> prob_idxs(probs.size());
-  std::iota(prob_idxs.begin(), prob_idxs.end(), 0);
-  std::sort(prob_idxs.begin(), prob_idxs.end(), [&](int i, int j) { return probs[i] > probs[j]; });
-  std::vector<float> _probs;
-  std::vector<int> _label_ids;
-  std::vector<int> _keep_idxs;
-  for (auto idx : prob_idxs) {
-    _probs.push_back(probs[idx]);
-    _label_ids.push_back(label_ids[idx]);
-    _keep_idxs.push_back(anchor_idxs[idx]);
-  }
-  probs = std::move(_probs);
-  label_ids = std::move(_label_ids);
-  anchor_idxs = std::move(_keep_idxs);
-}
-
 SSDHead::SSDHead(const Value& cfg) : MMDetection(cfg) {
   auto init = [&]() -> Result<void> {
     auto model = cfg["context"]["model"].get<Model>();
@@ -111,7 +37,7 @@ Result<Value> SSDHead::operator()(const Value& prep_res, const Value& infer_res)
   }
 }
 
-Result<Detections> SSDHead::GetBBoxes(const Value& prep_res, const Value& infer_res) {
+Result<Detections> SSDHead::GetBBoxes(const Value& prep_res, const Value& infer_res) const {
   auto dets = infer_res["dets"].get<Tensor>();
   auto scores = infer_res["labels"].get<Tensor>();
 
--- csrc/mmdeploy/codebase/mmdet/ssd_head.h
+++ csrc/mmdeploy/codebase/mmdet/ssd_head.h
@@ -12,24 +12,7 @@ class SSDHead : public MMDetection {
   explicit SSDHead(const Value& cfg);
 
   Result<Value> operator()(const Value& prep_res, const Value& infer_res);
-  Result<Detections> GetBBoxes(const Value& prep_res, const Value& infer_res);
-
- private:
-  // @brief Filter results using score threshold and topk candidates.
-  // scores (Tensor): The scores, shape (num_bboxes, K).
-  // probs: The scores after being filtered
-  // label_ids: The class labels
-  // anchor_idxs: The anchor indexes
-  static void FilterScoresAndTopk(Tensor& scores, float score_thr, int topk,
-                                  std::vector<float>& probs, std::vector<int>& label_ids,
-                                  std::vector<int>& anchor_idxs);
-  static float IOU(float xmin0, float ymin0, float xmax0, float ymax0, float xmin1, float ymin1,
-                   float xmax1, float ymax1);
-
-  static void Sort(std::vector<float>& probs, std::vector<int>& label_ids,
-                   std::vector<int>& anchor_idxs);
-
-  static void NMS(Tensor& dets, float iou_threshold, std::vector<int>& keep_idxs);
+  Result<Detections> GetBBoxes(const Value& prep_res, const Value& infer_res) const;
 
  private:
   float score_thr_{0.4f};
