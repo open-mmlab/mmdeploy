3d07feb54dcae507a660f673ae36ff1150f5a9bf
RunningLeon <mnsheng@yeah.net>
2022-10-27 15:53:34 +0800

    update

--- mmdeploy/mmcv/cnn/conv2d_adaptive_padding.py
+++ mmdeploy/mmcv/cnn/conv2d_adaptive_padding.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn.functional as F
 
 from mmdeploy.core import FUNCTION_REWRITER
-from mmdeploy.utils import Backend, is_dynamic_shape
+from mmdeploy.utils import Backend, is_dynamic_batch, is_dynamic_shape
 
 
 def compute_padding(input_size, kernel_size, stride, dilation):
@@ -32,6 +32,27 @@ def compute_padding(input_size, kernel_size, stride, dilation):
     return padded
 
 
+class AdaptivePadOp(torch.autograd.Function):
+    """Dummy adaptive pad op."""
+
+    @staticmethod
+    def forward(ctx, x, padded):
+        if padded is not None:
+            x = F.pad(x, padded)
+        return x
+
+    @staticmethod
+    def symbolic(g, x, padded):
+        if padded is None:
+            return g.op('Identity', x)
+        padded = g.op(
+            'Constant', value_t=torch.tensor(padded, dtype=torch.int64))
+        constant_value = g.op(
+            'Constant', value_t=torch.tensor(0, dtype=torch.int64))
+        return g.op(
+            'Pad', x, padded, constant_value, mode_s='constant', outputs=1)
+
+
 @FUNCTION_REWRITER.register_rewriter(
     func_name='mmcv.cnn.bricks.conv2d_adaptive_padding. \
         Conv2dAdaptivePadding.forward',
@@ -52,40 +73,12 @@ def conv2d_adaptive_padding__forward__tensorrt(ctx, self, x):
 
     deploy_cfg = ctx.cfg
     is_dynamic_flag = is_dynamic_shape(deploy_cfg)
-    if not is_dynamic_flag:
-
-        class AdaptivePadOp(torch.autograd.Function):
-            """Dummy adaptive pad op."""
-
-            @staticmethod
-            def forward(ctx, x):
-                padded = AdaptivePadOp.padded
-                if padded is not None:
-                    x = F.pad(x, padded)
-                return x
-
-            @staticmethod
-            def symbolic(g, x):
-                padded = AdaptivePadOp.padded
-                if padded is None:
-                    return g.op('Identity', x)
-                padded = g.op(
-                    'Constant',
-                    value_t=torch.tensor(padded, dtype=torch.int64))
-                constant_value = g.op(
-                    'Constant', value_t=torch.tensor(0, dtype=torch.int64))
-                return g.op(
-                    'Pad',
-                    x,
-                    padded,
-                    constant_value,
-                    mode_s='constant',
-                    outputs=1)
-
+    if (not is_dynamic_flag) or is_dynamic_batch(deploy_cfg):
         padded = compute_padding(x.shape[2:], self.weight.shape[2:],
                                  self.stride, self.dilation)
-        AdaptivePadOp.padded = padded
-        x = AdaptivePadOp.apply(x)
+        if padded is not None:
+            padded = [int(_) for _ in padded]
+        x = AdaptivePadOp.apply(x, padded)
         return F.conv2d(x, self.weight, self.bias, self.stride, self.padding,
                         self.dilation, self.groups)
     else:
