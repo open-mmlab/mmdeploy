2fa509515433c08f2538c42698a8339bf4e2b3f7
RunningLeon <mnsheng@yeah.net>
2022-10-27 16:20:39 +0800

    cherry-pick from commit 197a7ad

--- mmdeploy/mmcv/cnn/__init__.py
+++ mmdeploy/mmcv/cnn/__init__.py
@@ -1,10 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .conv2d_adaptive_padding import (
-    AdaptivePadOp, conv2d_adaptive_padding__forward__tensorrt)
-from .transformer import (MultiHeadAttentionop,
-                          multiheadattention__forward__ncnn)
+from . import context_block  # noqa: F401,F403
+from . import conv2d_adaptive_padding  # noqa: F401,F403
+from . import hsigmoid  # noqa: F401,F403
+from . import hswish  # noqa: F401,F403
+from .transformer import MultiHeadAttentionop
 
-__all__ = [
-    'multiheadattention__forward__ncnn', 'MultiHeadAttentionop',
-    'conv2d_adaptive_padding__forward__tensorrt', 'AdaptivePadOp'
-]
+__all__ = ['conv2d_adaptive_padding', 'MultiHeadAttentionop']
