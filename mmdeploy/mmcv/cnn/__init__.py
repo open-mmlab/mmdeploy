197a7ad425d0df2989982a4b6a982282427db2fc
RunningLeon <mnsheng@yeah.net>
2022-10-27 16:20:39 +0800

    fix efficientnet from mmcls (#1260)
    * fix efficientnet from mmcls
    
    * update

--- mmdeploy/mmcv/cnn/__init__.py
+++ mmdeploy/mmcv/cnn/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from . import context_block  # noqa: F401,F403
+from . import conv2d_adaptive_padding  # noqa: F401,F403
 from . import hsigmoid  # noqa: F401,F403
 from . import hswish  # noqa: F401,F403
-from .conv2d_adaptive_padding import AdaptivePadOp
 from .transformer import MultiHeadAttentionop
 
-__all__ = ['AdaptivePadOp', 'MultiHeadAttentionop']
+__all__ = ['conv2d_adaptive_padding', 'MultiHeadAttentionop']
