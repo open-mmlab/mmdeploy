2e10bc2b68a84800f4b44f53de394d7b3d38b039
RunningLeon <mnsheng@yeah.net>
2022-10-31 17:40:41 +0800

    update mmpose rewritings

--- mmdeploy/codebase/mmpose/models/heads/mspn_head.py
+++ mmdeploy/codebase/mmpose/models/heads/mspn_head.py
@@ -4,21 +4,20 @@ from mmdeploy.core import FUNCTION_REWRITER
 
 
 @FUNCTION_REWRITER.register_rewriter(
-    'mmpose.models.heads.heatmap_heads.MSPNHead.predict')
-def mspn_head__predict(ctx, self, feats, batch_data_samples, test_cfg=None):
-    """Rewrite `predict` of HeatmapHead for default backend.
+    'mmpose.models.heads.heatmap_heads.CPMHead.forward')
+@FUNCTION_REWRITER.register_rewriter(
+    'mmpose.models.heads.heatmap_heads.MSPNHead.forward')
+def mspn_head__forward(ctx, self, feats):
+    """Rewrite `forward` of MSPNHead and CPMHead for default backend.
 
     1. skip heatmaps decoding and return heatmaps directly.
 
     Args:
         feats (tuple[Tensor]): Input features.
-        batch_data_samples (list[SampleList]): Data samples contain
-            image meta information.
-        test_cfg (ConfigType): test config.
 
     Returns:
         output_heatmap (torch.Tensor): Output heatmaps.
     """
-    msmu_batch_heatmaps = self.forward(feats)
+    msmu_batch_heatmaps = ctx.origin_func(self, feats)
     batch_heatmaps = msmu_batch_heatmaps[-1]
     return batch_heatmaps
--- mmdeploy/codebase/mmpose/models/pose_estimators/base.py
+++ mmdeploy/codebase/mmpose/models/pose_estimators/base.py
@@ -4,33 +4,15 @@ from mmdeploy.core import FUNCTION_REWRITER
 
 @FUNCTION_REWRITER.register_rewriter(
     'mmpose.models.pose_estimators.base.BasePoseEstimator.forward')
-def base_pose_estimator__forward(ctx,
-                                 self,
-                                 inputs,
-                                 data_samples=None,
-                                 mode='predict',
-                                 **kwargs):
-    """Rewrite `forward_test` of TopDown for default backend.'.
+def base_pose_estimator__forward(ctx, self, inputs, *args, **kwargs):
+    """Rewrite `forward` of TopDown for default backend.'.
 
-    1. only support mode='predict'.
-    2. create data_samples if necessary
+    1.directly call _forward of subclass.
 
     Args:
         inputs (torch.Tensor[NxCxHxW]): Input images.
-        data_samples (SampleList | None): Data samples contain
-            image meta information.
 
     Returns:
         torch.Tensor: The predicted heatmaps.
     """
-    if data_samples is None:
-        from mmpose.structures import PoseDataSample
-        _, c, h, w = [int(_) for _ in inputs.shape]
-        metainfo = dict(
-            img_shape=(h, w, c),
-            input_size=(w, h),
-            heatmap_size=self.cfg.codec.heatmap_size)
-        data_sample = PoseDataSample(metainfo=metainfo)
-        data_samples = [data_sample]
-
-    return self.predict(inputs, data_samples)
+    return self._forward(inputs)
