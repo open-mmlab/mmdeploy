3e5d23e22253cafe332ed3897d0bcf15e16e5a4e
irexyc <xinchen.tju@gmail.com>
2022-10-31 15:20:30 +0800

    fix comments

--- mmdeploy/codebase/mmaction/models/recognizers/__init__.py
+++ mmdeploy/codebase/mmaction/models/recognizers/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
-from . import base
-from . import recognizer2d
-from . import recognizer3d
+from . import base  # noqa: F401,F403
+from . import recognizer2d  # noqa: F401,F403
+from . import recognizer3d  # noqa: F401,F403
