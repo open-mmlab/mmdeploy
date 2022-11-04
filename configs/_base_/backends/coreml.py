4da04fff3aff3c1e2d9d71253ad8c37848670c50
Jelle Maas <typiqally@gmail.com>
2022-11-04 10:32:41 +0100

    Add common configuration for Core ML backend

--- configs/_base_/backends/coreml.py
+++ configs/_base_/backends/coreml.py
@@ -1,1 +1,9 @@
-backend_config = dict(type='coreml', convert_to='mlprogram')
+backend_config = dict(
+    type='coreml',
+    convert_to='mlprogram',  # mlprogram or neuralnetwork
+    common_config=dict(
+        compute_precision='FLOAT32',  # FLOAT16 or FLOAT32, see coremltools.precision
+        minimum_deployment_target='iOS16',  # iOS15, iOS16, etc., see coremltools.target
+        skip_model_load=False
+    ),
+)
