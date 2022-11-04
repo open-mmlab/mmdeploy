3d5bf2096a56ed518104674a09c3c55547e7356f
AllentDan <AllentDan@yeah.net>
2022-11-04 14:40:29 +0800

    remote partition config

--- configs/mmdet/detection/detection_rknn_static-320x320.py
+++ configs/mmdet/detection/detection_rknn_static-320x320.py
@@ -7,15 +7,15 @@ codebase_config = dict(model_type='rknn')
 backend_config = dict(input_size_list=[[3, 320, 320]])
 
 # yolov3, yolox
-partition_config = dict(
-    type='rknn',  # the partition policy name
-    apply_marks=True,  # should always be set to True
-    partition_cfg=[
-        dict(
-            save_file='model.onnx',  # name to save the partitioned onnx
-            start=['detector_forward:input'],  # [mark_name:input, ...]
-            end=['yolo_head:input'])  # [mark_name:output, ...]
-    ])
+# partition_config = dict(
+#     type='rknn',  # the partition policy name
+#     apply_marks=True,  # should always be set to True
+#     partition_cfg=[
+#         dict(
+#             save_file='model.onnx',  # name to save the partitioned onnx
+#             start=['detector_forward:input'],  # [mark_name:input, ...]
+#             end=['yolo_head:input'])  # [mark_name:output, ...]
+#     ])
 
 # # retinanet, ssd, fsaf
 # partition_config = dict(
