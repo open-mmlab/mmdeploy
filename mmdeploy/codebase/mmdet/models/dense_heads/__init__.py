b772d66dbf2200af660dd1398513b46adc2e4c76
hanrui1sensetime <hanrui1@sensetime.com>
2022-11-07 20:18:41 +0800

    remove unused rewriter

--- mmdeploy/codebase/mmdet/models/dense_heads/__init__.py
+++ mmdeploy/codebase/mmdet/models/dense_heads/__init__.py
@@ -2,8 +2,7 @@
 from . import detr_head
 from .base_dense_head import (base_dense_head__predict_by_feat,
                               base_dense_head__predict_by_feat__ncnn)
-from .centernet_head import (centernet_head__decode_heatmap__default,
-                             centernet_head__predict_by_feat__default)
+from .centernet_head import centernet_head__predict_by_feat__default
 from .fovea_head import fovea_head__predict_by_feat
 from .gfl_head import gfl_head__predict_by_feat
 from .reppoints_head import reppoints_head__predict_by_feat
@@ -15,12 +14,18 @@ from .yolox_head import (yolox_head__predict_by_feat,
                          yolox_head__predict_by_feat__ncnn)
 
 __all__ = [
-    'rpn_head__predict_by_feat', 'rpn_head__get_bboxes__ncnn',
-    'yolov3_head__predict_by_feat', 'yolov3_head__predict_by_feat__ncnn',
-    'yolox_head__predict_by_feat', 'base_dense_head__predict_by_feat',
-    'fovea_head__predict_by_feat', 'base_dense_head__predict_by_feat__ncnn',
-    'yolox_head__predict_by_feat__ncnn', 'gfl_head__predict_by_feat',
-    'reppoints_head__predict_by_feat', 'detr_head',
-    'centernet_head__predict_by_feat__default', 'rtmdet_head__predict_by_feat',
-    'centernet_head__decode_heatmap__default'
+    'rpn_head__predict_by_feat',
+    'rpn_head__get_bboxes__ncnn',
+    'yolov3_head__predict_by_feat',
+    'yolov3_head__predict_by_feat__ncnn',
+    'yolox_head__predict_by_feat',
+    'base_dense_head__predict_by_feat',
+    'fovea_head__predict_by_feat',
+    'base_dense_head__predict_by_feat__ncnn',
+    'yolox_head__predict_by_feat__ncnn',
+    'gfl_head__predict_by_feat',
+    'reppoints_head__predict_by_feat',
+    'detr_head',
+    'centernet_head__predict_by_feat__default',
+    'rtmdet_head__predict_by_feat',
 ]
