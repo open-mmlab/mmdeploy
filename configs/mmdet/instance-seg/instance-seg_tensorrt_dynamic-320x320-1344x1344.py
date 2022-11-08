60bf644a04e3ae951f28ed08bafaa23347704574
hanrui1sensetime <hanrui1@sensetime.com>
2022-11-08 11:33:33 +0800

    remove debug config

--- configs/mmdet/instance-seg/instance-seg_tensorrt_dynamic-320x320-1344x1344.py
+++ configs/mmdet/instance-seg/instance-seg_tensorrt_dynamic-320x320-1344x1344.py
@@ -4,7 +4,7 @@ _base_ = [
 ]
 
 backend_config = dict(
-    common_config=dict(max_workspace_size=1 << 33),
+    common_config=dict(max_workspace_size=1 << 30),
     model_inputs=[
         dict(
             input_shapes=dict(
