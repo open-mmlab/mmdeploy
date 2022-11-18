fac2141c80674e3473526a620df2b9f134a8a5d1
RunningLeon <mnsheng@yeah.net>
2022-11-18 12:23:02 +0800

    add cfgoptions in reg test

--- tools/regression_test.py
+++ tools/regression_test.py
@@ -487,6 +487,14 @@ def get_backend_fps_metric(deploy_cfg_path: str, model_cfg_path: Path,
     ]
 
     codebase_name = get_codebase(str(deploy_cfg_path)).value
+    # to stop Dataloader OOM in docker CI
+    if codebase_name not in ['medit', 'mmocr']:
+        cfg_options = 'test_dataloader.num_workers=0 ' \
+                      'test_dataloader.persistent_workers=False ' \
+                      'val_dataloader.num_workers=0 ' \
+                      'val_dataloader.persistent_workers=False '
+        cmd_lines.append(f'--cfg-options {cfg_options}')
+
     # Test backend
     return_code = run_cmd(cmd_lines, log_path)
     fps, backend_metric, test_pass = get_fps_metric(return_code,
