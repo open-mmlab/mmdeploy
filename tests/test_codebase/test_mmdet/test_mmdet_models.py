6eaef158cdf2685880fb337ef0ffb0f6255abfee
Donggeun Yu <ddonggeunn@gmail.com>
2022-11-28 23:12:28 +0900

    Resolved test code conflict issue

--- tests/test_codebase/test_mmdet/test_mmdet_models.py
+++ tests/test_codebase/test_mmdet/test_mmdet_models.py
@@ -1908,7 +1908,6 @@ def test_detrhead_get_bboxes(backend_type: Backend, ir_type: str):
     """Test get_bboxes rewrite of base dense head."""
     check_backend(backend_type)
     dense_head = get_detrhead_model()
-
     dense_head.cpu().eval()
     s = 128
     img_metas = [{
@@ -1917,86 +1916,27 @@ def test_detrhead_get_bboxes(backend_type: Backend, ir_type: str):
         'img_shape': (s, s, 3)
     }]
 
-    output_names = ['dets', 'labels']
+    deploy_cfg = get_deploy_cfg(backend_type, ir_type)
 
-    deploy_cfg = mmcv.Config(
-        dict(
-            backend_config=dict(type=backend_type.value),
-            onnx_config=dict(output_names=output_names, input_shape=None),
-            codebase_config=dict(
-                type='mmdet',
-                task='ObjectDetection',
-                post_processing=dict(
-                    score_threshold=0.05,
-                    iou_threshold=0.5,
-                    max_output_boxes_per_class=200,
-                    pre_top_k=-1,
-                    keep_top_k=100,
-                    background_label_id=-1,
-                ))))
-
-    # the cls_score's size: (1, 4, 32, 32), (1, 4, 16, 16),
-    # (1, 4, 8, 8), (1, 4, 4, 4), (1, 4, 2, 2).
-    # the bboxes's size: (1, 4, 32, 32), (1, 4, 16, 16),
-    # (1, 4, 8, 8), (1, 4, 4, 4), (1, 4, 2, 2)
     seed_everything(1234)
-    cls_score = [
-        torch.rand(1, 1, pow(2, i), pow(2, i)) for i in range(5, 0, -1)
-    ]
+    cls_score = [[torch.rand(1, 100, 5) for i in range(5, 0, -1)]]
     seed_everything(5678)
-    bboxes = [torch.rand(1, 4, pow(2, i), pow(2, i)) for i in range(5, 0, -1)]
-
-    seed_everything(9101)
-    shape_preds = [
-        torch.rand(1, 2, pow(2, i), pow(2, i)) for i in range(5, 0, -1)
-    ]
-
-    seed_everything(1213)
-    loc_preds = [
-        torch.rand(1, 1, pow(2, i), pow(2, i)) for i in range(5, 0, -1)
-    ]
-
-    # to get outputs of pytorch model
-    model_inputs = {
-        'cls_scores': cls_score,
-        'bbox_preds': bboxes,
-        'shape_preds': shape_preds,
-        'loc_preds': loc_preds,
-        'img_metas': img_metas
-    }
-    model_outputs = get_model_outputs(dense_head, 'get_bboxes', model_inputs)
+    bboxes = [[torch.rand(1, 100, 4) for i in range(5, 0, -1)]]
 
     # to get outputs of onnx model after rewrite
     img_metas[0]['img_shape'] = torch.Tensor([s, s])
-    wrapped_model = WrapModel(
-        dense_head, 'get_bboxes', img_metas=img_metas, with_nms=True)
+    wrapped_model = WrapModel(dense_head, 'get_bboxes', img_metas=img_metas)
     rewrite_inputs = {
-        'cls_scores': cls_score,
-        'bbox_preds': bboxes,
-        'shape_preds': shape_preds,
-        'loc_preds': loc_preds,
+        'all_cls_scores_list': cls_score,
+        'all_bbox_preds_list': bboxes,
     }
-    rewrite_outputs, is_backend_output = get_rewrite_outputs(
+    rewrite_outputs, _ = get_rewrite_outputs(
         wrapped_model=wrapped_model,
         model_inputs=rewrite_inputs,
-        deploy_cfg=deploy_cfg)
+        deploy_cfg=deploy_cfg,
+        run_with_backend=False)
 
-    if is_backend_output:
-        if isinstance(rewrite_outputs, dict):
-            rewrite_outputs = convert_to_list(rewrite_outputs, output_names)
-        for model_output, rewrite_output in zip(model_outputs[0],
-                                                rewrite_outputs):
-            model_output = model_output.squeeze().cpu().numpy()
-            rewrite_output = rewrite_output.squeeze()
-            # hard code to make two tensors with the same shape
-            # rewrite and original codes applied different nms strategy
-            assert np.allclose(
-                model_output[:rewrite_output.shape[0]],
-                rewrite_output,
-                rtol=1e-03,
-                atol=1e-05)
-    else:
-        assert rewrite_outputs is not None
+    assert rewrite_outputs is not None
 
 
 def get_guided_anchor_head_model():
