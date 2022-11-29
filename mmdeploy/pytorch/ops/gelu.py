3233eefab5e8527c7301ff61dbfa5bbe6bc341a7
grimoire <yaoqian@sensetime.com>
2022-11-29 12:10:52 +0800

    fix gelu torch>1.12

--- mmdeploy/pytorch/ops/gelu.py
+++ mmdeploy/pytorch/ops/gelu.py
@@ -1,11 +1,18 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from torch.onnx import symbolic_helper
 
 from mmdeploy.core import SYMBOLIC_REWRITER
 from mmdeploy.utils import Backend
 
 
-@SYMBOLIC_REWRITER.register_symbolic(
-    'gelu', is_pytorch=True, arg_descriptors=['v'], backend=Backend.NCNN.value)
-def gelu__ncnn(ctx, g, self):
-    """Support export GELU with ncnn backend."""
+@symbolic_helper.parse_args('v')
+def gelu__ncnn_pt111(g, self):
+    """gelu for torch<=1.12."""
     return g.op('mmdeploy::Gelu', self)
+
+
+@SYMBOLIC_REWRITER.register_symbolic(
+    'gelu', is_pytorch=True, backend=Backend.NCNN.value)
+def gelu__ncnn(ctx, g, self, approximate: str = 'none'):
+    """Support export GELU with ncnn backend."""
+    return gelu__ncnn_pt111(g, self)
