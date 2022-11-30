a11402775c7794322ea60d35c9400ead4970a4e4
pppppM <gjf_mail@126.com>
2022-11-30 14:48:21 +0800

    add openvino quantize deploy configs

---
+++ configs/mmdet/detection/detection_openvino_dynamic-800x1344-quantize.py
@@ -0,0 +1,27 @@
+_base_ = ['../_base_/base_openvino_dynamic-800x1344.py']
+
+# TODO save in mmrazor's checkpoint
+quantizer = dict(
+    type='mmrazor.OpenvinoQuantizer',
+    is_qat=False,
+    skipped_methods=[
+        'mmdet.models.dense_heads.base_dense_head'
+        '.BaseDenseHead.predict_by_feat',
+    ],
+    prepare_custom_config_dict=None,
+    convert_custom_config_dict=None,
+    qconfig=dict(
+        qtype='affine',
+        w_observer=dict(type='mmrazor.MSEObserver'),
+        a_observer=dict(type='mmrazor.EMAMSEObserver'),
+        w_fake_quant=dict(type='mmrazor.FakeQuantize'),
+        a_fake_quant=dict(type='mmrazor.FakeQuantize'),
+        w_qscheme=dict(
+            bit=8,
+            is_symmetry=True,
+            is_per_channel=True,
+            is_pot_scale=False,
+        ),
+        a_qscheme=dict(
+            bit=8, is_symmetry=False, is_per_channel=False,
+            is_pot_scale=False)))
--- mmdeploy/core/__init__.py
+++ mmdeploy/core/__init__.py
@@ -1,3 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .exporters import *  # noqa: F401,F403
 from .optimizers import *  # noqa: F401,F403
 from .rewriters import *  # noqa: F401,F403
