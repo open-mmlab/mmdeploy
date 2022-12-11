df5bf5654237b54e50a6ca0555ca2f7204e1a032
grimoire <streetyao@live.com>
2022-12-11 11:31:23 +0800

    update init

--- mmdeploy/codebase/mmdet3d/__init__.py
+++ mmdeploy/codebase/mmdet3d/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .deploy import MMDetection3d, VoxelDetection
+from .deploy import MMDetection3d, VoxelDetectionTask
 
-__all__ = ['MMDetection3d', 'VoxelDetection']
+__all__ = ['MMDetection3d', 'VoxelDetectionTask']
