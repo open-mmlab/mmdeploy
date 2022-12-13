0b6b9ee69ff0bf836e2f5b206ee6326b1bc6fc6b
hanrui1sensetime <83800577+hanrui1sensetime@users.noreply.github.com>
2022-12-13 19:20:59 +0800

    sync #1493 to support TorchAllocator as TensorRT Gpu Allocator and fix DCNv2 tensorrt plugin error (#1519)

---
+++ mmdeploy/backend/tensorrt/torch_allocator.py
@@ -0,0 +1,63 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import tensorrt as trt
+import torch
+
+from mmdeploy.utils import get_root_logger
+
+
+class TorchAllocator(trt.IGpuAllocator):
+    """PyTorch Cuda Allocator Wrapper."""
+
+    def __init__(self, device_id: int = 0) -> None:
+        super().__init__()
+
+        self.device_id = device_id
+        self.mems = set()
+
+    def __del__(self):
+        """destructor."""
+        mems = self.mems.copy()
+        (self.deallocate(mem) for mem in mems)
+
+    def allocate(self: trt.IGpuAllocator, size: int, alignment: int,
+                 flags: int) -> int:
+        """allocate gpu memory.
+
+        Args:
+            self (trt.IGpuAllocator): gpu allocator
+            size (int): memory size.
+            alignment (int): memory alignment.
+            flags (int): flags.
+
+        Returns:
+            int: memory address.
+        """
+        torch_stream = torch.cuda.current_stream(self.device_id)
+        logger = get_root_logger()
+        logger.debug(f'allocate {size} memory with TorchAllocator.')
+        assert alignment >= 0
+        if alignment > 0:
+            size = size | (alignment - 1) + 1
+        mem = torch.cuda.caching_allocator_alloc(
+            size, device=self.device_id, stream=torch_stream)
+        self.mems.add(mem)
+        return mem
+
+    def deallocate(self: trt.IGpuAllocator, memory: int) -> bool:
+        """deallocate memory.
+
+        Args:
+            self (trt.IGpuAllocator): gpu allocator
+            memory (int): memory address.
+
+        Returns:
+            bool: deallocate success.
+        """
+        logger = get_root_logger()
+        logger.debug(f'deallocate {memory} with TorchAllocator.')
+        if memory not in self.mems:
+            return False
+
+        torch.cuda.caching_allocator_delete(memory)
+        self.mems.discard(memory)
+        return True
