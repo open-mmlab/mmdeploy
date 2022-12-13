a4a4e78a655143de96e296fe07b0f9e076eda263
pppppM <gjf_mail@126.com>
2022-12-13 19:44:30 +0800

    update is qat and  fake quant symbolic

--- configs/mmdet/detection/detection_openvino_dynamic-800x1344-quantize.py
+++ configs/mmdet/detection/detection_openvino_dynamic-800x1344-quantize.py
@@ -3,7 +3,6 @@ _base_ = ['../_base_/base_openvino_dynamic-800x1344.py']
 # TODO save in mmrazor's checkpoint
 quantizer = dict(
     type='mmrazor.OpenvinoQuantizer',
-    is_qat=False,
     skipped_methods=[
         'mmdet.models.dense_heads.base_dense_head'
         '.BaseDenseHead.predict_by_feat',
--- mmdeploy/pytorch/symbolics/fake_quant.py
+++ mmdeploy/pytorch/symbolics/fake_quant.py
@@ -4,7 +4,7 @@ from mmdeploy.core import SYMBOLIC_REWRITER
 
 @SYMBOLIC_REWRITER.register_symbolic(
     'fake_quantize_per_tensor_affine', is_pytorch=True)
-def fake_quantize_per_tensor_affine__default(ctx, g, x, scale, zero_point,
+def fake_quantize_per_tensor_affine__default(g, x, scale, zero_point,
                                              quant_min, quant_max):
     """Register ncnn symbolic function for `adaptive_avg_pool2d`.
 
@@ -16,7 +16,7 @@ def fake_quantize_per_tensor_affine__default(ctx, g, x, scale, zero_point,
 
 @SYMBOLIC_REWRITER.register_symbolic(
     'fake_quantize_per_channel_affine', is_pytorch=True)
-def fake_quantize_per_channel_affine__default(ctx, g, x, scale, zero_point,
+def fake_quantize_per_channel_affine__default( g, x, scale, zero_point,
                                               ch_axis, quant_min, quant_max):
     """Register ncnn symbolic function for `adaptive_avg_pool2d`.
 
