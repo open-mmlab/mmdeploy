03c992e3af58219f912deb18d07db9ecbf413e6b
DDGRCF <969609856@qq.com>
2022-12-14 00:05:22 +0800

    fix: fix unreasonable rewrite linspace__onnx

--- mmdeploy/pytorch/functions/linspace.py
+++ mmdeploy/pytorch/functions/linspace.py
@@ -1,6 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Optional
-
 import torch
 from torch.types import Number
 
@@ -8,17 +6,19 @@ from mmdeploy.core import FUNCTION_REWRITER
 
 
 @FUNCTION_REWRITER.register_rewriter(func_name='torch.linspace')
-@FUNCTION_REWRITER.register_rewriter(func_name='torch.Tensor.linspace')
 def linspace__onnx(ctx,
                    start: Number,
                    end: Number,
-                   steps: Optional[int] = None,
+                   steps: int = None,
                    **kwargs):
     """Rewrite `linspace` for onnxruntime."""
     steps = 100 if steps is None else steps
+    dtype = kwargs.pop('dtype', torch.float32)
+    dtype = dtype if dtype else torch.float32
     if steps == 1:
-        output = torch.arange(start, end + 1, **kwargs)[:steps]
+        output = torch.arange(start, end + 1, dtype=dtype, **kwargs)[:steps]
     else:
-        output = torch.arange(start, end + 1, (end - start) * 1. / (steps - 1),
-                              **kwargs)[:steps]
+        output = torch.arange(
+            start, end + 1, (end - start) / (steps - 1), dtype=dtype,
+            **kwargs)[:steps]
     return output
