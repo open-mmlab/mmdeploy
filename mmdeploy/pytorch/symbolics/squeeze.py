41d2a96f30224a283c246e5b19bbaa2a8408b533
AllentDan <AllentDan@yeah.net>
2022-12-15 12:35:14 +0800

    better way to get opset version

--- mmdeploy/pytorch/symbolics/squeeze.py
+++ mmdeploy/pytorch/symbolics/squeeze.py
@@ -3,7 +3,6 @@ import torch
 import torch.onnx.symbolic_helper as sym_help
 
 from mmdeploy.core import SYMBOLIC_REWRITER
-from mmdeploy.utils.config_utils import get_ir_config
 
 
 @SYMBOLIC_REWRITER.register_symbolic('squeeze', is_pytorch=True)
@@ -21,8 +20,7 @@ def squeeze__default(g, self, dim=None):
     else:
         dims = [sym_help._get_const(dim, 'i', 'dim')]
 
-    ctx = SYMBOLIC_REWRITER.get_context('squeeze')
-    if get_ir_config(ctx.cfg)['opset_version'] >= 13:
+    if g.opset >= 13:
         axes = g.op('Constant', value_t=torch.tensor(dims, dtype=torch.long))
         return g.op('Squeeze', self, axes)
 
