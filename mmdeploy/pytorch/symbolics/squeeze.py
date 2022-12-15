69060035e1ed1845b18a10fb560110ba73ab8e67
AllentDan <AllentDan@yeah.net>
2022-12-15 10:15:24 +0800

    fix squeeze export and unsqueeze pass for opset 13

--- mmdeploy/pytorch/symbolics/squeeze.py
+++ mmdeploy/pytorch/symbolics/squeeze.py
@@ -1,7 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import torch
 import torch.onnx.symbolic_helper as sym_help
 
 from mmdeploy.core import SYMBOLIC_REWRITER
+from mmdeploy.utils.config_utils import get_ir_config
 
 
 @SYMBOLIC_REWRITER.register_symbolic('squeeze', is_pytorch=True)
@@ -18,4 +20,10 @@ def squeeze__default(g, self, dim=None):
                 dims.append(i)
     else:
         dims = [sym_help._get_const(dim, 'i', 'dim')]
+
+    ctx = SYMBOLIC_REWRITER.get_context('squeeze')
+    if get_ir_config(ctx.cfg)['opset_version'] >= 13:
+        axes = g.op('Constant', value_t=torch.tensor(dims, dtype=torch.long))
+        return g.op('Squeeze', self, axes)
+
     return g.op('Squeeze', self, axes_i=dims)
