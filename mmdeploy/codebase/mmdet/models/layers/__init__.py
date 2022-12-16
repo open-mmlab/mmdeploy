862b29ab8fb07c5fd98e7fc5efa0a20f6881268d
Li-Hongda <1318159980@qq.com>
2022-12-16 22:17:33 +0800

    fix lint

--- mmdeploy/codebase/mmdet/models/layers/__init__.py
+++ mmdeploy/codebase/mmdet/models/layers/__init__.py
@@ -1,5 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from . import matrix_nms
-from .bbox_nms import multiclass_nms
 
-__all__ = ['multiclass_nms']
+from mmdeploy.mmcv.ops import multiclass_nms  # noqa: F401, F403
+from . import matrix_nms  # noqa: F401, F403
+
