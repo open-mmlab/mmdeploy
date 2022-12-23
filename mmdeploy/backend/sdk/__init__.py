d113a5f1c7350b17c5d28156d17d1a11efc78c9d
q.yao <streetyao@live.com>
2022-12-23 12:06:32 +0800

    [Refactor] refactor is_available, check_env (#1540)
    * refactor is available
    
    * remove try catch in apis
    
    * fix trt check env
    
    * fix ops_info
    
    * update default value
    
    * remove backend list
    
    * optimial pycuda
    
    * update requirement, check env for rknn

--- mmdeploy/backend/sdk/__init__.py
+++ mmdeploy/backend/sdk/__init__.py
@@ -1,33 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-import importlib
-import os
-import sys
-
-from mmdeploy.utils import get_file_path
 from .backend_manager import SDKManager
 
-_is_available = False
-
-module_name = 'mmdeploy_python'
-
-candidates = [
-    f'../../../build/lib/{module_name}.*.so',
-    f'../../../build/bin/*/{module_name}.*.pyd'
-]
-
-lib_path = get_file_path(os.path.dirname(__file__), candidates)
-
-if lib_path:
-    lib_dir = os.path.dirname(lib_path)
-    sys.path.append(lib_dir)
-
-if importlib.util.find_spec(module_name) is not None:
-    _is_available = True
-
-
-def is_available() -> bool:
-    return _is_available
-
+_BackendManager = SDKManager
+is_available = _BackendManager.is_available
+build_wrapper = _BackendManager.build_wrapper
 
 __all__ = ['SDKManager']
 
