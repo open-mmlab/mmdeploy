0d6aca9b82a36e1873c03e1822342bbe6cb6a55b
hanrui1sensetime <hanrui1@sensetime.com>
2022-12-28 11:04:19 +0800

    fix test_ops with teardown and skip

--- tests/test_ops/test_ops.py
+++ tests/test_ops/test_ops.py
@@ -16,6 +16,15 @@ TEST_TENSORRT = TestTensorRTExporter()
 TEST_NCNN = TestNCNNExporter()
 
 
+@pytest.fixture(scope="function")
+def disable_cudnn():
+    cudnn_enable = torch.backends.cudnn.enabled
+    torch.backends.cudnn.enabled = False
+
+    yield
+    torch.backends.cudnn.enabled = cudnn_enable
+
+
 @pytest.mark.parametrize('backend', [TEST_TENSORRT])
 @pytest.mark.parametrize('pool_h,pool_w,spatial_scale,sampling_ratio',
                          [(2, 2, 1.0, 2), (4, 4, 2.0, 4)])
@@ -264,7 +273,8 @@ def test_deform_conv(backend,
 @pytest.mark.parametrize('dynamic_export', [True, False])
 @pytest.mark.parametrize('fp16_mode', [True, False])
 @pytest.mark.parametrize('n, c, h, w', [(2, 3, 10, 10)])
-def test_instance_norm(backend,
+def test_instance_norm(disable_cudnn,
+                       backend,
                        dynamic_export,
                        fp16_mode,
                        n,
@@ -767,8 +777,8 @@ def test_gather(backend,
     # so the ncnn_outputs has 2 dimensions, not 1.
     import importlib
 
-    assert importlib.util.find_spec('onnxruntime') is not None, 'onnxruntime \
-         not installed.'
+    if importlib.util.find_spec('onnxruntime') is None:
+        pytest.skip(f'onnxruntime not installed.')
 
     import numpy as np
     import onnxruntime
@@ -781,7 +791,7 @@ def test_gather(backend,
                 np.array(indice[0], dtype=np.int64)
             ])))
     model_outputs = [model_output for model_output in model_outputs]
-
+    model_outputs = [data[indice[0]]]
     ncnn_outputs = ncnn_model(
         dict(zip(input_names, [data.float(), indice.float()])))
     ncnn_outputs = [ncnn_outputs[name] for name in output_names]
