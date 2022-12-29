1fdb9312351e13246e081658f9a5447a3da68e61
lvhan028 <lvhan_028@163.com>
2022-12-29 11:51:10 +0800

    add profiler to cxx demos

--- demo/csrc/cpp/classifier.cxx
+++ demo/csrc/cpp/classifier.cxx
@@ -1,32 +1,48 @@
 
 #include "mmdeploy/classifier.hpp"
 
+#include <iomanip>
+#include <iostream>
 #include <string>
 
 #include "opencv2/imgcodecs/imgcodecs.hpp"
 
 int main(int argc, char* argv[]) {
-  if (argc != 4) {
-    fprintf(stderr, "usage:\n  image_classification device_name model_path image_path\n");
+  if (argc < 4) {
+    std::cerr << "usage:" << std::endl
+              << "  ./bin/classifier device_name sdk_model_path image_path"
+                 " [--profile]"
+              << std::endl;
     return 1;
   }
   auto device_name = argv[1];
   auto model_path = argv[2];
   auto image_path = argv[3];
+  auto profile = argc > 4 ? std::string("--profile") == argv[argc - 1] : false;
+
   cv::Mat img = cv::imread(image_path);
   if (!img.data) {
-    fprintf(stderr, "failed to load image: %s\n", image_path);
+    std::cerr << "failed to load image: " << image_path;
     return 1;
   }
 
   mmdeploy::Model model(model_path);
-  mmdeploy::Classifier classifier(model, mmdeploy::Device{device_name, 0});
+  mmdeploy::Profiler profiler("profile.bin");
+  mmdeploy::Context context;
+
+  context.Add("model", mmdeploy::Model{model_path});
+  context.Add(mmdeploy::Device{device_name, 0});
+  if (profile) {
+    context.Add(profiler);
+  }
+
+  mmdeploy::Classifier classifier(model, context);
 
   auto res = classifier.Apply(img);
 
   for (const auto& cls : res) {
-    fprintf(stderr, "label: %d, score: %.4f\n", cls.label_id, cls.score);
+    std::cout << "label: " << cls.label_id << ", score: " << std::fixed << std::setprecision(2)
+              << cls.score << std::endl;
   }
-
   return 0;
 }
