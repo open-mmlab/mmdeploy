baa86aa4a5e7b0dd775fdc8634141ea8984fea15
hanrui1sensetime <83800577+hanrui1sensetime@users.noreply.github.com>
2022-12-30 14:27:41 +0800

    [Fix] Fix batch inference error for Mask R-CNN (#1576)
    * sync pr1575 to dev-1.x
    
    * only test_img accept list input

--- mmdeploy/apis/visualize.py
+++ mmdeploy/apis/visualize.py
@@ -11,7 +11,7 @@ from mmdeploy.utils import Backend, get_backend, get_input_shape, load_config
 def visualize_model(model_cfg: Union[str, mmengine.Config],
                     deploy_cfg: Union[str, mmengine.Config],
                     model: Union[str, Sequence[str]],
-                    img: Union[str, np.ndarray],
+                    img: Union[str, np.ndarray, Sequence[str]],
                     device: str,
                     backend: Optional[Backend] = None,
                     output_file: Optional[str] = None,
@@ -35,8 +35,9 @@ def visualize_model(model_cfg: Union[str, mmengine.Config],
         model_cfg (str | mmengine.Config): Model config file or Config object.
         deploy_cfg (str | mmengine.Config): Deployment config file or Config
             object.
-        model (str | list[str], BaseSubtask): Input model or file(s).
-        img (str | np.ndarray): Input image file or numpy array for inference.
+        model (str | Sequence[str]): Input model or file(s).
+        img (str | np.ndarray | Sequence[str]): Input image file or numpy array
+            for inference.
         device (str): A string specifying device type.
         backend (Backend): Specifying backend type, defaults to `None`.
         output_file (str): Output file to save visualized image, defaults to
@@ -84,10 +85,13 @@ def visualize_model(model_cfg: Union[str, mmengine.Config],
         visualize = False
 
     if visualize is True:
-        task_processor.visualize(
-            image=img,
-            model=model,
-            result=result,
-            output_file=output_file,
-            window_name=backend.value,
-            show_result=show_result)
+        if not isinstance(img, list):
+            img = [img]
+        for single_img in img:
+            task_processor.visualize(
+                image=single_img,
+                model=model,
+                result=result,
+                output_file=output_file,
+                window_name=backend.value,
+                show_result=show_result)
