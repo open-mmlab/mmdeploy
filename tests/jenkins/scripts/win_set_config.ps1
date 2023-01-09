26135565e701bb47d0936aca1f1f6a7a51e13a8f
pekopoke <1135796875@qq.com>
2023-01-09 17:04:58 +0800

    fix win_set_config.ps1 to support select param

--- tests/jenkins/scripts/win_set_config.ps1
+++ tests/jenkins/scripts/win_set_config.ps1
@@ -5,10 +5,10 @@ Copy-Item -Path $pwd/tests/jenkins/conf/$winconfig -Destination $pwd/tests/jenki
 #cp $pwd/tests/jenkins/conf/$winconfig $pwd/tests/jenkins/conf/win_tmp.config
 cat ./tests/jenkins/conf/win_tmp.config
 #(Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'codebase_list=.*',"codebase_list=$cblist" | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
-(Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'exec_performance=.*',"exec_performance=$eperformance" | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
-(Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'repo_url=.*',"repo_url=$rurl" | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
-(Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'mmdeploy_branch=.*',"mmdeploy_branch=$mbranch" | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
-$ConfigPath = '$pwd/tests/jenkins/conf/win_tmp.config'
+#(Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'exec_performance=.*',"exec_performance=$eperformance" | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
+#(Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'repo_url=.*',"repo_url=$rurl" | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
+#(Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'mmdeploy_branch=.*',"mmdeploy_branch=$mbranch" | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
+$ConfigPath = './tests/jenkins/conf/win_tmp.config'
 Write-Host "$ConfigPath"
 $content = Get-Content $ConfigPath
 $content.replace('codebase_list=.*', "codebase_list=$cblist") | Set-Content $ConfigPath -Verbos
