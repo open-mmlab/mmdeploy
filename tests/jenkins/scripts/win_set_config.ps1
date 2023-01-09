2cf4a91a652c5bbe6be0acbe7a65a46be52193d3
pekopoke <1135796875@qq.com>
2023-01-09 11:52:25 +0800

    fix win_set_config.ps1 to support select param

--- tests/jenkins/scripts/win_set_config.ps1
+++ tests/jenkins/scripts/win_set_config.ps1
@@ -2,7 +2,7 @@ param($cblist, $eperformance, $rurl, $mbranch)
 Write-Host "$cblist, $eperformance, $rurl, $mbranch"
 Write-Host "$pwd"
 Copy-Item ./tests/jenkins/conf/${params.CONFIG} ./tests/jenkins/conf/win_tmp.config
-(Get-content ./tests/jenkins/conf/win_tmp.config -replace 'codebase_list=.*','codebase_list=$cblist' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
-(Get-content ./tests/jenkins/conf/win_tmp.config -replace 'exec_performance=.*','exec_performance=$eperformance' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
-(Get-content ./tests/jenkins/conf/win_tmp.config -replace 'repo_url=.*','repo_url=$rurl' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
-(Get-content ./tests/jenkins/conf/win_tmp.config -replace 'mmdeploy_branch=.*','mmdeploy_branch=$mbranch' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
+(Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'codebase_list=.*','codebase_list=$cblist' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
+(Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'exec_performance=.*','exec_performance=$eperformance' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
+(Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'repo_url=.*','repo_url=$rurl' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
+(Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'mmdeploy_branch=.*','mmdeploy_branch=$mbranch' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
