7f95eeeab5a520378b6ecf3c03e471c098d8e9e0
pekopoke <1135796875@qq.com>
2023-01-09 16:06:01 +0800

    fix win_set_config.ps1 to support select param

--- tests/jenkins/scripts/win_set_config.ps1
+++ tests/jenkins/scripts/win_set_config.ps1
@@ -1,7 +1,8 @@
 param($cblist, $eperformance, $rurl, $mbranch, $winconfig)
 Write-Host "$cblist, $eperformance, $rurl, $mbranch , $winconfig"
 Write-Host "$pwd"
-Copy-Item -Path $pwd/tests/jenkins/conf/$winconfig -Destination $pwd/tests/jenkins/conf/win_tmp.config -Recurse -Force -Verbos
+#Copy-Item -Path $pwd/tests/jenkins/conf/$winconfig -Destination $pwd/tests/jenkins/conf/win_tmp.config -Recurse -Force -Verbos
+cp $pwd/tests/jenkins/conf/$winconfig $pwd/tests/jenkins/conf/win_tmp.config
 (Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'codebase_list=.*','codebase_list=$cblist' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
 (Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'exec_performance=.*','exec_performance=$eperformance' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
 (Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'repo_url=.*','repo_url=$rurl' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
