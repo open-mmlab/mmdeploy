bc5bc2c0eb2ec90644c0717ec89686640ab314ba
pekopoke <1135796875@qq.com>
2023-01-09 15:06:43 +0800

    fix win_set_config.ps1 to support select param

--- tests/jenkins/scripts/win_set_config.ps1
+++ tests/jenkins/scripts/win_set_config.ps1
@@ -1,7 +1,7 @@
 param($cblist, $eperformance, $rurl, $mbranch, $winconfig)
 Write-Host "$cblist, $eperformance, $rurl, $mbranch , $winconfig"
 Write-Host "$pwd"
-Copy-Item -Path ./tests/jenkins/conf/$winconfig -Destination ./tests/jenkins/conf/win_tmp.config -Recurse -Force
+Copy-Item -Path $pwd/tests/jenkins/conf/$winconfig -Destination $pwd/tests/jenkins/conf/win_tmp.config -Recurse -Force
 (Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'codebase_list=.*','codebase_list=$cblist' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
 (Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'exec_performance=.*','exec_performance=$eperformance' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
 (Get-content ./tests/jenkins/conf/win_tmp.config) -replace 'repo_url=.*','repo_url=$rurl' | Set-Content ./tests/jenkins/conf/win_tmp.config -Verbos
