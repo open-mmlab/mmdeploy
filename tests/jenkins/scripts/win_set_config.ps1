44c77e3c684af95abfe4e59a68e1c95513a1217b
pekopoke <1135796875@qq.com>
2023-01-10 10:41:15 +0800

    add the win_max_job_nums param

--- tests/jenkins/scripts/win_set_config.ps1
+++ tests/jenkins/scripts/win_set_config.ps1
@@ -1,5 +1,5 @@
-param($cblist, $eperformance, $rurl, $mbranch, $winconfig)
-Write-Host "$cblist, $eperformance, $rurl, $mbranch , $winconfig"
+param($cblist, $eperformance, $rurl, $mbranch, $winconfig, $mjn)
+Write-Host "$cblist, $eperformance, $rurl, $mbranch , $winconfig, $mjn"
 Write-Host "$pwd"
 Copy-Item -Path $pwd/tests/jenkins/conf/$winconfig -Destination $pwd/tests/jenkins/conf/win_tmp.config -Recurse -Force -Verbos
 #cp $pwd/tests/jenkins/conf/$winconfig $pwd/tests/jenkins/conf/win_tmp.config
@@ -9,6 +9,7 @@ Get-content $pwd/tests/jenkins/conf/win_tmp.config
 (Get-content $pwd/tests/jenkins/conf/win_tmp.config) -replace 'exec_performance=.*',"exec_performance=$eperformance" | Set-Content $pwd/tests/jenkins/conf/win_tmp.config -Verbos
 (Get-content $pwd/tests/jenkins/conf/win_tmp.config) -replace 'repo_url=.*',"repo_url=$rurl" | Set-Content $pwd/tests/jenkins/conf/win_tmp.config -Verbos
 (Get-content $pwd/tests/jenkins/conf/win_tmp.config) -replace 'mmdeploy_branch=.*',"mmdeploy_branch=$mbranch" | Set-Content $pwd/tests/jenkins/conf/win_tmp.config -Verbos
+(Get-content $pwd/tests/jenkins/conf/win_tmp.config) -replace 'max_job_nums=.*',"mmdeploy_branch=$mjn" | Set-Content $pwd/tests/jenkins/conf/win_tmp.config -Verbos
 #$ConfigPath = './tests/jenkins/conf/win_tmp.config'
 #Write-Host "$ConfigPath"
 #$content = Get-Content $ConfigPath
