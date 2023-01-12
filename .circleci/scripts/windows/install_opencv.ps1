90fb2e2f0d6caf1d726fa40cac3d7eee96ddc2cc
RunningLeon <mnsheng@yeah.net>
2023-01-12 12:14:33 +0800

    remove other workflow for test

--- .circleci/scripts/windows/install_opencv.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest -Uri https://github.com/irexyc/mmdeploy-ci-resource/releases/download/opencv/opencv-win-amd64-4.5.5-vc16.zip -OutFile opencv.zip
-Expand-Archive opencv.zip .
-Move-Item opencv-4.5.5 opencv
