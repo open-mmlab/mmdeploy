f73c3e2f0664eed2dc75bba194be5f1e81002c29
RangiLyu <lyuchqi@gmail.com>
2023-01-16 19:51:58 +0800

    [Feature] Support RTMDet-Ins.

---
+++ configs/mmdet/instance-seg/instance-seg_tensorrt_static-640x640.py
@@ -0,0 +1,17 @@
+_base_ = [
+    '../_base_/base_instance-seg_static.py',
+    '../../_base_/backends/tensorrt.py'
+]
+
+onnx_config = dict(input_shape=(640, 640))
+codebase_config = dict(post_processing=dict(export_postprocess_mask=True))
+backend_config = dict(
+    common_config=dict(max_workspace_size=1 << 30),
+    model_inputs=[
+        dict(
+            input_shapes=dict(
+                input=dict(
+                    min_shape=[1, 3, 640, 640],
+                    opt_shape=[1, 3, 640, 640],
+                    max_shape=[1, 3, 640, 640])))
+    ])
