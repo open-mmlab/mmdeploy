205f7293e8d453cb01c7f1d74a048ff0defe1506
irexyc <xinchen.tju@gmail.com>
2023-02-01 19:10:07 +0800

    remove stride buffer

--- csrc/mmdeploy/operation/cuda/permute.cpp
+++ csrc/mmdeploy/operation/cuda/permute.cpp
@@ -1,5 +1,7 @@
 // Copyright (c) OpenMMLab. All rights reserved.
 
+#include "mmdeploy/operation/cuda/permute.h"
+
 #include <cuda_runtime.h>
 
 #include "mmdeploy/operation/vision.h"
@@ -8,8 +10,8 @@ namespace mmdeploy::operation::cuda {
 
 namespace impl {
 template <typename T>
-void Permute(const T* src, const int* src_strides, T* dst, const int* dst_strides, int ndim,
-             int total, cudaStream_t stream);
+void Permute(const T* src, const TensorStride& src_strides, T* dst, const TensorStride& dst_strides,
+             int ndim, int total, cudaStream_t stream);
 }
 
 class PermuteImpl : public Permute {
@@ -20,6 +22,10 @@ class PermuteImpl : public Permute {
     int ndim = src.shape().size();
     if (ndim != axes.size()) {
       MMDEPLOY_ERROR("The size of axes should be equal of src, {} vs {}", axes.size(), ndim);
+      return Status(eInvalidArgument);
+    }
+    if (ndim > MAX_PERMUTE_DIM) {
+      MMDEPLOY_ERROR("Only support ndim < {}", MAX_PERMUTE_DIM);
       return Status(eInvalidArgument);
     }
     std::vector<int> axes_vis(ndim, 0);
@@ -39,31 +45,25 @@ class PermuteImpl : public Permute {
     }
     dst_tensor.Reshape(dst_dims);
 
-    std::vector<int> dst_strides(ndim);
-    std::vector<int> src_strides(ndim);
+    TensorStride dst_strides;
+    TensorStride src_strides;
+
     dst_strides[ndim - 1] = src_strides[ndim - 1] = 1;
     for (int i = ndim - 2; i >= 0; i--) {
       dst_strides[i] = dst_strides[i + 1] * dst_dims[i + 1];
       src_strides[i] = src_strides[i + 1] * src_dims[i + 1];
     }
 
-    std::vector<int> tmp(ndim);
+    TensorStride tmp;
     for (int i = 0; i < ndim; i++) {
       tmp[i] = src_strides[axes[i]];
     }
-    src_strides.swap(tmp);
-
-    Buffer _src_strides(device(), sizeof(int) * ndim);
-    Buffer _dst_strides(device(), sizeof(int) * ndim);
-    OUTCOME_TRY(stream().Copy(src_strides.data(), _src_strides));
-    OUTCOME_TRY(stream().Copy(dst_strides.data(), _dst_strides));
+    src_strides = tmp;
 
     if (src.data_type() == DataType::kINT8) {
-      OUTCOME_TRY(PermuteDispatch<uint8_t>(src, dst_tensor, GetNative<int*>(_src_strides),
-                                           GetNative<int*>(_dst_strides)));
+      OUTCOME_TRY(PermuteDispatch<uint8_t>(src, dst_tensor, src_strides, dst_strides));
     } else if (src.data_type() == DataType::kFLOAT) {
-      OUTCOME_TRY(PermuteDispatch<float>(src, dst_tensor, GetNative<int*>(_src_strides),
-                                         GetNative<int*>(_dst_strides)));
+      OUTCOME_TRY(PermuteDispatch<float>(src, dst_tensor, src_strides, dst_strides));
     } else {
       MMDEPLOY_ERROR("unsupported data type {}", src.data_type());
       return Status(eNotSupported);
@@ -73,8 +73,8 @@ class PermuteImpl : public Permute {
   }
 
   template <typename T>
-  Result<void> PermuteDispatch(const Tensor& src, Tensor& dst, const int* src_strides,
-                               const int* dst_strides) {
+  Result<void> PermuteDispatch(const Tensor& src, Tensor& dst, const TensorStride& src_strides,
+                               const TensorStride& dst_strides) {
     auto src_data = src.data<T>();
     auto dst_data = dst.data<T>();
     auto ndim = src.shape().size();
--- csrc/mmdeploy/operation/cuda/permute.cu
+++ csrc/mmdeploy/operation/cuda/permute.cu
@@ -2,14 +2,16 @@
 
 #include <cstdint>
 
+#include "mmdeploy/operation/cuda/permute.h"
+
 namespace mmdeploy {
 namespace operation {
 namespace cuda {
 namespace impl {
 
 template <typename T>
-__global__ void permute(const T* src, const int* src_strides, T* dst, const int* dst_strides,
-                        int ndim, int total) {
+__global__ void permute(const T* src, const TensorStride src_strides, T* dst,
+                        const TensorStride dst_strides, int ndim, int total) {
   int u = blockIdx.x * blockDim.x + threadIdx.x;
   if (u >= total) {
     return;
@@ -17,27 +19,29 @@ __global__ void permute(const T* src, const int* src_strides, T* dst, const int*
 
   int remaining = u;
   int v = 0;
-  for (int i = 0; i < ndim; i++) {
-    int p = remaining / dst_strides[i];
-    remaining -= p * dst_strides[i];
-    v += p * src_strides[i];
+  for (size_t i = 0; i < ndim; i++) {
+    int p = remaining / dst_strides.v_[i];
+    remaining -= p * dst_strides.v_[i];
+    v += p * src_strides.v_[i];
   }
   dst[u] = src[v];
 }
 
 template <typename T>
-void Permute(const T* src, const int* src_strides, T* dst, const int* dst_strides, int ndim,
-             int total, cudaStream_t stream) {
+void Permute(const T* src, const TensorStride& src_strides, T* dst, const TensorStride& dst_strides,
+             int ndim, int total, cudaStream_t stream) {
   int thread_num = 256;
   int block_num = (total + thread_num - 1) / thread_num;
   permute<T><<<block_num, thread_num, 0, stream>>>(src, src_strides, dst, dst_strides, ndim, total);
 }
 
-template void Permute<float>(const float* src, const int* src_strides, float* dst,
-                             const int* dst_strides, int ndim, int total, cudaStream_t stream);
+template void Permute<float>(const float* src, const TensorStride& src_strides, float* dst,
+                             const TensorStride& dst_strides, int ndim, int total,
+                             cudaStream_t stream);
 
-template void Permute<uint8_t>(const uint8_t* src, const int* src_strides, uint8_t* dst,
-                               const int* dst_strides, int ndim, int total, cudaStream_t stream);
+template void Permute<uint8_t>(const uint8_t* src, const TensorStride& src_strides, uint8_t* dst,
+                               const TensorStride& dst_strides, int ndim, int total,
+                               cudaStream_t stream);
 
 }  // namespace impl
 }  // namespace cuda
