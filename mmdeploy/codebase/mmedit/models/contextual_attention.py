f3434ff93796e74653c276c7c08d2adccb6e8f86
KKIEEK <ethan9867@gmail.com>
2023-02-16 15:18:44 +0900

    Refactor

--- mmdeploy/codebase/mmedit/models/contextual_attention.py
+++ mmdeploy/codebase/mmedit/models/contextual_attention.py
@@ -1,5 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import torch
+
 from mmdeploy.core import FUNCTION_REWRITER
+
+
+def _hw_shape(x):
+    return torch.gather(torch.tensor(x.shape), 0, torch.tensor(
+        (2, 3))).tolist()
 
 
 @FUNCTION_REWRITER.register_rewriter(
@@ -8,9 +15,7 @@ from mmdeploy.core import FUNCTION_REWRITER
 def contextual_attention__patch_correlation(ctx, self, x, kernel):
     # Force tensor shape to avoid the following RuntimeError:
     # Unsupported: ONNX export of convolution for kernel of unknown shape.
-    kernel_shape = ctx.cfg.codebase_config.get('kernel_shape',
-                                               (1024, 96, 3, 3))
-    kernel += kernel.new_zeros(kernel_shape)
+    kernel += kernel.new_zeros(_hw_shape(kernel))
     return ctx.origin_func(self, x, kernel)
 
 
@@ -21,7 +26,5 @@ def contextual_attention__patch_copy_deconv(ctx, self, attention_score,
                                             context_filter):
     # Force tensor shape to avoid the following RuntimeError:
     # Unsupported: ONNX export of convolution for kernel of unknown shape.
-    context_filter_shape = ctx.cfg.codebase_config.get('context_filter_shape',
-                                                       (1024, 96, 4, 4))
-    context_filter += context_filter.new_zeros(context_filter_shape)
+    context_filter += context_filter.new_zeros(_hw_shape(context_filter))
     return ctx.origin_func(self, attention_score, context_filter)
