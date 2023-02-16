f74dcc9ada75455f917aacc5f812662955eb6437
KKIEEK <ethan9867@gmail.com>
2023-02-16 14:50:02 +0900

    Add comment

--- mmdeploy/codebase/mmedit/models/contextual_attention.py
+++ mmdeploy/codebase/mmedit/models/contextual_attention.py
@@ -6,6 +6,8 @@ from mmdeploy.core import FUNCTION_REWRITER
     'mmedit.models.common.contextual_attention.ContextualAttentionModule.'
     'patch_correlation')
 def contextual_attention__patch_correlation(ctx, self, x, kernel):
+    # Force tensor shape to avoid the following RuntimeError:
+    # Unsupported: ONNX export of convolution for kernel of unknown shape.
     kernel_shape = ctx.cfg.codebase_config.get('kernel_shape',
                                                (1024, 96, 3, 3))
     kernel += kernel.new_zeros(kernel_shape)
@@ -17,6 +19,8 @@ def contextual_attention__patch_correlation(ctx, self, x, kernel):
     'patch_copy_deconv')
 def contextual_attention__patch_copy_deconv(ctx, self, attention_score,
                                             context_filter):
+    # Force tensor shape to avoid the following RuntimeError:
+    # Unsupported: ONNX export of convolution for kernel of unknown shape.
     context_filter_shape = ctx.cfg.codebase_config.get('context_filter_shape',
                                                        (1024, 96, 4, 4))
     context_filter += context_filter.new_zeros(context_filter_shape)
