a8f8ea19316a6c1fe61ba8b3816607e9bc9a204b
KKIEEK <ethan9867@gmail.com>
2023-02-16 14:39:33 +0900

    Add configs

---
+++ configs/mmedit/inpainting/inpainting_dynamic.py
@@ -0,0 +1,21 @@
+_base_ = ['./inpainting_static.py']
+
+onnx_config = dict(
+    dynamic_axes={
+        'masked_img': {
+            0: 'batch',
+            2: 'height',
+            3: 'width'
+        },
+        'mask': {
+            0: 'batch',
+            2: 'height',
+            3: 'width'
+        },
+        'output': {
+            0: 'batch',
+            2: 'height',
+            3: 'width'
+        }
+    },
+    input_shape=None)
