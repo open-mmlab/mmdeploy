8559bb5e9625aa933538ec6493bc79c998654e6f
RunningLeon <mnsheng@yeah.net>
2023-02-21 17:44:01 +0800

    support cuda10.2

--- tools/scripts/build_base_image.sh
+++ tools/scripts/build_base_image.sh
@@ -9,11 +9,15 @@ port=${2:8585}
 nohup python3 -m http.server --directory /data2/shared $port > tmp.log 2>&1
 
 export TENSORRT_URL=http://$ip:$port/TensorRT-8.2.3.0.Linux.x86_64-gnu.cuda-11.4.cudnn8.2.tar.gz
-export  TENSORRT_VERSION=8.2.3.0
+export TENSORRT_VERSION=8.2.3.0
+export CUDA_INT=113
+export CUDA_INT=102
 
 # build docker image
-docker build docker/Base/ -t runningleon814/mmdeploy-base:20.04-cu11.3 \
-    --build-arg TENSORRT_URL=${TENSORRT_URL} --build-arg TENSORRT_VERSION=${TENSORRT_VERSION}
+docker build ./docker/Base/ -t runningleon814/mmdeploy-base:20.04-cu11.3 \
+    --build-arg CUDA_INT=$CUDA_INT \
+    --build-arg TENSORRT_URL=${TENSORRT_URL} \
+    --build-arg TENSORRT_VERSION=${TENSORRT_VERSION}
 
 # test docker image
 docker run --gpus=all -itd \
