9c403826d02e9a58de2170453517926af8f8eb68
RunningLeon <mnsheng@yeah.net>
2023-02-22 14:29:06 +0800

    update build image tag

--- tools/scripts/build_base_image.sh
+++ tools/scripts/build_base_image.sh
@@ -11,9 +11,10 @@ nohup python3 -m http.server --directory /data2/shared $port > tmp.log 2>&1
 export TENSORRT_URL=http://$ip:$port/TensorRT-8.2.3.0.Linux.x86_64-gnu.cuda-11.4.cudnn8.2.tar.gz
 export TENSORRT_VERSION=8.2.3.0
 export CUDA_INT=113
+export TAG=ubuntu20.04-cuda11.3
 
 # build docker image
-docker build ./docker/Base/ -t runningleon814/mmdeploy-base:ubuntu20.04-cuda11.3 \
+docker build ./docker/Base/ -t runningleon814/mmdeploy-base:$TAG \
     --build-arg CUDA_INT=$CUDA_INT \
     --build-arg TENSORRT_URL=${TENSORRT_URL} \
     --build-arg TENSORRT_VERSION=${TENSORRT_VERSION}
@@ -23,9 +24,8 @@ docker run --gpus=all -itd \
   -v /data2/benchmark:/root/workspace/openmmlab-data \
   -v /data2/checkpoints:/root/workspace/mmdeploy_checkpoints \
   -v ~/mmdeploy:/root/workspace/mmdeploy \
-  --name test_mmdeploy_base \
-  runningleon814/mmdeploy-base:20.04-cu11.3
+  runningleon814/mmdeploy-base:$TAG
 
 # push to docker hub
 docker login
-docker push runningleon814/mmdeploy-base:20.04-cu11.3
+docker push runningleon814/mmdeploy-base:$TAG
