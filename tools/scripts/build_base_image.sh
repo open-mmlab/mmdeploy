0751b65b520cc5469129eda4d0fb8bbea90eabae
RunningLeon <mnsheng@yeah.net>
2023-02-22 11:33:40 +0800

    update docker file

--- tools/scripts/build_base_image.sh
+++ tools/scripts/build_base_image.sh
@@ -11,10 +11,9 @@ nohup python3 -m http.server --directory /data2/shared $port > tmp.log 2>&1
 export TENSORRT_URL=http://$ip:$port/TensorRT-8.2.3.0.Linux.x86_64-gnu.cuda-11.4.cudnn8.2.tar.gz
 export TENSORRT_VERSION=8.2.3.0
 export CUDA_INT=113
-export CUDA_INT=102
 
 # build docker image
-docker build ./docker/Base/ -t runningleon814/mmdeploy-base:20.04-cu11.3 \
+docker build ./docker/Base/ -t runningleon814/mmdeploy-base:ubuntu20.04-cuda11.3 \
     --build-arg CUDA_INT=$CUDA_INT \
     --build-arg TENSORRT_URL=${TENSORRT_URL} \
     --build-arg TENSORRT_VERSION=${TENSORRT_VERSION}
