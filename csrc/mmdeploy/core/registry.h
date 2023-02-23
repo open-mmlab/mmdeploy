a8b8b7fa0e39025b581347604a96eb7462cac304
zhangli <lzhang329@gmail.com>
2023-02-23 19:11:56 +0800

    comment

--- csrc/mmdeploy/core/registry.h
+++ csrc/mmdeploy/core/registry.h
@@ -129,7 +129,7 @@ class Registry : public Registry<void> {
   using Signature = GetSignature<Tag>;
   using CreatorType = Creator<Signature>;
 
-  // Fixes https://github.com/open-mmlab/mmdeploy/issues/1796
+  // Workaround for gcc-10.2 (https://github.com/open-mmlab/mmdeploy/issues/1796)
   Registry() : Registry<void>{} {}
 
   bool Add(CreatorType& creator) & { return AddCreator(creator); }
