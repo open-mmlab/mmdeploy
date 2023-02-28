1bf2149ea7058fac0a8c1b2a0db2293eb8f8aa4a
Chen Xin <irexyc@gmail.com>
2023-02-28 20:32:38 +0800

    [Feature] Dynamically load net module to remove dependencies of mmdeploy.so (#1776)
    * dynamic load net module
    
    * export xxx_net
    
    * add runpath
    
    * link dl
    
    * remove -ldl for macos
    
    * fix rpath
    
    * module -> shared
    
    * set MMDEPLOY_DYNAMIC_BACKEND OFF when MMDEPLOY_BUILD_SDK_MONOLITHIC is OFF

--- cmake/MMDeploy.cmake
+++ cmake/MMDeploy.cmake
@@ -12,6 +12,15 @@ function (mmdeploy_export NAME)
             RUNTIME DESTINATION bin)
 endfunction ()
 
+macro(mmdeploy_add_net NAME)
+    if (MMDEPLOY_DYNAMIC_BACKEND)
+        mmdeploy_add_library(${NAME} SHARED ${ARGN})
+        target_link_libraries(${PROJECT_NAME} PRIVATE mmdeploy)
+        set(BACKEND_LIB_NAMES ${BACKEND_LIB_NAMES} ${PROJECT_NAME} PARENT_SCOPE)
+    else ()
+        mmdeploy_add_module(${NAME} ${ARGN})
+    endif ()
+endmacro()
 
 function (mmdeploy_add_library NAME)
     # EXCLUDE: exclude from registering & exporting
