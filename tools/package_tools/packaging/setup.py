c808937021e055b62c34c1e2869d937faa04d434
irexyc <xinchen.tju@gmail.com>
2023-03-06 16:59:37 +0800

    generate_build_config.py

--- tools/package_tools/packaging/setup.py
+++ tools/package_tools/packaging/setup.py
@@ -1,6 +1,7 @@
 import os
 import os.path as osp
 import platform
+import sys
 
 try:
     from setuptools import find_packages, setup
@@ -9,7 +10,7 @@ except ImportError:
 
 CURDIR = os.path.realpath(os.path.dirname(__file__))
 version_file = osp.join(CURDIR, 'mmdeploy_python', 'version.py')
-
+package_name = 'mmdeploy_python'
 
 def get_version():
     with open(version_file, 'r') as f:
@@ -21,9 +22,22 @@ def get_platform_name():
     return platform.machine()
 
 
+def parse_arg_remove_boolean(argv, arg_name):
+    arg_value = False
+    if arg_name in sys.argv:
+        arg_value = True
+        argv.remove(arg_name)
+
+    return arg_value
+
+
+if parse_arg_remove_boolean(sys.argv, '--use-gpu'):
+    package_name = package_name + '-gpu'
+
+
 if __name__ == '__main__':
     setup(
-        name='mmdeploy_python',
+        name=package_name,
         version=get_version(),
         description='OpenMMLab Model Deployment SDK python api',
         author='OpenMMLab',
