700480e37df6aeb618ad14ae8a714f13b078657c
hanrui1sensetime <hanrui1@sensetime.com>
2023-03-08 20:06:04 +0800

    sync java docs for demo

--- demo/java/Utils.java
+++ demo/java/Utils.java
@@ -7,12 +7,25 @@ import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.File;
 import java.io.IOException;
+import java.lang.*;
 
+/** @description: this is a util class for java demo. */
 public class Utils {
+
+    /** This function loads the image by path.
+     * @param path: the image path.
+     * @return: the image with Mat format.
+     * @exception IOException: throws an IO exception when load failed.
+     */
     public static Mat loadImage(String path) throws IOException {
         BufferedImage img = ImageIO.read(new File(path));
         return bufferedImage2Mat(img);
     }
+
+    /** This function changes bufferedImage to Mat.
+     * @param img: the bufferedImage.
+     * @return: the image with Mat format.
+     */
     public static Mat bufferedImage2Mat(BufferedImage img) {
         byte[] data = ((DataBufferByte) img.getData().getDataBuffer()).getData();
         return new Mat(img.getHeight(), img.getWidth(), img.getColorModel().getNumComponents(),
