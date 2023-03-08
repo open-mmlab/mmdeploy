fbb9f4a3aaa7ca4bfcc6de4a55bb6f99a5e69916
grimoire <yaoqian@sensetime.com>
2023-03-08 10:21:06 +0800

    update init

--- mmdeploy/backend/ascend/__init__.py
+++ mmdeploy/backend/ascend/__init__.py
@@ -4,8 +4,10 @@ from .onnx2ascend import AtcParam
 from .utils import update_sdk_pipeline
 
 _BackendManager = AscendManager
-
 is_available = _BackendManager.is_available
 build_wrapper = _BackendManager.build_wrapper
+build_wrapper_from_param = _BackendManager.build_wrapper_from_param
+to_backend = _BackendManager.to_backend
+to_backend_from_param = _BackendManager.to_backend_from_param
 
 __all__ = ['update_sdk_pipeline', 'AtcParam', 'AscendParam', 'AscendManager']
--- mmdeploy/backend/coreml/__init__.py
+++ mmdeploy/backend/coreml/__init__.py
@@ -5,5 +5,8 @@ _BackendManager = CoreMLManager
 
 is_available = _BackendManager.is_available
 build_wrapper = _BackendManager.build_wrapper
+build_wrapper_from_param = _BackendManager.build_wrapper_from_param
+to_backend = _BackendManager.to_backend
+to_backend_from_param = _BackendManager.to_backend_from_param
 
 __all__ = ['CoreMLParam', 'CoreMLManager']
