54646810663ba2e1b692c3048d70e6a0770c5894
hanrui1sensetime <hanrui1@sensetime.com>
2023-03-28 17:32:32 +0800

    fix conformer precision

--- mmdeploy/codebase/mmcls/models/backbones/__init__.py
+++ mmdeploy/codebase/mmcls/models/backbones/__init__.py
@@ -1,3 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from . import conformer  # noqa: F401,F403
 from . import shufflenet_v2  # noqa: F401,F403
 from . import vision_transformer  # noqa: F401,F403
