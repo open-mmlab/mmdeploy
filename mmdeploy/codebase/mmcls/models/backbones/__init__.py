03d6a24c9d27b43a7bdc63041d458e4f21f448d5
hanrui1sensetime <hanrui1@sensetime.com>
2023-03-30 12:31:57 +0800

    fix interpolate

--- mmdeploy/codebase/mmcls/models/backbones/__init__.py
+++ mmdeploy/codebase/mmcls/models/backbones/__init__.py
@@ -1,4 +1,3 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from . import conformer  # noqa: F401,F403
 from . import shufflenet_v2  # noqa: F401,F403
 from . import vision_transformer  # noqa: F401,F403
