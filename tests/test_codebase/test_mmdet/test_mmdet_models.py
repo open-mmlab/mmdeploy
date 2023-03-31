9e1d9bf00ca1de4d707b8c974dec16b835a2c5ba
hanrui1sensetime <hanrui1@sensetime.com>
2023-03-31 12:00:17 +0800

    fix ut version

--- tests/test_codebase/test_mmdet/test_mmdet_models.py
+++ tests/test_codebase/test_mmdet/test_mmdet_models.py
@@ -9,14 +9,12 @@ import mmengine
 import numpy as np
 import pytest
 import torch
+from packaging import version
 
 try:
     from torch.testing import assert_close as torch_assert_close
 except Exception:
     from torch.testing import assert_allclose as torch_assert_close
-
-from mmengine import Config
-from mmengine.config import ConfigDict
 
 from mmdeploy.codebase import import_codebase
 from mmdeploy.core.rewriters.rewriter_manager import RewriterContext
@@ -24,6 +22,8 @@ from mmdeploy.utils import Backend, Codebase
 from mmdeploy.utils.test import (WrapFunction, WrapModel, backend_checker,
                                  check_backend, get_model_outputs,
                                  get_onnx_model, get_rewrite_outputs)
+from mmengine import Config
+from mmengine.config import ConfigDict
 
 try:
     import_codebase(Codebase.MMDET)
@@ -222,12 +222,11 @@ def test_multiclass_nms_with_keep_top_k(pre_top_k):
 @backend_checker(Backend.TENSORRT)
 def test__anchorgenerator__single_level_grid_priors():
     backend_type = 'tensorrt'
+    import mmdeploy.codebase.mmdet.models.task_modules.prior_generators.anchor  # noqa
     import onnx
+    from mmdeploy.apis.onnx import export
     from mmdet.models.task_modules.prior_generators.anchor_generator import \
         AnchorGenerator
-
-    import mmdeploy.codebase.mmdet.models.task_modules.prior_generators.anchor  # noqa
-    from mmdeploy.apis.onnx import export
 
     generator = AnchorGenerator(
         scales=[8], ratios=[0.5, 1.0, 2.0], strides=[4])
@@ -694,7 +693,7 @@ def test_forward_of_base_detector(model_cfg_path, backend):
 @pytest.mark.parametrize('backend', [Backend.ONNXRUNTIME])
 @pytest.mark.skipif(
     reason='mha only support torch greater than 1.10.0',
-    condition=torch.__version__ < '1.10.0')
+    condition=version.parse(torch.__version__) < version.parse('1.10.0'))
 @pytest.mark.parametrize(
     'model_cfg_path', ['tests/test_codebase/test_mmdet/data/detr_model.json'])
 def test_predict_of_detr_detector(model_cfg_path, backend):
