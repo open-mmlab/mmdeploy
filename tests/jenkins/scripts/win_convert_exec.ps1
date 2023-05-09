fb14ba04371ee96b1a23f026d31f6736a6936b4a
pekopoke <1135796875@qq.com>
2023-05-09 18:52:14 +0800

    fix the branch in utils.psm1

--- tests/jenkins/scripts/win_convert_exec.ps1
+++ tests/jenkins/scripts/win_convert_exec.ps1
@@ -47,7 +47,7 @@ $codebase_path = (Join-Path $env:JENKINS_WORKSPACE $codebase_fullname)
 Write-Host "codebase_path = $codebase_path"
 $date_snap=Get-Date -UFormat "%Y%m%d"
 $time_snap=Get-Date -UFormat "%Y%m%d%H%M"
-$log_dir = Join-PATH(Join-PATH(Join-Path $env:WORKSPACE "mmdeploy_regression_working_dir\$codebase\$env:CUDA_VERSION" $data_snap) $time_snap)$mmdeploy_branch)
+$log_dir = (Join-PATH(Join-PATH(Join-Path $env:WORKSPACE "mmdeploy_regression_working_dir\$codebase\$env:CUDA_VERSION" $data_snap) $time_snap)$mmdeploy_branch)
 Write-Host "log_dir = $log_dir"
 InitMim $codebase $codebase_fullname $mmdeploy_branch
 python -m pip uninstall mmcv-full -y
