eccb76a5b638acc39decd5b73791330842c6a38b
RunningLeon <mnsheng@yeah.net>
2023-05-16 17:50:33 +0800

    fix ncnn on win

--- mmdeploy/backend/ncnn/__init__.py
+++ mmdeploy/backend/ncnn/__init__.py
@@ -1,11 +1,19 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import os
+from ctypes import cdll
+
 from .backend_manager import NCNNManager
+from .init_plugins import get_ops_path
 from .onnx2ncnn import from_onnx
 
 _BackendManager = NCNNManager
 
 is_available = _BackendManager.is_available
 build_wrapper = _BackendManager.build_wrapper
+# load mmdeploy_ncnn_ops.dll before import ncnn_ext
+ops_path = get_ops_path()
+if os.path.exists(ops_path):
+    cdll.LoadLibrary(ops_path)
 
 __all__ = ['NCNNManager', 'from_onnx']
 
