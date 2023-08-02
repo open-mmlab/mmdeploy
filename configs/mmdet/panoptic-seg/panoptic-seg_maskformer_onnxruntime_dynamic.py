3f06043c3983d976b150884cc98830134211db76
RunningLeon <mnsheng@yeah.net>
2023-08-02 18:01:47 +0800

    support dynamic

---
+++ configs/mmdet/panoptic-seg/panoptic-seg_maskformer_onnxruntime_dynamic.py
@@ -0,0 +1,25 @@
+_base_ = [
+    '../_base_/base_panoptic-seg_static.py',
+    '../../_base_/backends/onnxruntime.py'
+]
+onnx_config = dict(
+    opset_version=12,
+    output_names=['cls_logits', 'mask_logits'],
+    dynamic_axes={
+        'input': {
+            0: 'batch',
+            2: 'height',
+            3: 'width'
+        },
+        'cls_logits': {
+            0: 'batch',
+            2: 'h',
+            3: 'w',
+        },
+        'mask_logits': {
+            0: 'batch',
+            2: 'h',
+            3: 'w',
+        },
+    },
+    input_shape=None)
--- mmdeploy/codebase/mmdet/models/layers/__init__.py
+++ mmdeploy/codebase/mmdet/models/layers/__init__.py
@@ -2,5 +2,6 @@
 # recovery for mmyolo
 from mmdeploy.mmcv.ops import multiclass_nms  # noqa: F401, F403
 from . import matrix_nms  # noqa: F401, F403
+from . import msdeformattn_pixel_decoder  # noqa: F401, F403
 
 __all__ = ['multiclass_nms']
