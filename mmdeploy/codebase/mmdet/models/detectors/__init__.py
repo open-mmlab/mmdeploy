636f3cf38ab37ca9b2a9c616fe669ad23beda091
lrh <cyril.lrh@gmail.com>
2023-09-18 09:05:21 +0000

    update

--- mmdeploy/codebase/mmdet/models/detectors/__init__.py
+++ mmdeploy/codebase/mmdet/models/detectors/__init__.py
@@ -3,7 +3,6 @@ from . import (base_detr, maskformer, panoptic_two_stage_segmentor,
                single_stage, single_stage_instance_seg, two_stage, condinst)
 
 __all__ = [
-<<<<<<< HEAD
     'base_detr',
     'single_stage',
     'single_stage_instance_seg',
@@ -11,8 +10,4 @@ __all__ = [
     'panoptic_two_stage_segmentor',
     'maskformer',
     'condinst'
-=======
-    'base_detr', 'single_stage', 'single_stage_instance_seg', 'two_stage',
-    'panoptic_two_stage_segmentor', 'maskformer', 'condinst'
->>>>>>> ecec42498f1f73e01fd931f10d6cc2b27af823a1
 ]
